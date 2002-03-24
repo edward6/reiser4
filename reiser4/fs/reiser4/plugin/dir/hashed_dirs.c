b1cae8a4ccb80c08adfbdb6b1c4441e295269748
!god <!god>
2002-03-24 16:20:38 +0000

    new files added
    2002/02/04 14:11:57-00:00 !god
    directories implementation:
    
    base methods for directory plugin:
      ->add_entry()
      ->rem_entry()
    
    Hashed directories.
    Directory item implementation.
    Handling of hash collisions, collect stats here.
    Degenerate hash for testing: deg_hash
    
    Object-id map implementation: just counter.
    
    Key assignment policies: see kassign.[ch]
    
    Move key out of reiser4 part of inode. Build it
    dynamically when needed.
    
    More user-level simulation.
    
    Bug correction during debugging.
    
    TBD: compress directory entries into one item:
          + saves 8 bytes per entry
          - lot of error-prone code to shift data, Hans?
    
    2001/08/10 18:26:35-00:00 !reiser
    Initial revision
    
    (Logical change 1.3)

---
+++ reiser4/fs/reiser4/plugin/dir/hashed_dirs.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2001 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+/*
+ * Directory plugin using hashes (see fs/reiser4/plugin/hash.c) to map
+ * file names to to files.
+ */
+
+/* $Id$ */
