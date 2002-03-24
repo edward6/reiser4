0b78cdf3eb0f7d13b6ba19aa941f1cb29621f23e
!reiser <!reiser>
2002-03-24 16:20:38 +0000

    Duplicative of key.c
    2001/08/10 18:25:52-00:00 !reiser
    Initial revision
    
    (Logical change 1.3)

---
+++ reiser4/fs/reiser4/key_ops.c
@@ -0,0 +1,6 @@
+				/* key operations should abstract out all knowledge of key structure */
+				/* we need a lot more things here.... */
+
+decrement_smallest_key_component(struct key *);
+				/* others */
+
