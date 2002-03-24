abf3fc8d3606c7936297821e1e4d86dba58efde4
!god <!god>
2002-03-24 16:20:38 +0000

    s/get_item/type/item_type_by_coord/g
    2002/03/19 16:04:46-00:00 !god
     - commented
     - rename oidmap to oid_allocator
    
    2002/03/18 20:46:01-00:00 !flx
    
    added
    
    /*
     * Make Linus happy.
     * Local variables:
     * c-indentation-style: "K&R"
     * mode-name: "LC"
     * c-basic-offset: 8
     * tab-width: 8
     * fill-column: 120
     * End:
     */
    
    2002/03/18 10:54:34-00:00 !god
    decreases in code quality to conform to the remembered parts of
    "management style":
    
      - remove file names and emacs mode declarations from the beginning
        of the files to make it harder to exchange code outside of the file
        system
    
      - remove $Id$ keyword to make it harder to keep track of by whom and
        when file was modified last
    
    More to follow: change all comments to conform to our unreadable style.
    Probably something other will be remembered.
    
    2002/03/11 15:36:40-00:00 !god
    another breakage fixups. Project suicide, eh?
    
    2002/02/27 17:08:39-00:00 !god
    take block size from VFS super block
    
    2002/02/14 15:42:03-00:00 !jmacd
    Transaction manager now belongs to super_info_data, not the tree.
    
    2002/02/08 15:48:44-00:00 !jmacd
    More constant up-casing, this time to things like LEAF_LEVEL and
    TWIG_LEVEL.  Plus some work on txnmgr interface.
    
    2002/02/05 18:25:14-00:00 !god
    - reiser4_get_current_super_data(): new helper function
    - per-fs trace flags
    
    2002/02/04 18:28:01-00:00 !god
    add statictics info into super-block
    
    2002/01/23 19:03:04-00:00 !god
    compilation reached
    
    2002/01/23 11:00:36-00:00 !god
    CVS $Log$ record removed
    
    2002/01/21 12:49:24-00:00 !god
    trying to compile
    
    2002/01/18 18:16:43-00:00 !god
    synching interfaces with each other. Moving to the elusive
    goal of compilability.
    
    2001/11/01 17:33:21-00:00 !god
    remove references to temporary stub declarations from
    io.h and journal.h
    
    I accept terms and conditions stated in the Legal Agreement
    (available at http://www.namesys.com/legalese.html)
    
    2001/10/11 09:21:09-00:00 !god
    message-ids are added to nikita's assertions, warnings etc.:
    Summary for user nikita:
    Found 463 unique error IDs
    Found 0 duplicate error IDs
    Found 0 assertions without error IDs
    Max error ID is 749
    
    I accept terms and conditions stated in the Legal Agreement
    (available at http://www.namesys.com/legalese.html)
    
    2001/09/28 19:18:10-00:00 !god
    Du cote de chez Compilation:
      debug.c, tree.c, znode.c, vfs_ops.c, key.c, inode.c, super.c,
      plugin/plugin.c, plugin/item/item.c, plugin/item/static_stat.c,
      plugin/object.c, plugin/node/node.c, plugin/file/file.c, plugin/hash.c
    
    
    I accept terms and conditions stated in the Legal Agreement
    (available at http://www.namesys.com/legalese.html)
    
    (Logical change 1.3)

---
+++ reiser4/fs/reiser4/super.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2001 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+/*
+ * Super-block functions.
+ */
+
+#if !defined( __REISER4_SUPER_H__ )
+#define __REISER4_SUPER_H__
+
+/** reiser4-specific part of inode */
+typedef struct reiser4_super_info_data {
+	reiser4_oidmap  oidmap;
+	reiser4_tree    tree;
+	uid_t           default_uid;
+	gid_t           default_gid;
+
+	__u32    blocks_used;
+	__u32    blocks_free;
+	__u32    inode_generation;
+	/** unique file-system identifier */
+	__u32    fsuid;
+
+	/**
+	 * per-fs tracing flags. Use reiser4_trace_flags enum to set
+	 * bits in it.
+	 */
+	__u32    trace_flags;
+
+	/* super block flags */
+
+	__u32    adg                :1;
+	__u32    one_node_plugin    :1;
+
+	reiser4_journal *journal;
+	reiser4_stat     stats;
+
+	/** transaction manager */
+	txn_mgr              tmgr;
+	int                  allocated;
+
+} reiser4_super_info_data;
+
+extern reiser4_super_info_data *reiser4_get_super_data_nocheck( const struct super_block *super );
+
+extern reiser4_super_info_data *reiser4_get_super_data( const struct super_block *super );
+
+extern reiser4_super_info_data *reiser4_get_current_super_data( void );
+
+extern const __u32 REISER4_SUPER_MAGIC;
+
+extern long reiser4_statfs_type( const struct super_block *super );
+extern int  reiser4_blksize( const struct super_block *super );
+extern long reiser4_data_blocks( const struct super_block *super );
+extern long reiser4_free_blocks( const struct super_block *super );
+extern long reiser4_reserved_blocks( const struct super_block *super, 
+			      uid_t uid, gid_t gid );
+extern reiser4_oidmap *reiser4_get_oid_allocator( const struct super_block *super );
+extern reiser4_tree *reiser4_get_tree( const struct super_block *super );
+extern __u32 reiser4_new_inode_generation( const struct super_block *super );
+extern int  reiser4_adg( const struct super_block *super );
+extern int  is_reiser4_super( const struct super_block *super );
+
+extern struct super_block *reiser4_get_current_sb( void );
+
+/* __REISER4_SUPER_H__ */
+#endif
+
+/* 
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * End:
+ */
