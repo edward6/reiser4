6a7cf1ef126400e867999e0da0e1205f0ffde0e7
namesys.com!god <namesys.com!god>
2002-03-25 16:57:19 +0000

    Auto merged
    2002/03/25 19:57:17+03:00 namesys.com!god
    Merge rename: reiser4/fs/reiser4/plugin/dir/dir.h -> reiser4/plugin/dir/dir.h
    
    (Logical change 1.18)

--- reiser4/plugin/dir/dir.h
+++ reiser4/plugin/dir/dir.h
@@ -9,25 +9,7 @@
 #if !defined( __REISER4_DIR_H__ )
 #define __REISER4_DIR_H__
 
-/** create sd for directory file. Create stat-data, dot, and dotdot. */
-extern int                directory_file_create( struct inode *object, 
-						 struct inode *parent,
-						 reiser4_object_create_data * );
-extern int                directory_file_delete( struct inode *object, 
-						 struct inode *parent );
-extern int                dir_file_owns_item   ( const struct inode *inode, 
-						 const tree_coord *coord );
-extern file_lookup_result directory_lookup     ( struct inode *inode, 
-						 const struct qstr *name,
-						 reiser4_key *key, 
-						 reiser4_entry *entry );
-extern int                directory_add_entry  ( struct inode *object,
-						 struct dentry *where, 
-						 reiser4_object_create_data *,
-						 reiser4_entry *entry );
-extern int                directory_rem_entry  ( struct inode *object, 
-						 struct dentry *where, 
-						 reiser4_entry *entry );
+extern void reiser4_directory_readahead( struct inode *dir, tree_coord *coord );
 
 /* __REISER4_DIR_H__ */
 #endif
