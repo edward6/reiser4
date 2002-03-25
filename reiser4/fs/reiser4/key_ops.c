bb9519325d4ed3a87f4158605d3b1b6cc8e7118d
namesys.com!reiser <namesys.com!reiser>
2002-03-25 15:58:28 +0000

    mvdir
    }(Logical change 1.14)

--- reiser4/fs/reiser4/key_ops.c
+++
@@ -1,6 +0,0 @@
-				/* key operations should abstract out all knowledge of key structure */
-				/* we need a lot more things here.... */
-
-decrement_smallest_key_component(struct key *);
-				/* others */
-
