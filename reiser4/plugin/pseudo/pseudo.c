a2316ab700ff335cfc63307366fe9656f3fd08ce
namesys.com!reiser <namesys.com!reiser>
2002-04-01 10:46:51 +0000

    none
    (Logical change 1.77)

--- reiser4/plugin/pseudo/pseudo.c
+++ reiser4/plugin/pseudo/pseudo.c
@@ -20,52 +20,17 @@
  * objects involved. On the other hand, we need to provide traditional
  * interface to VFS based on inode/dentry/file.
  *
- * Source code organization: currently everything is in pseudo.[ch], mainly
- * because there is not much code. Probably pseudo/ should be created and each
- * PF type will have special .[ch] pair there. Or may be plugin/pseudo/ is
- * better.
- *
- *
- * OPEN ISSUES
- *
- *
- * (A) 
- *
- *   To avoid code duplication, it seems rasonable to design such a unified
- *   interface to PF directory suitable for use by RSCCL and implement VFS
- *   operations on the top of it.
- *
- *   Problem is that RSCCL is not well defined as of now.
- *
- *
- * (B) 
- *
- *   Another complication is with inode numbers: it is not clear what inode
- *   numbers should be assigned to inodes created for PFs (in the case of VFS
- *   access paths), and to what super block(s) they have to be attached.
- *
- *   Different schemata can be devised for this. For now it is better to stick
- *   to something really simple. For eaxmple, we can use existing reiser4
- *   super block, and assign the same otherwise invalid inode number to all
- *   PFs. Then, find_actor passed to the iget can be used to distinguish them.
- *
- *
- * (C) 
- *
- *   How to implement VFS operations/objects? One solution is to implement new
- *   inode flavour completely separate from other reiser4 inodes, with
- *   different private part, different set of VFS ops, etc. This is easier to
- *   do in 2.5., because no changes in any global data structures are longer
- *   necessary.
- *
- *   On the other hand, we can implement each type of PF ("..key", "..foo") as
+
+When assigning inode numbers to pseudo files, we use the last 2^62 oids of the 64 oid space.
+
+     We implement each type of PF ("..key", "..foo") as
  *   new object plugin in addition to the standard object plugins (regular
- *   file, directory, pipe, and so on). This has an advantage on reusing
- *   existing object plugin methods.
- *
- *   Problems with latter approach is that PF inode has to store some
- *   additional information somewhere. At the very least, pointer to the
- *   "host" object inode is required. Adding yet another field to the generic
+ *   file, directory, pipe, and so on). 
+
+ *   PF inodes require a pointer to the
+ *   "host" object inode. 
+
+     Adding yet another field to the generic
  *   reiser4_inode_info_data that will only be used for the PF seems to be an
  *   excess. Moreover, each PF type can require its own additional state,
  *   which is hard to predict in advance, and ability to freely add new PF
@@ -80,17 +45,26 @@
  *
  *   So, for now, we can reuse locality_id in reiser4 private part of inode to
  *   store number of host inode of PF, and create special object plugin for
- *   all PF types. That plugin will use some other field in reiser4 private
+ *   each PF type. 
+
+ *   That plugin will use some other field in reiser4 private
  *   inode part that is meaning less for pseudo files (->extmask?) to
  *   determine type of PF and use appopriate low level PF operations
  *   (pseudo_ops) to implement VFS operations.
+
+Use the pluginid field?
+
  *
  *   All this (hack-like) machinery has of course nothing to do with reiser4()
  *   system call that will use pseudo_ops directly.
+
+ We do however need to define the methods that the reiser4 system call will use.
+
  *
  * NOTES
  *
  *  Special flag in reiser4_plugin_ref->flags to detect pseudo file.
+(The plugin id?)
  *  Mark PF inode as loaded: ->flags | REISER4_LOADED
  *
  */
