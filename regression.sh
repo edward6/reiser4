301f203b9f1285018e749c88540ed340c41302a2
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Rename: reiser4/regression.sh -> regression.sh
    (Logical change 1.109)

---
+++ regression.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+./a.out nikita ibk 10000 || exit 1
+./a.out nikita dir 1 100 || exit 2
+./a.out nikita dir 3 1000 || exit 3
+./a.out jmacd build 3 1000 1000 || exit 4
+./a.out vs || exit 5
+
+
