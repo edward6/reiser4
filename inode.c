f331d1c13425bab593654c7824df758c93a505e0
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Rename: reiser4/inode.c -> inode.c
    (Logical change 1.109)

---
+++ inode.c
@@ -0,0 +1,390 @@
+/*
+ * Copyright 2001 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+/*
+ * Inode specific operations.
+ */
+
+#include "reiser4.h"
+
+/** return pointer to reiser4-specific part of inode */
+reiser4_inode_info_data *reiser4_inode_data( const struct inode *inode )
+{
+	assert( "nikita-254", inode != NULL );
+	/* If you got compilation error at this point, go to the
+	   include/linux/reiser4_i.h and increase
+	   REISER4_STUB_INODE_INFO_LENGTH */
+	cassert( sizeof( reiser4_inode_info_data ) <= 
+		 sizeof( struct reiser4_inode_info ) );
+	return ( reiser4_inode_info_data * )&inode -> u.reiser4_i;
+}
+
+/** return reiser4 internal tree which inode belongs to */
+reiser4_tree *tree_by_inode( const struct inode *inode )
+{
+	assert( "nikita-256", inode != NULL );
+	assert( "nikita-257", inode -> i_sb != NULL );
+	return reiser4_get_tree( inode -> i_sb );
+}
+
+/** main function for external code to get plugins associated with 
+    given file-system object */
+reiser4_plugin_ref *reiser4_get_object_state( const struct inode *inode )
+{
+	assert( "nikita-264", inode != NULL );
+	return &reiser4_inode_data( inode ) -> plugin;
+}
+
+/** return internal reiser4 inode flags, stored as part of object plugin
+    state in reiser4-specific part of inode */
+__u32 *reiser4_inode_flags( const struct inode *inode )
+{
+	assert( "nikita-267", inode != NULL );
+
+	return & reiser4_get_object_state( inode ) -> flags;
+}
+
+/** file plugin for @inode */
+reiser4_file_plugin *reiser4_get_file_plugin( const struct inode *inode )
+{
+	assert( "nikita-269", inode != NULL );
+	assert( "nikita-270", is_reiser4_inode( inode ) );
+
+	return &reiser4_get_object_state( inode ) -> file -> u.file;
+}
+
+
+/** lock inode. We lock file-system wide spinlock, because we have to lock
+ *  inode _before_ we have actually read and initialised it and we cannot rely
+ *  on memset() in fs/inode.c to initialise spinlock. Alternative is to grab
+ *  i_sem, but it's semaphore rather than spinlock, so it's not clear what
+ *  would be more effective.
+ *
+ *  Taking inode->i_sem is simple and scalable, but taking and releasing
+ *  semaphore is much more expensive than taking spin-lock. So, for the time
+ *  being, let's just pile a number of possible locking schemes here and
+ *  choose best (or leave them as options) after benchmarking.
+ *
+ *  This is because we dont't have enough empirical evidence about scalability
+ *  of each scheme.
+ *
+ * FIXME-NIKITA ->i_sem is not we actually won't. May be spinlock is better
+ * after all.
+ */
+void reiser4_lock_inode( struct inode *inode )
+{
+	assert( "nikita-272", inode != NULL );
+
+	down( &inode -> i_sem );
+}
+
+/**
+ * Same as reiser4_lock_inode(), but signals are allowed to wake current
+ * thread up while it is sleeping waiting for the inode lock.
+ *
+ * This is preferrable approach as it allows user to kill process stuck in "D"
+ * state waiting for inode. Unfortunately there are situations when it is
+ * inconvenient.
+ *
+ */
+int reiser4_lock_inode_interruptible( struct inode *inode )
+{
+	assert( "nikita-1270", inode != NULL );
+
+	return down_interruptible( &inode -> i_sem );
+}
+
+/**
+ * Release lock on inode.
+ *
+ */
+void reiser4_unlock_inode( struct inode *inode )
+{
+	assert( "nikita-277", inode != NULL );
+
+	up( &inode -> i_sem );
+}
+
+/** check that "inode" is on reiser4 file-system */
+int is_reiser4_inode( const struct inode *inode )
+{
+	return( ( inode != NULL ) && 
+		( is_reiser4_super( inode -> i_sb ) ||
+		  ( inode -> i_op == &reiser4_inode_operations ) ) );
+		
+}
+
+/**
+ * Maximal length of a name that can be stored in directory @inode.
+ *
+ * This is used in check during file creation and lookup.
+ */
+int reiser4_max_filename_len( const struct inode *inode )
+{
+	assert( "nikita-287", is_reiser4_inode( inode ) );
+	return plugin_by_id( REISER4_ITEM_PLUGIN_ID, REISER4_DIR_ITEM_PLUGIN ) -> 
+		u.item.s.dir.max_name_len( reiser4_blksize( inode -> i_sb ) );
+}
+
+/** return plugin that should be used to create stat-data for this
+    file */
+reiser4_plugin *reiser4_get_sd_plugin( const struct inode *inode UNUSED_ARG )
+{
+	assert( "nikita-288", is_reiser4_inode( inode ) );
+	return plugin_by_id( REISER4_ITEM_PLUGIN_ID, SD_ITEM_ID );
+}
+
+/** return information about "repetitive access" (ra) patterns,
+    accumulated in inode. */
+inter_syscall_ra_hint *reiser4_inter_syscall_ra( const struct inode *inode )
+{
+	assert( "nikita-289", is_reiser4_inode( inode ) );
+	return &reiser4_get_object_state( inode ) -> ra;
+}
+
+/* should be moved into .h */
+
+/*  has inode been initialized? */
+static int is_inode_loaded( const struct inode *inode )
+{
+	assert( "nikita-1120", inode != NULL );
+	return reiser4_get_object_state( inode ) -> flags & REISER4_LOADED;
+}
+
+/**
+ * Install file, inode, and address_space operation on @inode, depending on
+ * its mode.
+ */
+int setup_inode_ops( struct inode *inode )
+{
+	switch( inode -> i_mode & S_IFMT ) {
+	case S_IFSOCK:
+	case S_IFBLK:
+	case S_IFCHR:
+	case S_IFIFO: {
+		__u32 rdev = 0; /* to keep gcc happy */
+
+		/* ugly hack with rdev */
+		rdev = inode -> i_rdev;
+		inode -> i_rdev = 0;
+		inode -> i_blocks = 0;
+		init_special_inode( inode, inode -> i_mode, rdev );
+		break;
+	}
+	case S_IFLNK:
+	case S_IFDIR:
+	case S_IFREG:
+		inode -> i_op = &reiser4_inode_operations;
+		inode -> i_fop = &reiser4_file_operations;
+		inode -> i_mapping -> a_ops = &reiser4_as_operations;
+		break;
+	default:
+		warning( "nikita-291", "wrong file mode: %o for %lx", 
+			 inode -> i_mode, ( long ) inode -> i_ino );
+		make_bad_inode( inode );
+		return -EINVAL;
+	}
+	return 0;
+}
+
+/** initialise inode from disk data. Called with inode locked.
+    Return inode locked. */
+int init_inode( struct inode *inode, tree_coord *coord )
+{
+	int result;
+	reiser4_plugin *plugin;
+	void           *body;
+	int             length;
+
+	assert( "nikita-292", coord != NULL );
+	assert( "nikita-293", inode != NULL );
+
+	plugin = item_plugin_by_coord( coord );
+	body = item_body_by_coord( coord );
+	length = item_length_by_coord( coord );
+
+	assert( "nikita-295", plugin != NULL );
+	assert( "nikita-296", body != NULL );
+	assert( "nikita-297", length > 0 );
+
+	/* call stat-data plugin method to load sd content into inode */
+	result = plugin -> u.item.s.sd.init_inode( inode, body, length );
+	if( result )
+		return result;
+	reiser4_get_object_state( inode ) -> sd = plugin;
+	return setup_inode_ops( inode );
+}
+
+/* read `inode' from the disk. This is what was previously in 
+   reiserfs_read_inode2().
+
+   Must be called with inode locked. Return inode still locked.
+*/
+static int read_inode( struct inode * inode )
+{
+	int          result;
+	reiser4_key  key;
+	tree_coord  coord;
+	reiser4_lock_handle lh;
+
+	assert( "nikita-298", inode != NULL );
+	
+	if( is_inode_loaded( inode ) )
+		return 0;
+
+	if( reiser4_get_object_state( inode ) -> locality_id == 0 ) {
+		warning( "nikita-300", "no locality in inode %lx",
+			 ( long ) inode -> i_ino );
+		make_bad_inode( inode );
+		return -EINVAL;
+	}
+
+	/* Release inode lock during io. */
+	reiser4_unlock_inode( inode );
+	reiser4_init_coord( &coord );
+	reiser4_init_lh( &lh );
+	/* locate stat-data in a tree and return znode locked */
+	result = lookup_sd( inode, ZNODE_READ_LOCK, &coord, &lh, &key );
+	reiser4_lock_inode( inode );
+	if( ( result == 0 ) && ! is_inode_loaded( inode ) ) {
+		*reiser4_inode_flags( inode ) |= REISER4_LOADED;
+		assert( "nikita-301", is_inode_loaded( inode ) );
+		/* use stat-data plugin to load sd into inode. */
+		result = init_inode( inode, &coord );
+		if( result == 0 ) {
+			/*
+			 * setup inode number and locality id from key.
+			 */
+			inode -> i_ino = get_key_objectid( &key );
+			assert( "nikita-1271", 
+				reiser4_get_object_state( inode ) -> 
+				locality_id == get_key_locality( &key ) );
+		}
+	}
+	/* lookup_sd() doesn't release coord because we want znode
+	   stay read-locked while stat-data fields are accessed in
+	   init_inode() */
+	reiser4_done_lh( &lh );
+	reiser4_done_coord( &coord );
+	if( result != 0 ) {
+		if( inode != NULL )
+			make_bad_inode( inode );
+	}
+	return result;
+}
+ 
+/** this is our helper function a la iget().
+    Probably we also need function taking locality_id as the second argument. ???
+    This will be called by reiser4_lookup() and reiser4_read_super().
+    Return inode locked.
+*/
+struct inode * reiser4_iget( struct super_block *super, 
+			     const reiser4_key *key /* key of inode's stat-data */
+			     )
+{
+	struct inode *inode;
+	int failed;
+
+	assert( "nikita-302", super != NULL );
+	assert( "nikita-303", key != NULL );
+
+	/** call iget(). Our ->read_inode() is dummy, so this will either
+	    find inode in cache or return uninitialised inode */
+	/* FIXME_JMACD: get_key_objectid is 64 bits, but iget() takes unsigned long */
+	inode = iget( super, (__u32) get_key_objectid( key ) );
+	failed = 0;
+	if( ! inode ) 
+		return NULL;
+	else if( is_bad_inode( inode ) ) {
+		warning( "nikita-304", "Stat data not found" );
+		print_key( "key", key );
+		failed = 1;
+	} else {
+		reiser4_key found_key;
+		/* locking: the only way for the process to obtain a reiser4
+		   inode is through reiserfs_iget().  Knfsd used to call
+		   iget() directly but newer versions rely on ->fh_to_dentry()
+		   method.
+
+		   If process B calls this function while process A is
+		   sleeping in read_inode() for the same inode, B will find
+		   inode in the cache and go into read_inode() also.
+
+		   read_inode() takes care not to initialise the same inode
+		   twice.
+		*/
+		failed = reiser4_lock_inode_interruptible( inode );
+		if( !failed && ! is_inode_loaded( inode ) ) {
+			reiser4_get_object_state( inode ) -> locality_id = 
+				get_key_locality( key );
+			/* now, inode has objectid as -> i_ino and locality in
+			   reiser4-specific part. This data is enough for
+			   read_inode() to read stat data from the disk */
+			if( read_inode( inode ) != 0 )
+				failed = 1;
+		}
+		build_sd_key( inode, &found_key );
+		if( !failed && ( keycmp( &found_key, key ) != EQUAL_TO ) ) {
+			warning( "nikita-305", "Wrong key in sd" );
+			print_key( "sought for", key );
+			print_key( "found", &found_key );
+			failed = 1;
+		}
+	}
+	if( failed ) {
+		iput( inode );
+		return NULL;
+	} else
+		return inode;
+}
+
+/**
+ * Debugging aid: print information about inode.
+ */
+void print_inode( const char *prefix, const struct inode *i )
+{
+	reiser4_key         inode_key;
+	reiser4_plugin_ref *ref;
+
+	if( i == NULL ) {
+		info( "%s: inode: null\n", prefix );
+		return;
+	}
+	info( "%s: ino: %lu, count: %i, link: %i, mode: %o, size: %llu\n",
+	      prefix, i -> i_ino, atomic_read( &i -> i_count ), i -> i_nlink,
+	      i -> i_mode, ( unsigned long long ) i -> i_size );
+	info( "\tuid: %i, gid: %i, dev: %i, rdev: %i\n", 
+	      i -> i_uid, i -> i_gid, ( int ) i -> i_dev, i -> i_rdev );
+	info( "\tatime: %li, mtime: %li, ctime: %li\n",
+	      i -> i_atime, i -> i_mtime, i -> i_ctime );
+	info( "\tblkbits: %i, blksize: %lu, blocks: %lu\n",
+	      i -> i_blkbits, i -> i_blksize, i -> i_blocks );
+	info( "\tversion: %lu, generation: %i, attr. flags: %u, flags: %u\n",
+	      i -> i_version, i -> i_generation, i -> i_attr_flags, 
+	      i -> i_flags );
+	info( "\tis_reiser4_inode: %i\n", is_reiser4_inode( i ) );
+	print_key( "\tkey", build_sd_key( i, &inode_key ) );
+	ref = &reiser4_inode_data( i ) -> plugin;
+	print_plugin( "\tfile", ref -> file );
+	print_plugin( "\tperm", ref -> perm );
+	print_plugin( "\ttail", ref -> tail );
+	print_plugin( "\thash", ref -> hash );
+	print_plugin( "\tsd", ref -> sd );
+	info( "\tflags: %u, bytes: %llu, extmask: %llu, sd_len: %i, pmask: %i\n",
+	      ref -> flags, ref -> bytes, ref -> extmask, 
+	      ( int ) ref -> sd_len, ref -> plugin_mask );
+}
+
+/* 
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * End:
+ */
+
+
