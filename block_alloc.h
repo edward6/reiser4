b52da58b5f2ee9c4e730d4c4bc27f2681e986246
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Rename: reiser4/block_alloc.h -> block_alloc.h
    (Logical change 1.109)

---
+++ block_alloc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2002 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+
+int reiser4_free_block (block_nr block);
+int allocate_new_blocks (block_nr * hint, block_nr * count);
+
+/*
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * End:
+ */
