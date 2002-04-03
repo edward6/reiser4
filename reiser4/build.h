48085d423b682fdba66a44a337199113f8f7ca8b
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Delete: reiser4/Attic/build.h
    }(Logical change 1.109)

--- reiser4/build.h
+++
@@ -1,6 +0,0 @@
-/* -*- C -*- Automatically generated. Do not edit. */
-#define BUILD_DATE "Fri Feb 15 18:36:38 UTC 2002"
-#define BUILD_HOST "laputa"
-#define BUILDER    "god"
-#define BUILD       32
-
