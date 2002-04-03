77253be4bf3153db8c4d5078f73ee11d4e683d04
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Rename: reiser4/slum.h -> slum.h
    (Logical change 1.109)

---
+++ slum.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2001 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+/*
+ * Slum data-types. See slum_track.c for description.
+ */
+
+#if !defined( __SLUM_H__ )
+#define __SLUM_H__
+
+#define SLUM_SCAN_MAXNODES 10
+
+/* The slum_scan data structure maintains the state of an in-progress slum scan. */
+struct slum_scan {
+	/* The number of nodes currently in the slum. */
+	unsigned  size;
+
+	/* True if we found the leftmost node before reaching max size. */
+	int       stop;
+
+	/* The current scan position. */
+	jnode    *node;
+
+	/* The current scan atom. */
+	txn_atom *atom;
+};
+
+extern int flush_jnode_slum ( jnode *node );
+
+/* __SLUM_H__ */
+#endif
+
+/* 
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * scroll-step: 1
+ * End:
+ */
+
