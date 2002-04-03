2e748200029addc049441c41c29ad9cb4077fe97
namesys.com!jmacd <namesys.com!jmacd>
2002-04-03 09:11:03 +0000

    Rename: reiser4/sys_reiser4.c -> sys_reiser4.c
    (Logical change 1.109)

---
+++ sys_reiser4.c
@@ -0,0 +1,9 @@
+/* System call for accessing enhanced semantics of the Reiser Filesystem Version 4 (reiser4). */
+
+/* This system call feeds a string to parser.c, parser.c converts the
+   string into a set of commands which are executed, and then this
+   system call returns after the completion of those commands. */
+
+/* Please read parser.c and return. */
+
+/* Mr. Demidov, please implement by the end of March. */
