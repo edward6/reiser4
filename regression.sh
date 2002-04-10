63cdd551d85a31003a5d8bc9c77400c95dd4284d
namesys.com!vs <namesys.com!vs>
2002-04-10 14:12:20 +0000

    10000 is too many
    (Logical change 1.161)

--- regression.sh
+++ regression.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-./a.out nikita ibk 10000 || exit 1
+./a.out nikita ibk 1000 || exit 1
 ./a.out nikita dir 1 100 || exit 2
 ./a.out nikita dir 3 1000 || exit 3
 ./a.out jmacd build 3 1000 1000 || exit 4
