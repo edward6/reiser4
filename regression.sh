127284f14f1cabcadc0ebfe535f10c39a81c6821
namesys.com!god <namesys.com!god>
2002-04-16 13:27:28 +0000

    trivial improvements to regression test "suite"
    (Logical change 1.233)

--- regression.sh
+++ regression.sh
@@ -1,9 +1,15 @@
 #! /bin/sh
 
-./a.out nikita ibk 1000 || exit 1
+printf "REISER4_TRACE_FLAGS:     %x\n" $REISER4_TRACE_FLAGS
+printf "REISER4_BLOCK_SIZE:      %i\n" ${REISER4_BLOCK_SIZE:-256}
+printf "REISER4_UL_DURABLE_MMAP: %s\n" $REISER4_UL_DURABLE_MMAP
+
+export REISER4_PRINT_STATS=1
+
+./a.out nikita ibk 10000 || exit 1
 ./a.out nikita dir 1 100 || exit 2
 ./a.out nikita dir 3 1000 || exit 3
 ./a.out jmacd build 3 1000 1000 || exit 4
-./a.out vs || exit 5
+( find /tmp | ./a.out vs copydir ) || exit 5
 
 
