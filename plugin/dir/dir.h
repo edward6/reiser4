79639037e4a00937a9508e352d61b2c3e0063da8
namesys.com!vs <namesys.com!vs>
2002-04-16 07:41:52 +0000

    strcture used by directory plugin methods
    (Logical change 1.217)

--- plugin/dir/dir.h
+++ plugin/dir/dir.h
@@ -11,6 +11,31 @@
 
 extern void reiser4_directory_readahead( struct inode *dir, tree_coord *coord );
 
+/** 
+ * description of directory entry being created/destroyed/sought for
+ * 
+ * It is passed down to the directory plugin and farther to the
+ * directory item plugin methods. Creation of new directory is done in
+ * several stages: first we search for an entry with the same name, then
+ * create new one. reiser4_dir_entry_desc is used to store some information
+ * collected at some stage of this process and required later: key of
+ * item that we want to insert/delete and pointer to an object that will
+ * be bound by the new directory entry. Probably some more fields will
+ * be added there.
+ *
+ */
+struct reiser4_dir_entry_desc {
+	/*
+	 * key of directory entry
+	 */
+	reiser4_key   key;
+	/*
+	 * object bound by this entry.
+	 */
+	struct inode *obj;
+};
+
+
 /* __REISER4_DIR_H__ */
 #endif
 
