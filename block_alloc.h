f0e64db322daca65f8c540c41e32f9ed470d7337
namesys.com!zam <namesys.com!zam>
2002-04-26 17:52:22 +0000

    bitmap.h is merged with block_alloc.h because of common code in journal hooks.
    (Logical change 1.335)

--- block_alloc.h
+++ block_alloc.h
@@ -16,6 +16,22 @@ struct reiser4_blocknr_hint {
 	 * parameters */
 };
 
+/* Block allocation/deallocation are done through special bitmap objects which
+ * are allocated in an array at fs mount. */
+struct reiser4_bnode {
+	znode * working;	/* working bitmap block */
+	znode * commit;		/* commit bitmap block */
+};
+
+extern int reiser4_bitmap_alloc (block_nr *, block_nr, int, int);
+
+extern int block_alloc_pre_commit_hook (txn_atom*);
+extern int block_alloc_post_commit_hook (txn_atom*);
+extern int block_alloc_post_writeback_hook (txn_atom*);
+
+extern int reiser4_init_bitmap ();
+extern void reiser4_done_bitmap ();
+
 extern int reiser4_blocknr_is_fake (const reiser4_disk_addr *);
 
 extern int reiser4_alloc_new_unf_blocks (int count);
@@ -26,8 +42,6 @@ extern int reiser4_alloc_blocks (struct reiser4_blocknr_hint * hint UNUSED_ARG,
 				 block_nr * start, int * len);
 extern void reiser4_dealloc_block (jnode *node);
 
-#endif /* __FS_REISER4_BLOCK_ALLOC_H__ */
-
 /* FIXME_ZAM: these prototypes of old block allocator functions are here just
  * for now, until full support of new block allocator (it includes working
  * mkreiser4) is completed. */
@@ -36,6 +50,8 @@ extern int allocate_new_blocks (block_nr * start, block_nr *len);
 extern void reiser4_delete_block (block_nr block UNUSED_ARG);
 extern int free_blocks (block_nr start UNUSED_ARG, block_nr len UNUSED_ARG);
 extern void reiser4_free_block (block_nr block UNUSED_ARG);
+
+#endif /* __FS_REISER4_BLOCK_ALLOC_H__ */
 
 /*
  * Make Linus happy.
