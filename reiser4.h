545591ebec63ee860cce286646fd0b6f1f83b0a3
namesys.com!zam <namesys.com!zam>
2002-04-26 17:52:22 +0000

    bimap.h excluded.
    (Logical change 1.335)

--- reiser4.h
+++ reiser4.h
@@ -239,7 +239,6 @@ typedef struct { int foo; } NAME ## _spin_dummy
 #include "znode.h"
 /*#include "tree.h"*/
 #include "slum.h"
-#include "bitmap.h"
 #include "block_alloc.h"
 #include "tree_walk.h"
 #include "pool.h"
