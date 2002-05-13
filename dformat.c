5ab5edaa2bb9b552a724e098aa13a02ad1ac386b
namesys.com!vs <namesys.com!vs>
2002-05-13 11:11:00 +0000

    conflict resolved
    2002/05/10 12:50:27+04:00 namesys.com!vs
    space allocator plugin is added
    
    2002/05/07 12:41:45+04:00 namesys.com!vs
    s/mgr/allocator
    
    (Logical change 1.399)

--- dformat.c
+++ dformat.c
@@ -1,20 +1,11 @@
-/* Copyright 2002 Hans Reiser, licensing governed by reiser4/README */
+/*
+ * Copyright 2002 Hans Reiser, licensing governed by reiser4/README
+ */
+
+DO NOT COMPILE THIS
 
 #include "reiser4.h"
 #define REISER4_FIRST_BITMAP_BLOCK 100
-
-/** A number of bitmap blocks for given fs. This number can be stored on disk
- * or calculated on fly; it depends on disk format. */
-int get_nr_bmap (struct super_block * super)
-{
-	reiser4_super_info_data * info_data = get_super_private(super); 
-
-	assert ("zam-391", super->s_blocksize > 0);
-	assert ("zam-392", info_data != NULL);
-	assert ("zam-393", info_data->blocks_used != 0);
-
-	return (info_data->blocks_used - 1) / super->s_blocksize + 1;
-}
 
 
 /** return a physical disk address for logical bitmap number @bmap */
@@ -33,27 +24,6 @@ void get_bitmap_blocknr (struct super_block * super, int bmap, reiser4_block_nr 
 		*bnr = bmap * super->s_blocksize * 8;
 	}
 }
-
-
-reiser4_plugin space_plugins[ LAST_SPACE_MGR_ID ] = {
-	[ DEFAULT_40_SPACE_MGR_ID ] = {
-		.h = {
-			.type_id = REISER4_SPACE_MGR_PLUGIN_TYPE,
-			.id      = DEFAULT_40_SPACE_MGR_ID,
-			.pops    = NULL,
-			.label   = "reiser40 default disk space manager",
-			.desc    = "bitmap based",
-			.linkage = TS_LIST_LINK_ZERO,
-		},
-		.u = {
-			.space_mgr = {
-				.init_space_mgr = NULL
-			}
-		}
-	}
-};
-
-
 
 
 /* 
