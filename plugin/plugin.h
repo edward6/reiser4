df62a9a6265d40cb98ed39b29d5d8863c22fadc6
namesys.com!god <namesys.com!god>
2002-05-14 10:57:35 +0000

    Auto merged
    2002/05/13 22:07:59+04:00 namesys.com!zam
    hooks from journal code added to space allocator plugin
    interface.
    
    (Logical change 1.412)

--- plugin/plugin.h
+++ plugin/plugin.h
@@ -320,7 +320,14 @@ typedef struct space_allocator_plugin {
 	int ( *alloc_blocks )( reiser4_blocknr_hint *, int needed,
 			       reiser4_block_nr *start, int *len );
 	void ( *dealloc_blocks )( reiser4_block_nr start, int len );
+reiser4_blocknr_hint *, int needed,
+			       reiser4_block_nr *start, int *len );
+	void ( *dealloc_blocks )( reiser4_block_nr start, int len );
 
+	/* program hooks from journal code */
+	void ( *pre_commit_hook      )( txn_atom * );
+	void ( *post_commit_hook     )( txn_atom * );
+	void ( *post_write_back_hook )( txn_atom * );
 } space_allocator_plugin;
 
 /* disk layout plugin: this specifies super block, journal, bitmap (if there
