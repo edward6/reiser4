cc97dd1591512170e0dbc802db13ca0c77363409
namesys.com!god <namesys.com!god>
2002-05-15 15:00:33 +0000

    comments updated
    (Logical change 1.425)

--- kcond.c
+++ kcond.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001 by Hans Reiser, licensing governed by reiser4/README
+ * Copyright 2002 by Hans Reiser, licensing governed by reiser4/README
  */
 /*
  * Kernel condition variables implemenation.
@@ -156,3 +156,14 @@ int kcond_are_waiters( kcond_t *cvar /* cvar to query */ )
 	return cvar -> queue != NULL;
 }
 
+/*
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * scroll-step: 1
+ * End:
+ */
