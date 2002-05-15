a1611ce5ca012867c6cc4acc4c04623feaa01e91
namesys.com!god <namesys.com!god>
2002-05-15 15:01:09 +0000

    (Logical change 1.426)

---
+++ seal.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright 2002 by Hans Reiser, licensing governed by reiser4/README
+ */
+/*
+ * Seals implemenation.
+ */
+/*
+ * Seals are "weak" tree pointers. They are analogous to tree coords in
+ * allowing to bypass tree traversal. But normal usage of coords implies that
+ * node pointer to by coord is locked, whereas seals don't keep a lock (or
+ * even a reference) to znode. In stead, each znode contains a version number,
+ * increased on each znode modification. This version number is copied into
+ * seal when seal is created. Later, one can "validate" seal by calling
+ * seal_validate(). If znode is in cache and its version number is still the
+ * same, seal is "pristine" and coord associated with it can be re-used
+ * immediately.
+ *
+ * If, on the other hand, znode is out of cache, or it is obviously different
+ * one from the znode seal was initially attached to (for example, it is on
+ * the different level, or is being removed from the tree), seal is
+ * irreparably invalid ("burned") and tree traversal has to be repeated.
+ *
+ * Otherwise, there is some hope, that while znode was modified (and seal was
+ * "broken" as a result), key attached to the seal is still in the node. This
+ * is checked by first comparing this key with delimiting keys of node and, if
+ * key is ok, doing intra-node lookup.
+ *
+ *
+ */
+
+#include "reiser4.h"
+
+static int seal_is_set( const seal_t *seal );
+static znode *seal_node( const seal_t *seal );
+static int seal_matches( const seal_t *seal, znode *node );
+static int seal_search_node( seal_t *seal, tree_coord *coord, 
+			     znode *node, reiser4_key *key, lookup_bias bias,
+			     tree_level level );
+
+/** 
+ * initialise seal. This can be called several times on the same seal. @coord
+ * and @key can be NULL. 
+ */
+void seal_init( seal_t      *seal /* seal to initialise */, 
+		tree_coord  *coord /* coord @seal will be attached to */, 
+		reiser4_key *key  /* key @seal will be attached to */ )
+{
+	assert( "nikita-1886", seal != NULL );
+	xmemset( seal, 0, sizeof *seal );
+	if( coord != NULL ) {
+		znode *node;
+
+		node = coord -> node;
+		assert( "nikita-1879", node != NULL );
+		spin_lock_znode( node );
+		spin_lock_tree( current_tree );
+		seal -> epoch = current_tree -> znode_epoch;
+		assert( "nikita-1880", seal -> epoch != 0 );
+		spin_unlock_tree( current_tree );
+		seal -> version = node -> version;
+		seal -> block   = *znode_get_block( node );
+		if( REISER4_DEBUG ) {
+			seal -> coord = *coord;
+			if( key != NULL )
+				seal -> key = *key;
+		}
+		spin_unlock_znode( node );
+	}
+}
+
+/** finish with seal */
+void seal_done( seal_t *seal )
+{
+	assert( "nikita-1887", seal != NULL );
+	if( REISER4_DEBUG )
+		seal -> epoch = 0;
+}
+
+/**
+ * (re-)validate seal.
+ *
+ * Checks whether seal is pristine, and try to revalidate it if possible.
+ *
+ * If seal was burned, or broken irreparable, return -EAGAIN.
+ *
+ */
+int seal_validate( seal_t            *seal  /* seal to validate */, 
+		   tree_coord        *coord /* coord to validate against */, 
+		   reiser4_key       *key   /* key to validate against */, 
+		   tree_level         level /* level of node */,
+		   lock_handle       *lh    /* resulting lock handle */, 
+		   lookup_bias        bias  /* search bias */,
+		   znode_lock_mode    mode  /* lock node */,
+		   znode_lock_request request /* locking priority */ )
+{
+	znode *node;
+	int    result;
+
+	assert( "nikita-1889", seal != NULL );
+	assert( "nikita-1881", seal_is_set( seal ) );
+	assert( "nikita-1882", key != NULL );
+	assert( "nikita-1883", coord != NULL );
+	assert( "nikita-1884", lh != NULL );
+	assert( "nikita-1885", keyeq( &seal -> key, key ) );
+	assert( "nikita-1886", !memcmp( &seal -> coord, coord, sizeof *coord ) );
+
+	/* obtain znode by block number */
+	node = seal_node( seal );
+	if( node != NULL ) {
+		/* znode was in cache, lock it */
+		result = longterm_lock_znode( lh, node, mode, request );
+		zput( node );
+		if( result == 0 ) {
+			if( seal_matches( seal, node ) ) {
+				/* if seal version and znode version
+				 * coincide */
+				assert( "nikita-1887", 
+					node == seal -> coord.node );
+				result = 0;
+			} else if( znode_contains_key_lock( node, key ) )
+				/* 
+				 * seal is broken, but there still is a hope
+				 * that key is still in @node
+				 */
+				result = seal_search_node( seal, 
+							   coord, 
+							   node, 
+							   key, bias, level );
+			else
+				/* key is not in @node */
+				result = -EAGAIN;
+		}
+		if( result != 0 )
+			/* unlock node on failure */
+			done_lh( lh );
+	} else
+		/* znode wasn't in cache */
+		result = -EAGAIN;
+	return result;
+}
+
+/* helpers functions */
+
+/** true if seal was initialised */
+static int seal_is_set( const seal_t *seal /* seal to query */ )
+{
+	assert( "nikita-1890", seal != NULL );
+	return seal -> epoch != 0;
+}
+
+/** obtain reference to znode seal points to, if in cache */
+static znode *seal_node( const seal_t *seal /* seal to query */ )
+{
+	assert( "nikita-1891", seal != NULL );
+	return zlook( current_tree, &seal -> block );
+}
+
+/** true if @seal version and @node version coincide */
+static int seal_matches( const seal_t *seal /* seal to check */, 
+			 znode *node /* node to check */ )
+{
+	int result;
+
+	assert( "nikita-1890", seal != NULL );
+	assert( "nikita-1891", node != NULL );
+
+	spin_lock_znode( node );
+	spin_lock_tree( current_tree );
+	result = 
+		( seal -> version == node -> version ) &&
+		( seal -> epoch == current_tree -> znode_epoch );
+	spin_unlock_tree( current_tree );
+	spin_unlock_znode( node );
+	return result;
+}
+
+/** intranode search */
+static int seal_search_node( seal_t *seal /* seal to repair */, 
+			     tree_coord *coord /* coord attached to @seal */, 
+			     znode *node /* node to search in */, 
+			     reiser4_key *key /* key attached to @seal */, 
+			     lookup_bias bias /* search bias */,
+			     tree_level level /* node level */ )
+{
+	int result;
+
+	assert( "nikita-1888", seal != NULL );
+	assert( "nikita-1889", coord != NULL );
+	assert( "nikita-1892", node != NULL );
+	assert( "nikita-1893", znode_is_any_locked( node ) );
+
+	if( ( znode_get_level( node ) != level ) ||
+	    ZF_ISSET( node, ZNODE_HEARD_BANSHEE ) ||
+	    ZF_ISSET( node, ZNODE_IS_DYING ) )
+		return -EAGAIN;
+
+	result = zparse( node );
+	if( result != 0 )
+		return result;
+	
+	result = node_plugin_by_node( node ) -> lookup( node, key, 
+							bias, coord );
+	if( result == NS_FOUND ) {
+		/* renew seal */
+		seal_init( seal, coord, key );
+	} else
+		result = -EAGAIN;
+	return result;
+}
+
+/*
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * scroll-step: 1
+ * End:
+ */
---
+++ seal.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2002 by Hans Reiser, licensing governed by reiser4/README
+ */
+
+/*
+ * Declaration of seals: "weak" tree pointers. See seal.c for comments.
+ */
+
+#ifndef __SEAL_H__
+#define __SEAL_H__
+
+/** seal. See comment at the top of seal.c */
+typedef struct seal_s { 
+	/** epoch recorded at the time of seal creation */
+	__u32            epoch;
+	/** version of znode recorder at the time of seal creation */
+	__u32            version;
+	/** block number of znode attached to this seal */
+	reiser4_block_nr block;
+#if REISER4_DEBUG
+	/** coord this seal is attached to. For debugging. */
+	tree_coord       coord;
+	/** key this seal is attached to. For debugging. */
+	reiser4_key      key;
+#endif
+} seal_t;
+
+extern void seal_init( seal_t *seal, tree_coord *coord, reiser4_key *key );
+extern void seal_done( seal_t *seal );
+
+extern int seal_validate( seal_t            *seal, 
+			  tree_coord        *coord, 
+			  reiser4_key       *key, 
+			  tree_level         level,
+			  lock_handle       *lh, 
+			  lookup_bias        bias,
+			  znode_lock_mode    mode,
+			  znode_lock_request request );
+
+/* __SEAL_H__ */
+#endif
+
+/*
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * End:
+ */
