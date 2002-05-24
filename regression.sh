20c1df3f1668fc57146f2b32e5e187532b219313
namesys.com!god <namesys.com!god>
2002-05-24 11:35:03 +0000

    update regression.sh to work with persistent backing store
    (Logical change 1.481)

--- regression.sh
+++ regression.sh
@@ -14,22 +14,34 @@ rm -f gmon.out.*
 for r in `seq 1 $ROUNDS` 
 do
 echo Round $r
-./a.out nikita ibk 10000 || exit 1
-mv gmon.out gmon.out.ibk.10000.$r
+# "non-persistent" tests
+if [ x$$REISER4_UL_DURABLE_MMAP = x ]
+then
+	./a.out nikita ibk 10000 || exit 1
+	mv gmon.out gmon.out.ibk.10000.$r
+
+	./a.out jmacd build 3 1000 1000 || exit 4
+	mv gmon.out gmon.out.build.3.1000.$r
+fi
+
 ./a.out nikita dir 1 100 || exit 2
 mv gmon.out gmon.out.dir.1.100.$r
+
 ./a.out nikita dir 3 1000 || exit 3
 mv gmon.out gmon.out.dir.3.1000.$r
-./a.out jmacd build 3 1000 1000 || exit 4
-mv gmon.out gmon.out.build.3.1000.$r
+
 ./a.out nikita mongo 3 1000 || exit 5
 mv gmon.out gmon.out.mongo.3.1000.$r
+
 ./a.out nikita rm 3 1000 1000 || exit 6
 mv gmon.out gmon.out.rm.3.1000.$r
+
 ./a.out nikita unlink 10000 || exit 7
 mv gmon.out gmon.out.unlink.10000.$r
-./a.out nikita queue 30 10000 100000  || exit 6
-mv gmon.out gmon.out.rm.30.1000.$r
+
+./a.out nikita queue 30 10000 10000  || exit 6
+mv gmon.out gmon.out.rm.30.10000.10000.$r
+
 ./a.out nikita mongo 30 1000 || exit 6
 mv gmon.out gmon.out.rm.30.1000.$r
 #( find /tmp | ./a.out vs copydir ) || exit 8
