b5dc01f0441b522e2f44d0d6134bbff861f4a923
namesys.com!vs <namesys.com!vs>
2002-05-30 08:52:16 +0000

    bitmap_init_allocator updated to match changed prototype
    (Logical change 1.519)

--- plugin/space/bitmap.h
+++ plugin/space/bitmap.h
@@ -18,7 +18,7 @@ typedef struct {
 
 /* declarations of functions implementing methods of space allocator plugin for
  * bitmap based allocator. The functions theirself are in bitmap.c */
-int bitmap_init_allocator (reiser4_space_allocator *, struct super_block *);
+int bitmap_init_allocator (reiser4_space_allocator *, struct super_block *, void *);
 int bitmap_destroy_allocator (reiser4_space_allocator *, struct super_block *);
 int bitmap_alloc_blocks ( reiser4_blocknr_hint *, int needed,
 			 reiser4_block_nr *start, reiser4_block_nr *len);
