0053a5a6a9797ca08d2f05881c5514054256d97a
namesys.com!umka <namesys.com!umka>
2002-05-31 16:11:37 +0000

    (Logical change 1.556)

---
+++ reiser4lib/acconfig.h
@@ -0,0 +1,24 @@
+/* Define this to be the name of the package */
+#undef PACKAGE
+
+/* Define to the version of the package */
+#undef VERSION
+
+#undef LIBREISERFS_MAX_INTERFACE_VERSION
+#undef LIBREISERFS_MIN_INTERFACE_VERSION
+
+#undef loff_t
+
+#undef ENABLE_NLS
+
+#undef HAVE_CATGETS
+
+#undef HAVE_GETTEXT
+
+#undef HAVE_LC_MESSAGES
+
+#undef HAVE_STPCPY
+
+#undef DEBUG
+
+#undef HAVE_UUID
