29b12f4acf832b650f88e3a7547a3f42b243c469
namesys.com!umka <namesys.com!umka>
2002-06-03 09:26:25 +0000

    Added tools.c and tools.h
    2002/06/03 13:16:04+04:00 namesys.com!umka
    
    (Logical change 1.567)

---
+++ reiser4lib/libreiserfs/tools.c
@@ -0,0 +1,15 @@
+/*
+	tools.c -- tool functions which are needed by libreiserfs
+	Copyright (C) 1996 - 2002 Hans Reiser
+*/
+
+int reiserfs_tools_log2(int n) {
+	int x;
+	for (x = 0; 1 << x <= n; x++);
+		return x - 1;
+}
+
+int reiserfs_tools_power_of_two(unsigned long n) {
+	return (n & -n) == n;
+}
+
