918199f2b96c20bc7fc5560a643d720a7681e045
namesys.com!umka <namesys.com!umka>
2002-06-03 12:21:27 +0000

    Plugin-working functions added
    (Logical change 1.570)

--- reiser4lib/include/reiserfs/plugin.h
+++ reiser4lib/include/reiserfs/plugin.h
@@ -3,3 +3,142 @@
 	Copyright (C) 1996 - 2002 Hans Reiser
 */
 
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+enum reiserfs_plugin_type {
+	REISERFS_FILE_PLUGIN,
+	REISERFS_DIR_PLUGIN,
+	REISERFS_ITEM_PLUGIN,
+	REISERFS_NODE_PLUGIN,
+	REISERFS_HASH_PLUGIN,
+	REISERFS_TAIL_PLUGIN,
+	REISERFS_HOOK_PLUGIN,
+	REISERFS_PERM_PLUGIN,
+	REISERFS_SD_EXT_PLUGIN,
+	REISERFS_LAYOUT_PLUGIN,
+	REISERFS_OID_PLUGIN,
+	REISERFS_ALLOC_PLUGIN
+};
+
+typedef enum reiserfs_plugin_type reiserfs_plugin_type_t;
+typedef int reiserfs_plugin_id_t;
+
+#define REISERFS_PLUGIN_MAX_LABEL	255
+#define REISERFS_PLUGIN_MAX_DESC	4096
+
+struct reiserfs_plugin_header {
+	void *handle;
+	reiserfs_plugin_id_t id;
+	reiserfs_plugin_type_t type;
+	const char label[REISERFS_PLUGIN_MAX_LABEL];
+	const char desc[REISERFS_PLUGIN_MAX_DESC];
+	int nlink;
+};
+
+typedef struct reiserfs_plugin_header reiserfs_plugin_header_t;
+
+struct reiserfs_file_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_file_plugin reiserfs_file_plugin_t;
+
+struct reiserfs_dir_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_dir_plugin reiserfs_dir_plugin_t;
+
+struct reiserfs_item_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_item_plugin reiserfs_item_plugin_t;
+
+struct reiserfs_node_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_node_plugin reiserfs_node_plugin_t;
+
+struct reiserfs_hash_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_hash_plugin reiserfs_hash_plugin_t;
+
+struct reiserfs_tail_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_tail_plugin reiserfs_tail_plugin_t;
+
+struct reiserfs_hook_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_hook_plugin reiserfs_hook_plugin_t;
+
+struct reiserfs_perm_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_perm_plugin reiserfs_perm_plugin_t;
+
+struct reiserfs_layout_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_layout_plugin reiserfs_layout_plugin_t;
+
+struct reiserfs_oid_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_oid_plugin reiserfs_oid_plugin_t;
+
+struct reiserfs_alloc_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_alloc_plugin reiserfs_alloc_plugin_t;
+
+struct reiserfs_journal_plugin {
+	reiserfs_plugin_header_t h;
+};
+
+typedef struct reiserfs_journal_plugin reiserfs_journal_plugin_t;
+
+union reiserfs_plugin {
+	reiserfs_plugin_header_t h;
+	
+	reiserfs_file_plugin_t file;
+	reiserfs_dir_plugin_t dir;
+	reiserfs_item_plugin_t item;
+	reiserfs_node_plugin_t node;
+	reiserfs_hash_plugin_t hash;
+	reiserfs_tail_plugin_t tail;
+	reiserfs_hook_plugin_t hook;
+	reiserfs_perm_plugin_t perm;
+	reiserfs_layout_plugin_t layout;
+	reiserfs_oid_plugin_t oid;
+	reiserfs_alloc_plugin_t alloc;
+	reiserfs_journal_plugin_t journal;
+};
+
+typedef union reiserfs_plugin reiserfs_plugin_t;
+
+extern reiserfs_plugin_t *reiserfs_plugin_load_by_name(const char *name, 
+	const char *point);
+
+extern int reiserfs_plugin_find_by_cords(reiserfs_plugin_type_t type, 
+	reiserfs_plugin_id_t id, char *name);
+
+extern reiserfs_plugin_t *reiserfs_plugin_load(reiserfs_plugin_type_t type, 
+	reiserfs_plugin_id_t id);
+
+extern void reiserfs_plugin_unload(reiserfs_plugin_t *plugin);
+
+#endif
+
--- reiser4lib/libreiserfs/plugin.c
+++ reiser4lib/libreiserfs/plugin.c
@@ -3,4 +3,103 @@
 	Copyright (C) 1996 - 2002 Hans Reiser
 */
 
+#include <dlfcn.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+
+#include <reiserfs/plugin.h>
+
+reiserfs_plugin_t *reiserfs_plugin_load_by_name(const char *name, const char *point) {
+	char *error;
+	void *handle, *entry;
+	reiserfs_plugin_t *plugin;
+	reiserfs_plugin_t *(get_plugin) (void);
+
+	ASSERT(name != NULL, return NULL);
+	ASSERT(point != NULL, return NULL);
+
+	if (!(handle = dlopen(name, RTLD_NOW))) {
+		reiserfs_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-0001", 
+			_("Can't load plugin library %s. Error: %s."), name, strerror(errno));
+		return NULL;
+	}
+
+	entry = dlsym(handle, point);
+	if ((error = dlerror()) != NULL) {
+		reiserfs_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-0002", 
+			_("Can't find symbol %s in plugin %s. Error: %s."), point, name, error);
+		goto error_free_handle;
+	}
+	
+	get_plugin = (reiserfs_plugin_t *(*)(void))entry;
+	plugin = get_plugin();
+
+	plugin->h.handle = handle;
+	plugin->h.nlink = 0;
+
+	return plugin;
+	
+error_free_handle:
+	dlclose(handle);
+error:
+	return NULL;
+}
+
+static reiserfs_plugin_t *reiserfs_plugin_from_cashe(reiserfs_plugin_type_t type, 
+	reiserfs_plugin_id_t id) 
+{
+	reiserfs_plugin_t *plugin;
+	
+	/* Getting plugin from plugin cashe */
+	
+	plugin->h.nlink++;
+	return NULL;
+}
+
+static void reiserfs_plugin_to_cashe(reiserfs_plugin_t plugin) {
+	plugin->h.nlink--;
+	if (!plugin->h.nlink) {
+		/* Delete specified plugin from plugin cashe */
+	}
+}
+
+int reiserfs_plugin_find_by_cords(reiserfs_plugin_type_t type, 
+	reiserfs_plugin_id_t id, char *name) 
+{
+	ASSERT(name != NULL, return 0);
+	return 1;
+}
+
+reiserfs_plugin_t *reiserfs_plugin_load(reiserfs_plugin_type_t type, reiserfs_plugin_id_t id) {
+	char name[PATH_MAX];
+	reiserfs_plugin_t *plugin;
+		
+	/* Looking up code for plugin in plugin map must be here */
+	if (!(plugin = reiserfs_plugin_from_cashe(type, id)))
+		return plugin;
+	
+	/* Loading plugin */
+	memset(name, 0, sizeof(name));
+	if (!reiserfs_plugin_find_by_cords(type, id, name)) {
+		reiserfs_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-0003", 
+			_("Can't find plugin by its type=%d and id=%d."), (int)type, (int)id);
+		return NULL;
+	}
+
+	if (!(plugin = reiserfs_plugin_load_by_name(name))) {
+		reiserfs_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-0004", 
+			_("Can't load plugin by name %s."), name);
+		return NULL;
+	}
+	
+	return plugin;
+}
+
+void reiserfs_plugin_unload(reiserfs_plugin_t *plugin) {
+	ASSERT(plugin != NULL, return);
+	reiserfs_plugin_to_cashe(plugin);
+	if (!plugin->h.nlink)
+		dlclose(plugin);
+}
 
