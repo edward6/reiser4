28e84f19ef8f91ba26c6c44b63efaa26ba88fcc1
namesys.com!vs <namesys.com!vs>
2002-06-04 09:41:02 +0000

    test tail plugin changed
    (Logical change 1.581)

--- plugin/tail_policy.c
+++ plugin/tail_policy.c
@@ -37,7 +37,7 @@ static int test_tail( const struct inode *inode UNUSED_ARG /* inode to operate
 							    * on */,
 		      loff_t size /* new object size */ )
 {
-	if (size > PAGE_SIZE * 8)
+	if (size > PAGE_SIZE * 4)
 		return 0;
 	return 1;
 }
