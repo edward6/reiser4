735ae22d196cad9a91443865b7a95b67871d24d2
namesys.com!umka <namesys.com!umka>
2002-06-06 10:02:16 +0000

    .
    2002/06/06 13:37:32+04:00 namesys.com!umka
    Rename: reiser4lib/include/reiserfs/list.h -> reiser4lib/include/aal/list.h
    
    (Logical change 1.624)

---
+++ reiser4lib/include/aal/list.h
@@ -0,0 +1,35 @@
+/*
+	list.h -- simple list implementation
+	Copyright (C) 1996 - 2002 Hans Reiser
+*/
+
+#ifndef LIST_H
+#define LIST_H
+
+struct aal_list {
+	void **body;
+	int size, count, inc;
+};
+
+typedef struct aal_list aal_list_t;
+
+extern aal_list_t *aal_list_create(int inc);
+extern void aal_list_free(aal_list_t *list);
+
+extern int aal_list_expand(aal_list_t *list);
+extern int aal_list_shrink(aal_list_t *list);
+
+extern void *aal_list_at(aal_list_t *list, int pos);
+extern int aal_list_pos(aal_list_t *list, void *item);
+
+extern int aal_list_insert(aal_list_t *list, void *item, int pos);
+extern int aal_list_delete(aal_list_t *list, int pos);
+
+extern int aal_list_add(aal_list_t *list, void *item);
+extern int aal_list_remove(aal_list_t *list, void *item);
+
+extern void *aal_list_run(aal_list_t *list, int (*item_func)(void *, void *), void *data);
+extern int aal_list_count(aal_list_t *list);
+
+#endif
+
