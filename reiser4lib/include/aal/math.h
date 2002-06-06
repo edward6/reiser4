611b35cee206d30d5db384a7dcad39650f6f5942
namesys.com!umka <namesys.com!umka>
2002-06-06 10:02:16 +0000

    Rename: reiser4lib/include/aal/math.c -> reiser4lib/include/aal/math.h
    (Logical change 1.624)

---
+++ reiser4lib/include/aal/math.h
@@ -0,0 +1,13 @@
+/*
+	math.h -- some math functions which are needed by liaal.
+	Copyright (C) 1996-2002 Hans Reiser.
+*/
+
+#ifndef MATH_H
+#define MATH_H
+
+extern int aal_pow_of_two(unsigned long n);
+extern int aal_log2(int n);
+
+#endif
+
