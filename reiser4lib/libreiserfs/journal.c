d065f301ef84d8db0dab767006cf76cd4924fe26
namesys.com!umka <namesys.com!umka>
2002-06-07 09:47:23 +0000

    .
    2002/06/07 12:42:49+04:00 namesys.com!umka
    
    (Logical change 1.643)

---
+++ reiser4lib/libreiserfs/journal.c
@@ -0,0 +1,74 @@
+/*
+	journal.c -- reiserfs filesystem journal common code.
+	Copyright (C) 1996-2002 Hans Reiser.
+*/
+
+#include <aal/aal.h>
+#include <reiserfs/reiserfs.h>
+#include <reiserfs/debug.h>
+
+int reiserfs_journal_open(reiserfs_fs_t *fs, aal_device_t *device, int replay) {
+	reiserfs_plugin_id_t id;
+	reiserfs_plugin_t *plugin;
+	
+	ASSERT(fs != NULL, return 0);
+	ASSERT(fs->super != NULL, return 0);
+	ASSERT(device != NULL, return 0);
+	
+	if (fs->journal) {
+		aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-011", 
+			"Journal already initialized.");
+		return 0;
+	}
+	
+	if (!(fs->journal = aal_calloc(sizeof(*fs->journal), 0)))
+		return 0;
+	
+	fs->journal->device = device;
+	id = fs->super->plugin->layout.journal_plugin_id(fs->super->entity);
+
+	if (!(plugin = reiserfs_plugin_find(REISERFS_JOURNAL_PLUGIN, id))) {
+		aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-011", 
+			"Can't find journal plugin by its identifier %d.", id);
+		goto error_free_journal;
+	}
+	
+	fs->journal->plugin = plugin;
+	
+	if ((fs->journal->entity = plugin->journal.init(device))) {
+		aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-012", 
+			"Can't initialize the journal plugin.");
+		goto error_free_journal;
+	}
+	
+	/* Optional replaying the journal */
+	if (replay && !plugin->journal.replay(fs->journal->entity)) {
+		aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, "umka-013", 
+			"Can't replay the journal.");
+		goto error_free_entity;
+	}
+
+	return 1;
+
+error_free_entity:
+	plugin->journal.done(fs->journal->entity);
+	fs->journal->entity = NULL;
+error_free_journal:
+	aal_free(fs->journal);
+	fs->journal = NULL;
+error:
+	return 0;
+}
+
+int reiserfs_journal_reopen(reiserfs_fs_t *fs, aal_device_t *device, int replay) {
+	reiserfs_journal_close(fs);
+	return reiserfs_journal_open(fs, device, replay);
+}
+
+void reiserfs_journal_close(reiserfs_fs_t *fs) {
+	fs->journal->plugin->journal.done(fs->journal->entity);
+	fs->journal->entity = NULL;
+	aal_free(fs->journal);
+	fs->journal = NULL;
+}
+
