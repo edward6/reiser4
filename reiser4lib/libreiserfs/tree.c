cd7c762c7eb41a0ae927c53212da0e3acf258a12
namesys.com!umka <namesys.com!umka>
2002-06-07 11:32:36 +0000

    .
    2002/06/07 14:01:29+04:00 namesys.com!umka
    
    (Logical change 1.650)

---
+++ reiser4lib/libreiserfs/tree.c
@@ -0,0 +1,23 @@
+/*
+	tree.c -- reiserfs balanced tree code.
+	Copyright (C) 1996-2002 Hans Reiser.
+*/
+
+#include <reiserfs/reiserfs.h>
+#include <reiserfs/debug.h>
+
+int reiserfs_tree_open(reiserfs_fs_t *fs) {
+	
+	if (!(fs->tree = aal_calloc(sizeof(*fs->tree), 0)))
+		return 0;
+	
+	return 1;
+}
+
+void reiserfs_tree_close(reiserfs_fs_t *fs) {
+	ASSERT(fs != NULL, return);
+	
+	aal_free(fs->tree);
+	fs->tree = NULL;
+}
+
