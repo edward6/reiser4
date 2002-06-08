ee9663bdd335cf57b578df85336f07ee48056083
namesys.com!umka <namesys.com!umka>
2002-06-08 12:43:40 +0000

    A lot of fixes. Space allocation plugins were added
    2002/06/08 15:47:38+04:00 namesys.com!umka
    
    (Logical change 1.678)

---
+++ reiser4lib/plugin/alloc36/alloc36.c
@@ -0,0 +1,56 @@
+/*
+	alloc36.c -- Space allocator plugin for reiserfs 3.6.x
+	Copyright (C) 1996-2002 Hans Reiser
+*/
+
+#include <aal/aal.h>
+#include <reiserfs/reiserfs.h>
+
+#include "alloc36.h"
+
+static reiserfs_alloc36_t *reiserfs_alloc36_init(aal_device_t *device) {
+	reiserfs_alloc36_t *alloc;
+	
+	if (!device)
+		return NULL;
+	
+	if (!(alloc = aal_calloc(sizeof(*alloc), 0)))
+		return NULL;
+	
+	/* Allocator initialization must be here */
+	
+	alloc->device = device;
+	return alloc;
+
+error_free_format:
+	aal_free(alloc);
+error:
+	return NULL;
+}
+
+static void reiserfs_alloc36_done(reiserfs_alloc36_t *alloc, int sync) {
+	if (sync) {
+		/* Synchronizing code must be here */
+	}
+	aal_free(alloc);
+}
+
+reiserfs_plugin_t plugin_info = {
+	.alloc = {
+		.h = {
+			.handle = NULL,
+			.id = 0x2,
+			.type = REISERFS_ALLOC_PLUGIN,
+			.label = "alloc36",
+			.desc = "Space allocator for reiserfs 3.6.x, ver. 0.1, "
+				"Copyright (C) 1996-2002 Hans Reiser",
+		},
+		.init = (reiserfs_alloc_opaque_t *(*)(aal_device_t *))reiserfs_alloc36_init,
+		.done = (void (*)(reiserfs_alloc_opaque_t *, int))reiserfs_alloc36_done
+	}
+};
+
+reiserfs_plugin_t *reiserfs_plugin_info() {
+	return &plugin_info;
+}
+
