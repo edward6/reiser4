3b574513a47c2de63ca51df06f5373faede0197e
namesys.com!vitaly <namesys.com!vitaly>
2002-06-10 14:43:19 +0000

    (Logical change 1.714)

--- reiser4lib/include/reiserfs/node.h
+++ reiser4lib/include/reiserfs/node.h
@@ -1,28 +1,22 @@
 /*
-    node.h -- reiserfs formated node functions and common structures.
+    journal.h -- reiserfs node functions.
     Copyright (C) 1996-2002 Hans Reiser.
     Author Vitaly Fertman.
-*/ 
+ */ 
 
 #ifndef NODE_H
 #define NODE_H
 
 #include <aal/aal.h>
 
-struct reiserfs_node_header {
-    uint16_t plugin_id; 
+struct reiserfs_node_common_header {
+    uint16_t node_plugin_id; 
 };
 
-typedef struct reiserfs_node_header reiserfs_node_header_t;
-
-#define REISERFS_NODE_LEAF		1
-#define REISERFS_NODE_INTERNAL		2
-
-#define get_nh_plugin_id(nh)		get_le16(nh, plugin_id)
-#define set_nh_plugin_id(nh, val)	set_le16(nh, plugin_id, val)
+typedef struct reiserfs_node_common_header reiserfs_node_common_header_t;
 
 struct reiserfs_node {
-    reiserfs_node_header_t *header;
+    reiserfs_node_common_header_t *header;
     reiserfs_node_opaque_t *entity; 
     reiserfs_plugin_t *plugin;
 };
@@ -30,9 +24,18 @@ struct reiserfs_node {
 typedef struct reiserfs_node reiserfs_node_t;
 
 extern reiserfs_node_t *reiserfs_node_open(aal_block_t *block);
-extern reiserfs_node_t *reiserfs_node_create(aal_block_t *block);
+extern reiserfs_node_t *reiserfs_node_create(aal_block_t *block, uint32_t level);
 extern int reiserfs_node_check(reiserfs_node_t *node, int flags);
 extern void reiserfs_node_close(reiserfs_node_t *node, int sync);
+    
+extern uint32_t reiserfs_max_item_size (void);
+extern uint32_t reiserfs_max_item_num  (void);
+extern uint32_t reiserfs_count (const reiserfs_node_t *);
+extern uint32_t reiserfs_level (const reiserfs_node_t *);
+extern uint32_t reiserfs_get_free_space (const reiserfs_node_t *);
+extern void     reiserfs_set_free_space (const reiserfs_node_t *);
+     
+void (*print) (const reiserfs_node_t * node);
 
 #endif
 
--- reiser4lib/plugin/node40/node40.h
+++ reiser4lib/plugin/node40/node40.h
@@ -1,7 +1,7 @@
 /*
     node40.h -- reiser4 default node plugin.
     Copyright (C) 1996-2002 Hans Reiser.
-    Author Vitaly Fertman.
+    Author: Vitaly Fertman.
 */
 
 #ifndef NODE40_H
@@ -9,27 +9,38 @@
 
 #include <aal/aal.h>
 
-typedef uint16_t node40_size_t;
+/* format of node header for 40 node layouts. */
+typedef struct node40_header {
+    reiserfs_node_header_t header;	/* node header common for all node plugins */
+    size_t free_space;			/* free space of node in bytes */
+    size_t free_space_start;		/* start position of free space in node in bytes */
+    uint8_t  level;                     /* 1 is leaf level, 2 is twig level, etc */
+    uint32_t magic;                     /* ?? */
+    uint16_t num_items;                 /* number of items */
+/*    char     flags;	*/
+    uint32_t flush_time;		/* flush time, used for solving conflicts in 
+					   reiser4fsck */
+} node40_header;
 
-/* Format of node header for 40 node layouts. */
-struct reiserfs_node40_header {
-    reiserfs_node_header_t header;
-    node40_size_t free_space; 
-    node40_size_t free_space_start;
-    uint8_t  level;
-    uint32_t magic;
-    uint16_t num_items;
-    uint32_t flush_time;
-};
+typedef struct node40_header node40_header_t;  
 
-typedef struct reiserfs_node40_header reiserfs_node40_header_t;
-
+/* node object which plugin works with */
 struct reiserfs_node40 {
     aal_device_t *block;
-    aal_block_t *header;
+    aal_block_t  *header;
 };
 
 typedef struct reiserfs_node40 reiserfs_node40_t;
+
+/* item headers are not standard across all node layouts, pass
+ * pos_in_node to functions instead */
+typedef struct item_header_40 {
+    reiser4_key key;	    
+    node40_size_t offset;   /* offset from start of a node */
+    uint16_t length;
+    uint16_t plugin_id;
+    /* 2 bytes are lost here */
+} item_header_40;
 
 #endif
 
