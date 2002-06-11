3294426aa62b6f6efe4f8391eb85e0f380fd56cb
namesys.com!zam <namesys.com!zam>
2002-06-11 15:57:49 +0000

    (Logical change 1.742)

--- plugin/space/bitmap.h
+++ plugin/space/bitmap.h
@@ -9,15 +9,21 @@
 
 /* declarations of functions implementing methods of space allocator plugin for
  * bitmap based allocator. The functions theirself are in bitmap.c */
-int bitmap_init_allocator    (reiser4_space_allocator *, struct super_block *,
+extern int bitmap_init_allocator    (reiser4_space_allocator *, struct super_block *,
 			       void *);
-int bitmap_destroy_allocator (reiser4_space_allocator *, struct super_block *);
-int bitmap_alloc_blocks      (reiser4_space_allocator *,
+extern int bitmap_destroy_allocator (reiser4_space_allocator *, struct super_block *);
+extern int bitmap_alloc_blocks      (reiser4_space_allocator *,
 			       reiser4_blocknr_hint *, int needed,
 			       reiser4_block_nr *start, reiser4_block_nr *len);
 
-void bitmap_pre_commit_hook (void);
-void bitmap_post_commit_hook (void);
-void bitmap_post_write_back_hook (void);
+extern void bitmap_pre_commit_hook (void);
+extern void bitmap_post_commit_hook (void);
+extern void bitmap_post_write_back_hook (void);
+
+typedef __u64 bmap_nr_t;
+typedef __u32 bmap_off_t;
+
+/* exported for user-level simulator */
+extern void get_bitmap_blocknr (struct super_block *, bmap_nr_t, reiser4_block_nr *);
 
 #endif /* __REISER4_PLUGIN_SPACE_BITMAP_H__ */
