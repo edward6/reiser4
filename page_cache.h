3d1a2445ce7510ac0043ea1bc1af9b9dcc139af4
namesys.com!god <namesys.com!god>
2002-06-11 11:48:08 +0000

    Rename: memory.h -> page_cache.h
    (Logical change 1.728)

---
+++ page_cache.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2001, 2002 by Hans Reiser, licensing governed by reiser4/README
+ */
+/*
+ * Memory pressure hooks. Fake inodes handling. See memory.c.
+ */
+
+#if !defined( __REISER4_MEMORY_H__ )
+#define __REISER4_MEMORY_H__
+
+extern int init_fakes( void );
+extern int init_formatted_fake( struct super_block *super );
+extern int read_in_formatted( struct super_block *super, 
+			      sector_t block, char **data );
+
+#if REISER4_DEBUG
+extern void *xmemcpy( void *dest, const void *src, size_t n );
+extern void *xmemmove( void *dest, const void *src, size_t n );
+extern void *xmemset( void *s, int c, size_t n );
+#else
+#define xmemcpy( d, s, n ) memcpy( ( d ), ( s ), ( n ) )
+#define xmemmove( d, s, n ) memmove( ( d ), ( s ), ( n ) )
+#define xmemset( s, c, n ) memset( ( s ), ( c ), ( n ) )
+#endif
+
+/* __REISER4_MEMORY_H__ */
+#endif
+
+/*
+ * Make Linus happy.
+ * Local variables:
+ * c-indentation-style: "K&R"
+ * mode-name: "LC"
+ * c-basic-offset: 8
+ * tab-width: 8
+ * fill-column: 120
+ * scroll-step: 1
+ * End:
+ */
