ab5fd7b94ebab49683eb94113a8b2d9437ee1da8
namesys.com!vs <namesys.com!vs>
2002-06-12 13:17:36 +0000

    cleanups
    2002/06/10 19:21:22+04:00 namesys.com!vs
    Auto merged
    
    2002/06/10 11:12:11+04:00 namesys.com!vs
    cleanup
    
    2002/06/05 13:56:22+04:00 namesys.com!vs
    compiling in kernel
    
    (Logical change 1.764)

--- plugin/file/file.h
+++ plugin/file/file.h
@@ -11,14 +11,27 @@
 #define __REISER4_FILE_H__
 
 
+void get_exclusive_access      (struct inode * inode);
+void drop_exclusive_access     (struct inode * inode);
+void get_nonexclusive_access   (struct inode * inode);
+void drop_nonexclusive_access  (struct inode * inode);
+int  tail2extent               (struct inode * inode);
+int  extent2tail               (struct file * file);
+int  unix_file_readpage_nolock (struct file * file, struct page * page);
+int  find_item                 (reiser4_key * key, new_coord * coord,
+				lock_handle * lh, znode_lock_mode lock_mode);
+
+
+
 /* plugin->file.* */
 int     unix_file_create   (struct inode *object, struct inode *parent,
 				 reiser4_object_create_data *data );
-ssize_t unix_file_write    (struct file * file, char * buf, size_t size,
+ssize_t unix_file_write    (struct file * file, const char * buf, size_t size,
 				loff_t *off);
 ssize_t unix_file_read     (struct file * file, char * buf, size_t size,
 				loff_t * off);
 int     unix_file_release  (struct file * file);
+int     unix_file_mmap     (struct file * file, struct vm_area_struct * vma);
 int     unix_file_truncate (struct inode * inode, loff_t size);
 int     unix_file_key_by_inode ( struct inode *, loff_t off, reiser4_key * );
 int     unix_file_create   (struct inode * object, struct inode * parent,
