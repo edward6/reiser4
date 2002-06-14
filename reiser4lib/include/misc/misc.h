591d23b9dbb1262e7b23f7727c569fcfe01ebc61
namesys.com!vitaly <namesys.com!vitaly>
2002-06-14 13:31:55 +0000

    2002/06/14 16:15:29+04:00 namesys.com!vitaly
    (Logical change 1.808)

---
+++ reiser4lib/include/misc/misc.h
@@ -0,0 +1,20 @@
+/*
+    misc.h -- miscellaneous useful code.
+    Copyright (C) 1996 - 2002 Hans Reiser
+    Author Vitaly Fertman.
+*/
+
+#ifndef MISC_H
+#define MISC_H
+
+#include <stdint.h>
+
+typedef void *(get_element_to_comp_t)(void *, int64_t);
+typedef int (comp_function_t)(void *, void *);
+
+extern int reiserfs_bin_search (void * find_it, int64_t * ppos, 
+    uint32_t count, void *entity, get_element_to_comp_t get_elem, 
+    comp_function_t comp_func);
+extern int reiserfs_comp_keys (void *key1, void *key2);
+
+#endif
