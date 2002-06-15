5f5aeb356f0982422fcb231f1c7390ba8548716a
namesys.com!green <namesys.com!green>
2002-06-15 11:48:59 +0000

    Performed audit.
    (Logical change 1.822)

--- new_coord.c
+++ new_coord.c
@@ -5,6 +5,7 @@
 #include "reiser4.h"
 
 /* Internal constructor. */
+/* Audited by: green(2002.06.15) */
 static inline void ncoord_init_values (new_coord  *coord,
 				      znode       *node,
 				      pos_in_node  item_pos,
@@ -18,6 +19,7 @@ static inline void ncoord_init_values (new_coord  *coord,
 }
 
 /* after shifting coord previously set properly may become invalid. */
+/* Audited by: green(2002.06.15) */
 void ncoord_normalize (new_coord * coord)
 {
 	znode * node;
@@ -45,6 +47,7 @@ void ncoord_normalize (new_coord * coord)
 }
 
 /* Copy a coordinate. */
+/* Audited by: green(2002.06.15) */
 void ncoord_dup (new_coord *coord, const new_coord *old_coord)
 {
 	assert ("jmacd-9800", ncoord_check (old_coord));
@@ -53,6 +56,7 @@ void ncoord_dup (new_coord *coord, const new_coord *old_coord)
 
 /* Copy a coordinate without check. Useful when old_coord->node is not
  * loaded. As in cbk_tree_lookup -> connect_znode -> connect_one_side */
+/* Audited by: green(2002.06.15) */
 void ncoord_dup_nocheck (new_coord *coord, const new_coord *old_coord)
 {
 	coord->node     = old_coord->node;
@@ -63,6 +67,7 @@ void ncoord_dup_nocheck (new_coord *coord, const new_coord *old_coord)
 
 
 /* Initialize an invalid coordinate. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_invalid (new_coord *coord, znode *node)
 {
 	ncoord_init_values (coord, node, 0, 0, INVALID_COORD);
@@ -70,6 +75,7 @@ void ncoord_init_invalid (new_coord *coord, znode *node)
 
 /* Initialize a coordinate to point at the first unit of the first item.  If the node is
  * empty, it is positioned at the EMPTY_NODE. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_first_unit (new_coord *coord, znode *node)
 {
 	int is_empty = node_is_empty (node);
@@ -81,6 +87,7 @@ void ncoord_init_first_unit (new_coord *coord, znode *node)
 
 /* Initialize a coordinate to point at the last unit of the last item.  If the node is
  * empty, it is positioned at the EMPTY_NODE. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_last_unit  (new_coord *coord, znode *node)
 {
 	int is_empty = node_is_empty (node);
@@ -95,6 +102,7 @@ void ncoord_init_last_unit  (new_coord *coord, znode *node)
 
 /* Initialize a coordinate to before the first item.  If the node is empty, it is
  * positioned at the EMPTY_NODE. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_before_first_item (new_coord *coord, znode *node)
 {
 	int is_empty = node_is_empty (node);
@@ -106,6 +114,7 @@ void ncoord_init_before_first_item (new_coord *coord, znode *node)
 
 /* Initialize a coordinate to after the last item.  If the node is empty, it is positioned
  * at the EMPTY_NODE. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_after_last_item (new_coord *coord, znode *node)
 {
 	int is_empty = node_is_empty (node);
@@ -117,6 +126,7 @@ void ncoord_init_after_last_item (new_coord *coord, znode *node)
 
 /* Initialize a parent hint pointer. (parent hint pointer is a field in znode,
  * look for comments there) */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_parent_hint (new_coord *coord, znode *node)
 {
 	coord->node = node;
@@ -125,6 +135,7 @@ void ncoord_init_parent_hint (new_coord *coord, znode *node)
 
 /* Initialize a coordinate by 0s. Used in places where init_coord was used and
  * it was not clear how actually */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_zero (new_coord *coord)
 {
 	memset (coord, 0, sizeof (*coord));
@@ -132,6 +143,7 @@ void ncoord_init_zero (new_coord *coord)
 
 
 /* Return the number of items at the present node.  Asserts coord->node != NULL. */
+/* Audited by: green(2002.06.15) */
 unsigned ncoord_num_items (const new_coord * coord)
 {
 	assert ("jmacd-9805", coord->node != NULL);
@@ -140,6 +152,7 @@ unsigned ncoord_num_items (const new_coord * coord)
 }
 
 /* Return the number of units at the present item.  Asserts ncoord_is_existing_item(). */
+/* Audited by: green(2002.06.15) */
 unsigned ncoord_num_units (const new_coord * coord)
 {
 	assert ("jmacd-9806", ncoord_is_existing_item (coord));
@@ -148,12 +161,14 @@ unsigned ncoord_num_units (const new_coord * coord)
 }
 
 /* Return the last valid unit number at the present item (i.e., ncoord_num_units() - 1). */
+/* Audited by: green(2002.06.15) */
 unsigned ncoord_last_unit_pos (const new_coord * coord)
 {
 	return ncoord_num_units (coord) - 1;
 }
 
 /* Returns the current item positions.  Asserts non-empty. */
+/* Audited by: green(2002.06.15) */
 unsigned ncoord_item_pos (const new_coord *coord)
 {
 	assert ("jmacd-5512", coord->between != EMPTY_NODE && coord->between != INVALID_COORD);
@@ -162,6 +177,7 @@ unsigned ncoord_item_pos (const new_coord *coord)
 }
 
 /* Returns true if the coord was initializewd by ncoord_init_invalid (). */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_invalid (const new_coord *coord)
 {
 	return coord->between == INVALID_COORD;
@@ -170,6 +186,7 @@ int ncoord_is_invalid (const new_coord *coord)
 /* Returns true if the coordinate is positioned at an existing item, not before or after
  * an item.  It may be placed at, before, or after any unit within the item, whether
  * existing or not. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_existing_item (const new_coord * coord)
 {
 	switch (coord->between) {
@@ -191,6 +208,7 @@ int ncoord_is_existing_item (const new_coord * coord)
 
 /* Returns true if the coordinate is positioned at an existing unit, not before or after a
  * unit. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_existing_unit (const new_coord *coord)
 {
 	switch (coord->between) {
@@ -213,6 +231,7 @@ int ncoord_is_existing_unit (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned at the first unit of the first item.  Not
  * true for empty nodes nor coordinates positioned before the first item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_leftmost_unit (const new_coord *coord)
 {
 	return (coord->between == AT_UNIT &&
@@ -222,6 +241,7 @@ int ncoord_is_leftmost_unit (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned at the last unit of the last item.  Not
  * true for empty nodes nor coordinates positioned after the last item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_rightmost_unit (const new_coord *coord)
 {
 	assert ("jmacd-9809", ncoord_is_existing_item (coord));
@@ -232,6 +252,7 @@ int ncoord_is_rightmost_unit (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned at any unit of the last item.  Not true
  * for empty nodes nor coordinates positioned after the last item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_rightmost_item (const new_coord *coord)
 {
 	assert ("jmacd-9820", ncoord_is_existing_item (coord));
@@ -298,6 +319,7 @@ int ncoord_check (const new_coord *coord)
 
 /* Adjust coordinate boundaries based on the number of items prior to ncoord_next/prev.
  * Returns 1 if the new position is does not exist. */
+/* Audited by: green(2002.06.15) */
 static int ncoord_adjust_items (new_coord *coord, unsigned items, int is_next)
 {
 	/* If the node is invalid, leave it. */
@@ -336,6 +358,7 @@ static int ncoord_adjust_items (new_coord *coord, unsigned items, int is_next)
 /* Advances the coordinate by one unit to the right.  If empty, no change.  If
  * ncoord_is_rightmost_unit, advances to AFTER THE LAST ITEM.  Returns 0 if new position is an
  * existing unit. */
+/* Audited by: green(2002.06.15) */
 int ncoord_next_unit (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -394,6 +417,7 @@ int ncoord_next_unit (new_coord *coord)
 /* Advances the coordinate by one item to the right.  If empty, no change.  If
  * ncoord_is_rightmost_unit, advances to AFTER THE LAST ITEM.  Returns 0 if new position is
  * an existing item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_next_item (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -436,6 +460,7 @@ int ncoord_next_item (new_coord *coord)
 /* Advances the coordinate by one unit to the left.  If empty, no change.  If
  * ncoord_is_leftmost_unit, advances to BEFORE THE FIRST ITEM.  Returns 0 if new position
  * is an existing unit. */
+/* Audited by: green(2002.06.15) */
 int ncoord_prev_unit (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -492,6 +517,7 @@ int ncoord_prev_unit (new_coord *coord)
 /* Advances the coordinate by one item to the left.  If empty, no change.  If
  * ncoord_is_leftmost_unit, advances to BEFORE THE FIRST ITEM.  Returns 0 if new position
  * is an existing item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_prev_item (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -530,6 +556,7 @@ int ncoord_prev_item (new_coord *coord)
 }
 
 /* Calls either ncoord_init_first_unit or ncoord_init_last_unit depending on sideof argument. */
+/* Audited by: green(2002.06.15) */
 void ncoord_init_sideof_unit (new_coord *coord, znode *node, sideof dir)
 {
 	assert ("jmacd-9821", dir == LEFT_SIDE || dir == RIGHT_SIDE);
@@ -542,6 +569,7 @@ void ncoord_init_sideof_unit (new_coord *coord, znode *node, sideof dir)
 
 /* Calls either ncoord_is_before_leftmost or ncoord_is_after_rightmost depending on sideof
  * argument. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_after_sideof_unit (new_coord *coord, sideof dir)
 {
 	assert ("jmacd-9822", dir == LEFT_SIDE || dir == RIGHT_SIDE);
@@ -553,6 +581,7 @@ int ncoord_is_after_sideof_unit (new_coord *coord, sideof dir)
 }
 
 /* Calls either ncoord_next_unit or ncoord_prev_unit depending on sideof argument. */
+/* Audited by: green(2002.06.15) */
 int ncoord_sideof_unit (new_coord *coord, sideof dir)
 {
 	assert ("jmacd-9823", dir == LEFT_SIDE || dir == RIGHT_SIDE);
@@ -565,6 +594,7 @@ int ncoord_sideof_unit (new_coord *coord, sideof dir)
 
 /* Returns true if two coordinates are consider equal.  Coordinates that are between units
  * or items are considered equal. */
+/* Audited by: green(2002.06.15) */
 int ncoord_eq (const new_coord *c1, const new_coord *c2)
 {
 	assert ("nikita-1807", c1 != NULL);
@@ -612,6 +642,7 @@ int ncoord_eq (const new_coord *c1, const new_coord *c2)
 
 /* If ncoord_is_after_rightmost return NCOORD_ON_THE_RIGHT, if ncoord_is_after_leftmost
  * return NCOORD_ON_THE_LEFT, otherwise return NCOORD_INSIDE. */
+/* Audited by: green(2002.06.15) */
 coord_wrt_node ncoord_wrt (const new_coord *coord)
 {
 	if (ncoord_is_before_leftmost (coord)) {
@@ -627,6 +658,7 @@ coord_wrt_node ncoord_wrt (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned after the last item or after the last unit
  * of the last item or it is an empty node. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_after_rightmost (const new_coord *coord)
 {
 	assert ("jmacd-7313", ncoord_check (coord));
@@ -655,6 +687,7 @@ int ncoord_is_after_rightmost (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned before the first item or it is an empty
  * node. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_before_leftmost (const new_coord *coord)
 {
 	assert ("jmacd-7313", ncoord_check (coord));
@@ -680,6 +713,7 @@ int ncoord_is_before_leftmost (const new_coord *coord)
 
 /* Returns true if the coordinate is positioned after a item, before a item, after the
  * last unit of an item, before the first unit of an item, or at an empty node. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_between_items (const new_coord *coord)
 {
 	assert ("jmacd-7313", ncoord_check (coord));
@@ -707,6 +741,7 @@ int ncoord_is_between_items (const new_coord *coord)
 
 /* Returns true if the coordinates are positioned at adjacent units, regardless of
  * before-after or item boundaries. */
+/* Audited by: green(2002.06.15) */
 int ncoord_are_neighbors (new_coord *c1, new_coord *c2)
 {
 	new_coord *left;
@@ -744,6 +779,7 @@ int ncoord_are_neighbors (new_coord *c1, new_coord *c2)
 
 /* Assuming two coordinates are positioned in the same node, return COORD_CMP_ON_RIGHT,
  * COORD_CMP_ON_LEFT, or COORD_CMP_SAME depending on c1's position relative to c2.  */
+/* Audited by: green(2002.06.15) */
 coord_cmp ncoord_compare (new_coord * c1, new_coord * c2)
 {
 	assert ("vs-209", c1->node == c2->node);
@@ -762,6 +798,7 @@ coord_cmp ncoord_compare (new_coord * c1, new_coord * c2)
 
 /* Returns true if coord is set to or before the first (if LEFT_SIDE) unit of the item and
  * to or after the last (if RIGHT_SIDE) unit of the item. */
+/* Audited by: green(2002.06.15) */
 int ncoord_is_delimiting (new_coord *coord, sideof dir)
 {
 	assert ("jmacd-9824", dir == LEFT_SIDE || dir == RIGHT_SIDE);
@@ -776,6 +813,7 @@ int ncoord_is_delimiting (new_coord *coord, sideof dir)
 /* If the coordinate is before an item/unit, set to next item/unit.  If the coordinate is
  * after an item/unit, set to the previous item/unit.  Returns 0 on success and non-zero
  * if there is no position (i.e., if the coordinate is empty). */
+/* Audited by: green(2002.06.15) */
 int ncoord_set_to_unit (new_coord *coord)
 {
 	assert ("jmacd-7316", ncoord_check (coord));
@@ -802,6 +840,7 @@ int ncoord_set_to_unit (new_coord *coord)
 
 /* If the coordinate is between items, shifts it to the right.  Returns 0 on success and
  * non-zero if there is no position to the right. */
+/* Audited by: green(2002.06.15) */
 int ncoord_set_to_right (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -857,6 +896,7 @@ int ncoord_set_to_right (new_coord *coord)
 
 /* If the coordinate is between items, shifts it to the left.  Returns 0 on success and
  * non-zero if there is no position to the left. */
+/* Audited by: green(2002.06.15) */
 int ncoord_set_to_left (new_coord *coord)
 {
 	unsigned items = ncoord_num_items (coord);
@@ -913,6 +953,7 @@ int ncoord_set_to_left (new_coord *coord)
 	return 0;
 }
 
+/* Audited by: green(2002.06.15) */
 static const char * ncoord_tween (between_enum n)
 {
 	switch (n) {
@@ -928,6 +969,7 @@ static const char * ncoord_tween (between_enum n)
 }
 
 
+/* Audited by: green(2002.06.15) */
 void ncoord_print (const char * mes, const new_coord * coord, int node)
 {
 	if( coord == NULL ) {
@@ -943,6 +985,7 @@ void ncoord_print (const char * mes, const new_coord * coord, int node)
 
 /* Hack. */
 /*
+/* Audited by: green(2002.06.15) */
 void ncoord_to_tcoord (new_coord *tcoord, const new_coord *ncoord)
 {
 	tcoord->item_pos = ncoord->item_pos;
@@ -964,6 +1007,7 @@ void ncoord_to_tcoord (new_coord *tcoord, const new_coord *ncoord)
 	}
 }
 
+/* Audited by: green(2002.06.15) */
 void tcoord_to_ncoord (new_coord *ncoord, const new_coord *tcoord)
 {
 	unsigned items;
