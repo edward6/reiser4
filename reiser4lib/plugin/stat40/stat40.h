802b59019a42c1e377e42b3632ec4af3a027a232
namesys.com!umka <namesys.com!umka>
2002-06-20 11:25:36 +0000

    A lot of fixes in node40 plugin. Some fixes in device.c.
    2002/06/20 14:34:12+04:00 namesys.com!umka
    Rename: reiser4lib/plugin/stat40/sd40.h -> reiser4lib/plugin/stat40/stat40.h
    
    (Logical change 1.944)

---
+++ reiser4lib/plugin/stat40/stat40.h
@@ -0,0 +1,16 @@
+/*
+    stat40.h -- reiser4 default stat data structures.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Vitaly Fertman.
+*/
+
+#ifndef STAT40_H
+#define STAT40_H
+
+struct reiserfs_stat40 {
+};
+
+typedef struct reiserfs_stat40 reiserfs_stat40_t;  
+
+#endif
+
