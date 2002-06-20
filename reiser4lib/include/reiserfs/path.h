3d80c5790246aef939897b637ab6c1f88eb3d6e2
namesys.com!umka <namesys.com!umka>
2002-06-20 17:18:41 +0000

    Were added path functions. They are needed by tree lookup and traverse functions.
    2002/06/20 20:33:17+04:00 namesys.com!umka
    
    (Logical change 1.973)

---
+++ reiser4lib/include/reiserfs/path.h
@@ -0,0 +1,47 @@
+/*
+    path.h -- path functions nneded for tree lookup.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef PATH_H
+#define PATH_H
+
+#include <aal/aal.h>
+#include <reiserfs/reiserfs.h>
+
+struct reiserfs_path {
+    aal_list_t *entity;		/* list for holding path elements */
+    void *data;			/* user specified per-path data */
+};
+
+typedef struct reiserfs_path reiserfs_path_t;
+
+struct reiserfs_coord {
+    reiserfs_opaque_t *node;	/* node in the tree */
+    int16_t item_pos;		/* pos of an item in the node */
+    int16_t unit_pos;		/* pos of an unit in the item */
+};
+
+typedef struct reiserfs_coord reiserfs_coord_t;
+
+extern reiserfs_path_t *reiserfs_path_create(void *data);
+extern void reiserfs_path_free(reiserfs_path_t *path);
+
+extern reiserfs_coord_t *reiserfs_path_append(reiserfs_path_t *path, 
+    reiserfs_coord_t *coord);
+
+extern reiserfs_coord_t *reiserfs_path_insert(reiserfs_path_t *path, 
+    reiserfs_coord_t *coord, uint8_t level);
+
+extern void reiserfs_path_remove(reiserfs_path_t *path, 
+    reiserfs_coord_t *coord);
+
+extern void reiserfs_path_delete(reiserfs_path_t *path, 
+    uint8_t level);
+
+extern reiserfs_coord_t *reiserfs_path_at(reiserfs_path_t *path, 
+    uint8_t level);
+
+#endif
+
