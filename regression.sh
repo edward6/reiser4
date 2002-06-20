e9c2b95ead0f7adc595b0f4614faa295675d8c91
namesys.com!god <namesys.com!god>
2002-06-20 10:41:14 +0000

    make regression.sh "scalable"
    (Logical change 1.934)

--- regression.sh
+++ regression.sh
@@ -17,54 +17,57 @@ export REISER4_CRASH_MODE=debugger
 
 rm -f gmon.out.*
 
+#ORDER='000'
+ORDER='0'
+
 for r in `seq 1 $ROUNDS` 
 do
 echo Round $r
 # "non-persistent" tests
 if [ x$REISER4_UL_DURABLE_MMAP = x ]
 then
-	run ./a.out nikita ibk 30000 || exit 1
-	mv gmon.out gmon.out.ibk.30000.$r 2>/dev/null
+	run ./a.out nikita ibk 30${ORDER} || exit 1
+	mv gmon.out gmon.out.ibk.30${ORDER}.$r 2>/dev/null
 
 #	run ./a.out jmacd build 3 1000 1000 || exit 4
 #	mv gmon.out gmon.out.build.3.1000.$r 2>/dev/null
 fi
 
-run ./a.out nikita dir 1 100000 0 || exit 2
-mv gmon.out gmon.out.dir.1.100000.0.$r 2>/dev/null
+run ./a.out nikita dir 1 100${ORDER} 0 || exit 2
+mv gmon.out gmon.out.dir.1.100${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita dir 1 100000 1 || exit 2
-mv gmon.out gmon.out.dir.1.100000.1.$r 2>/dev/null
+run ./a.out nikita dir 1 100${ORDER} 1 || exit 2
+mv gmon.out gmon.out.dir.1.100${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita dir 4 7000 0 || exit 3
-mv gmon.out gmon.out.dir.7.7000.0.$r 2>/dev/null
+run ./a.out nikita dir 4 7${ORDER} 0 || exit 3
+mv gmon.out gmon.out.dir.7.7${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita dir 4 7000 1 || exit 3
-mv gmon.out gmon.out.dir.7.7000.1.$r 2>/dev/null
+run ./a.out nikita dir 4 7${ORDER} 1 || exit 3
+mv gmon.out gmon.out.dir.7.7${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita mongo 3 20000 0 || exit 5
-mv gmon.out gmon.out.mongo.3.20000.0.$r 2>/dev/null
+run ./a.out nikita mongo 3 20${ORDER} 0 || exit 5
+mv gmon.out gmon.out.mongo.3.20${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita mongo 3 20000 1 || exit 5
-mv gmon.out gmon.out.mongo.3.20000.1.$r 2>/dev/null
+run ./a.out nikita mongo 3 20${ORDER} 1 || exit 5
+mv gmon.out gmon.out.mongo.3.20${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita rm 6 10000 0 || exit 6
-mv gmon.out gmon.out.rm.6.10000.0.$r 2>/dev/null
+run ./a.out nikita rm 6 10${ORDER} 0 || exit 6
+mv gmon.out gmon.out.rm.6.10${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita rm 6 10000 1 || exit 6
-mv gmon.out gmon.out.rm.6.10000.1.$r 2>/dev/null
+run ./a.out nikita rm 6 10${ORDER} 1 || exit 6
+mv gmon.out gmon.out.rm.6.10${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita unlink 15000 || exit 7
-mv gmon.out gmon.out.unlink.15000.$r 2>/dev/null
+run ./a.out nikita unlink 15${ORDER} || exit 7
+mv gmon.out gmon.out.unlink.15${ORDER}.$r 2>/dev/null
 
-#run ./a.out nikita queue 30 10000 10000  || exit 6
-#mv gmon.out gmon.out.rm.30.10000.10000.$r 2>/dev/null
+#run ./a.out nikita queue 30 10${ORDER} 10000  || exit 6
+#mv gmon.out gmon.out.rm.30.10${ORDER}.10000.$r 2>/dev/null
 
-run ./a.out nikita mongo 30 1500 0 || exit 6
-mv gmon.out gmon.out.rm.30.1500.0.$r 2>/dev/null
+run ./a.out nikita mongo 30 1${ORDER} 0 || exit 6
+mv gmon.out gmon.out.rm.30.1${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita mongo 30 1500 1 || exit 6
-mv gmon.out gmon.out.rm.30.1500.1.$r 2>/dev/null
+run ./a.out nikita mongo 30 1${ORDER} 1 || exit 6
+mv gmon.out gmon.out.rm.30.1${ORDER}.1.$r 2>/dev/null
 
 #( find /tmp | ./a.out vs copydir ) || exit 8
 #mv gmon.out gmon.out.vs.copydir.tmp.$r
