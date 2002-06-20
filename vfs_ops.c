d04dc41bf1a563b900493f82c39b979d33d1b13b
namesys.com!god <namesys.com!god>
2002-06-20 12:29:58 +0000

    Auto merged
    2002/06/20 16:29:52+04:00 namesys.com!god
    add ->set_page_dirty() method
    
    (Logical change 1.951)

--- vfs_ops.c
+++ vfs_ops.c
@@ -1325,8 +1325,6 @@ static int put_super (struct super_block * s)
 	REISER4_ENTRY (s);
 	assert( "umka-087", get_super_private (s) != NULL );
 	
-	print_tree_rec( "put_super", current_tree, ~0ull );
-
 	if ((ret = txn_mgr_force_commit (s))) {
 		warning ("jmacd-7711", "txn_force failed in put_super: %u", ret);
 	}
