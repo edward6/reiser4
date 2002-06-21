6a4677f4f470d063e30b692ee6e5aa8ffcf594ac
namesys.com!god <namesys.com!god>
2002-06-21 16:42:28 +0000

    cleanup
    2002/06/21 16:11:01+04:00 namesys.com!god
    Auto merged
    
    2002/06/21 16:10:49+04:00 namesys.com!god
    bug fixing
    
    2002/06/20 21:39:38+04:00 namesys.com!god
    znode/jnode split
    
    (Logical change 1.997)

--- znode.c
+++ znode.c
@@ -268,11 +268,10 @@ static void zinit( znode *node /* znode to initialise */,
 	jnode_init( &node -> zjnode );
 	reiser4_init_lock( &node -> lock );
 
-	coord_init_parent_hint (&node -> ptr_in_parent_hint, parent);
-
 	assert( "umka-051", current_tree != NULL );
 	
 	spin_lock_tree( current_tree );
+	coord_init_parent_hint (&node -> ptr_in_parent_hint, parent);
 	node -> version = ++ current_tree -> znode_epoch;
 	spin_unlock_tree( current_tree );
 }
@@ -876,56 +875,6 @@ int znode_is_loaded( const znode *node /* znode to query */ )
 	return ZF_ISSET( node, ZNODE_LOADED );
 }
 
-/* functions to maintain (partial) tree of znodes */
-
-/** block number of node */
-/* Audited by: umka (2002.06.11) */
-const reiser4_block_nr *jnode_get_block( const jnode *node /* jnode to
-							    * query */ )
-{
-	assert( "nikita-528", node  != NULL );
-
-/* As soon as we implement accessing nodes not stored on block devices
-   (e.g. distributed reiserfs), then we need to replace this line with
-   a call to a node plugin.
-
-   Josh replies: why not extent the block number to be
-   node_id/device/block_nr.  I don't think the concept of a block number
-   changes in a distributed setting, but you will need a node method to get
-   the block: likely we already have that.
-*/
-	return & node -> blocknr;
-}
-
-/* Audited by: umka (2002.06.11) */
-void jnode_set_block( jnode *node /* jnode to update */,
-		      const reiser4_block_nr *blocknr /* new block nr */ )
-{
-	assert( "nikita-2020", node  != NULL );
-	assert( "umka-055", blocknr != NULL );
-	
-	node -> blocknr = *blocknr;
-}
-#if 0
-/* this is used to assign block number to jnode of unformatted node */
-void jnode_set_block (jnode * node, reiser4_block_nr block)
-{
-	assert ("vs-650", node != NULL);
-	assert ("vs-672", node->blocknr);
-	assert ("vs-651", blocknr_is_fake (&node->blocknr));
-	node->blocknr = block;
-}
-#endif
-
-/* return true if jnode has real blocknr */
-int jnode_has_block (jnode * node)
-{
-	assert ("vs-673", node);
-	assert ("vs-674", node->blocknr);
-	return blocknr_is_fake (&node->blocknr) ? 0 : 1;
-}
-
-
 /** left delimiting key of znode */
 /* Audited by: umka (2002.06.11) */
 reiser4_key *znode_get_rd_key( znode *node /* znode to query */ )
@@ -1039,80 +988,44 @@ int znode_is_root( const znode *node /* znode to query */ )
 	return result;
 }
 
-/* Audited by: umka (2002.06.15) */
-void jnode_attach_page_nolock( jnode *node, struct page *pg )
+void init_dh( data_handle *dh )
 {
-	assert( "nikita-2060", node != NULL );
-	assert( "nikita-2061", pg != NULL );
+	assert( "nikita-2105", dh != NULL );
+	xmemset( dh, 0, sizeof *dh );
+}
 
-	assert( "nikita-2050", 
-		( pg -> private == 0ul ) || 
-		( pg -> private == ( unsigned long ) node ) );
-	if( !PagePrivate( pg ) ) {
-		assert( "nikita-2059", pg -> private == 0ul );
-		pg -> private = ( unsigned long ) node;
-		node -> pg  = pg;
-		SetPagePrivate( pg );
-		/* add reference to page */
-		page_cache_get( pg );
+void done_dh( data_handle *dh )
+{
+	assert( "nikita-2106", dh != NULL );
+	if( dh -> node != NULL ) {
+		for( ; dh -> d_ref > 0 ; -- dh -> d_ref )
+			zrelse( dh -> node );
 	}
 }
 
-/* Audited by: umka (2002.06.15) */
-void jnode_attach_page( jnode *node, struct page *pg )
+int load_dh_node( data_handle *dh, znode *node )
 {
-	assert( "nikita-2047", node != NULL );
-	assert( "nikita-2048", pg != NULL );
+	assert( "nikita-2107", dh != NULL );
+	assert( "nikita-2108", node != NULL );
+	assert( "nikita-2109", ergo( dh -> node != NULL, dh -> node == node ) );
 
-	spin_lock( &_jnode_ptr_lock );
-	jnode_attach_page_nolock( node, pg );
-	spin_unlock( &_jnode_ptr_lock );
+	dh -> node = node;
+	return load_dh( dh );
 }
 
-/* Audited by: umka (2002.06.15) */
-static void break_page_jnode_linkage( struct page *page, jnode *node )
+int load_dh( data_handle *dh )
 {
-	assert( "nikita-2063", page != NULL );
-	assert( "nikita-2064", node != NULL );
+	int result;
 
-	page -> private = 0ul;
-	ClearPagePrivate( page );
-	node -> pg = NULL;
+	assert( "nikita-2110", dh != NULL );
+	assert( "nikita-2111", dh -> node != NULL );
+
+	result = zload( dh -> node );
+	if( result == 0 )
+		++ dh -> d_ref;
+	return result;
 }
 
-/* Audited by: umka (2002.06.15) */
-jnode *page_detach_jnode( struct page *page )
-{
-	jnode *node;
-
-	assert( "nikita-2062", page != NULL );
-
-	spin_lock( &_jnode_ptr_lock );
-	node = ( jnode * ) page -> private;
-	if( node != NULL )
-		break_page_jnode_linkage( page, node );
-	spin_unlock( &_jnode_ptr_lock );
-	page_cache_release( page );
-	return node;
-}
-
-/* Audited by: umka (2002.06.15) */
-void jnode_detach_page( jnode *node )
-{
-	struct page *page;
-
-	assert( "nikita-2052", node != NULL );
-
-	spin_lock( &_jnode_ptr_lock );
-	page = jnode_page( node );
-	if( page == NULL ) {
-		spin_unlock( &_jnode_ptr_lock );
-		return;
-	}
-	break_page_jnode_linkage( page, node );
-	spin_unlock( &_jnode_ptr_lock );
-	page_cache_release( page );
-}
 
 /** debugging aid: znode invariant */
 /* Audited by: umka (2002.06.11) */
@@ -1272,39 +1185,6 @@ void print_znode( const char *prefix /* prefix to print */,
 	print_key( "\tld", &node -> ld_key );
 	print_key( "\trd", &node -> rd_key );
 	info( "\treaders: %i\n", node -> lock.nr_readers );
-}
-
-#define jnode_state_name( node, flag )			\
-	( JF_ISSET( ( node ), ( flag ) ) ? ((#flag ## "|")+6) : "" )
-
-/** debugging aid: output human readable information about @node */
-/* Audited by: umka (2002.06.11) */
-void info_jnode( const char *prefix /* prefix to print */, 
-		 const jnode *node /* node to print */ )
-{
-	assert( "umka-068", prefix != NULL );
-	
-	if( node == NULL ) {
-		info( "%s: null\n", prefix );
-		return;
-	}
-
-	info( "%s: %p: state: %lu: [%s%s%s%s%s%s%s%s%s%s%s], level: %i, ",
-	      prefix, node, node -> state, 
-
-	      jnode_state_name( node, ZNODE_LOADED ),
-	      jnode_state_name( node, ZNODE_HEARD_BANSHEE ),
-	      jnode_state_name( node, ZNODE_LEFT_CONNECTED ),
-	      jnode_state_name( node, ZNODE_RIGHT_CONNECTED ),
-	      jnode_state_name( node, ZNODE_NEW ),
-	      jnode_state_name( node, ZNODE_ALLOC ),
-	      jnode_state_name( node, ZNODE_RELOC ),
-	      jnode_state_name( node, ZNODE_WANDER ),
-	      jnode_state_name( node, ZNODE_DIRTY ),
-	      jnode_state_name( node, ZNODE_WRITEOUT ),
-	      jnode_state_name( node, ZNODE_IS_DYING ),
-	      
-	      jnode_get_level( node ));
 }
 
 /** debugging aid: output human readable information about @node */
