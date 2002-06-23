ba6fc15a66dfeb9d63a945972e77b2ac025fba97
namesys.com!vs <namesys.com!vs>
2002-06-23 12:04:39 +0000

    reiser4_get_right_neighbor may return -ENOENT when not right neighbor found
    (Logical change 1.1045)

--- flush.c
+++ flush.c
@@ -340,6 +340,12 @@ static int flush_right_relocate_end_of_twig (flush_position *pos)
 	if ((ret = reiser4_get_right_neighbor (& right_lock, pos->parent_lock.node, ZNODE_WRITE_LOCK, 0))) {
 		/* If -ENAVAIL we are finished (or do we go upward anyway?). */
 		if (ret == -ENAVAIL) { ret = flush_pos_stop (pos); }
+
+		/*
+		 * FIXME-VS: reiser4_get_right_neighbor returns -ENOENT when it
+		 * could not get right neighbor
+		 */
+		if (ret == -ENOENT) { ret = flush_pos_stop (pos); }
 		goto exit;
 	}
 
