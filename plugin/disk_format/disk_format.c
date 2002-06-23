e999c485000c23511a2a7855bb16cac8ec0c17fb
namesys.com!reiser <namesys.com!reiser>
2002-06-23 21:23:47 +0000

    Rename: plugin/disk_format/layout.c -> plugin/disk_format/disk_format.c
    (Logical change 1.1058)

---
+++ plugin/disk_format/disk_format.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2002 Hans Reiser, licensing governed by reiser4/README
+ */
+
+#include "../../reiser4.h"
+
+
+/* initialization of disk layout plugins */
+reiser4_plugin layout_plugins[ LAST_LAYOUT_ID ] = {
+	[ LAYOUT_40_ID ] = {
+		.layout = {
+			.h = {
+				.type_id = REISER4_LAYOUT_PLUGIN_TYPE,
+				.id      = LAYOUT_40_ID,
+				.pops    = NULL,
+				.label   = "reiser40",
+				.desc    = "standard disk layout for reiser40",
+				.linkage = TS_LIST_LINK_ZERO,
+			},
+			.get_ready     = layout_40_get_ready,
+			.root_dir_key  = layout_40_root_dir_key,
+			.release       = NULL,
+			.print_info    = NULL
+		}
+	},
+	[ TEST_LAYOUT_ID ] = {
+		.layout = {
+			.h = {
+				.type_id = REISER4_LAYOUT_PLUGIN_TYPE,
+				.id      = TEST_LAYOUT_ID,
+				.pops    = NULL,
+				.label   = "test",
+				.desc    = "layout for debugging",
+				.linkage = TS_LIST_LINK_ZERO,
+			},
+			.get_ready     = test_layout_get_ready,
+			.root_dir_key  = test_layout_root_dir_key,
+			.release       = test_layout_release,
+			.print_info    = test_layout_print_info
+		}
+	}
+};
