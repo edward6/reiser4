d3d156fafb8475c848c235cbe5f38a2d888247fe
namesys.com!jmacd <namesys.com!jmacd>
2002-06-24 18:03:50 +0000

    i hate this entering comments in resolve
    2002/06/24 22:02:35+04:00 namesys.com!jmacd
    fix nikita's first report, it was a bogus assertion
    
    (Logical change 1.1087)

--- flush.c
+++ flush.c
@@ -601,11 +601,6 @@ static int flush_squalloc_one_changed_ancestor (znode *node, int call_depth, flu
 			 * have an unformatted node to the right.  That is tested again in
 			 * the calling function.  Could be done here without a second
 			 * test, except that complicates the recursion here. */
-			/*
-			 * FIXME:NIKITA->JMACD this fails with "a.out nikita
-			 * dir 1 100 1" on an empty fs.
-			 */
-			assert ("jmacd-5332", call_depth == 0);
 			ret = 0;
 		}
 		/* Otherwise error. */
