b0f2349881e0ffa1913f97af5b861cb41b00524b
namesys.com!umka <namesys.com!umka>
2002-06-24 07:36:44 +0000

    A lot of fixes. Temporary node creating code.
    2002/06/23 18:28:44+04:00 namesys.com!umka
    .
    
    2002/06/23 15:34:44+04:00 namesys.com!umka
    
    (Logical change 1.1061)

---
+++ reiser4lib/libreiserfs/hack.c
@@ -0,0 +1,198 @@
+/*
+    hack.c -- temporary hack for creating fresh tree. Will be removed later.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#include <sys/stat.h>
+
+#include <aal/aal.h>
+#include <reiserfs/reiserfs.h>
+
+#include <plugin/node40/node40.h>
+#include <plugin/stat40/stat40.h>
+#include <plugin/internal40/internal40.h>
+#include <plugin/direntry40/direntry40.h>
+
+#define OID_CHARS (sizeof(uint64_t) - 1)
+
+static uint64_t pack_string( const char *name, int start_idx) {
+    unsigned i;
+    uint64_t str;
+
+    str = 0;
+    for( i = 0 ; ( i < sizeof str - start_idx ) && name[ i ] ; ++ i ) {
+	str <<= 8;
+	str |= ( unsigned char ) name[ i ];
+    }
+    str <<= ( sizeof str - i - start_idx ) << 3;
+    return str;
+}
+
+
+blk_t hack_create_tree(reiserfs_fs_t *fs, reiserfs_plugin_id_t node_plugin_id) {
+    blk_t root_blk, blk;
+    aal_block_t *block;
+
+    reiserfs_node40_header_t *node;
+    reiserfs_item40_header_t *item;
+    
+    reiserfs_internal40_t *internal_body;
+    reiserfs_stat40_t *stat_body;
+    reiserfs_direntry40_t *direntry_body;
+
+    reiserfs_objid_t *dot_key;
+    reiserfs_objid_t *dotdot_key;
+
+    aal_assert("umka-486", fs != NULL, return -1);
+
+    /* Forming internal node */
+    
+    if (!(blk = reiserfs_alloc_find(fs)))
+	return 0;
+
+    root_blk = blk;
+    
+    reiserfs_alloc_use(fs, blk);
+    if (!(block = aal_device_alloc_block(fs->device, blk, 0)))
+	return 0;
+    
+    node = (reiserfs_node40_header_t *)block->data;
+    node->header.plugin_id = node_plugin_id;
+
+    set_nh40_level(node, 2);
+    set_nh40_magic(node, REISERFS_NODE40_MAGIC);
+    set_nh40_num_items(node, 1);
+    
+    set_nh40_free_space_start(node, sizeof(reiserfs_node40_header_t) + 
+	sizeof(reiserfs_internal40_t));
+    
+    set_nh40_free_space(node, block->size - sizeof(reiserfs_node40_header_t) - 
+	sizeof(reiserfs_item40_header_t) - sizeof(reiserfs_internal40_t));
+
+    /* Forming internal item and body */
+    item = (reiserfs_item40_header_t *)(block->data + block->size - sizeof(*item));
+    aal_memset(&item->key, 0, sizeof(reiserfs_key_t));
+    
+    set_key_type(&item->key, KEY_SD_MINOR);
+    set_key_locality(&item->key, 41);
+    set_key_objectid(&item->key, 42);
+    
+    set_ih40_plugin_id(item, 0x2);
+    set_ih40_length(item, sizeof(reiserfs_internal40_t));
+    set_ih40_offset(item, sizeof(reiserfs_node40_header_t));
+    
+    internal_body = (reiserfs_internal40_t *)(node + get_ih40_offset(item));
+    
+    if (!(blk = reiserfs_alloc_find(fs))) {
+	aal_device_free_block(block);
+	return 0;
+    }	
+
+    reiserfs_alloc_use(fs, blk);
+    internal_body->block_nr = blk;
+    
+    if (!aal_device_write_block(fs->device, block)) {
+	aal_device_free_block(block);
+	return 0;
+    }
+    
+    aal_device_free_block(block);
+ 
+    /* Forming leaf node */
+    if (!(block = aal_device_alloc_block(fs->device, blk, 0)))
+	return 0;
+    
+    node = (reiserfs_node40_header_t *)block->data;
+    node->header.plugin_id = node_plugin_id;
+
+    set_nh40_level(node, 1);
+    set_nh40_magic(node, REISERFS_NODE40_MAGIC);
+    set_nh40_num_items(node, 2);
+    
+    set_nh40_free_space_start(node, sizeof(reiserfs_node40_header_t) + 
+	sizeof(reiserfs_stat40_t) + sizeof(reiserfs_direntry40_t));
+    
+    set_nh40_free_space(node, block->size - (sizeof(reiserfs_node40_header_t) + 
+	(2 * sizeof(reiserfs_item40_header_t)) + sizeof(reiserfs_stat40_t) + 
+	sizeof(reiserfs_direntry40_t) + 2*sizeof(reiserfs_direntry40_unit_t) + 2 + 3 + 
+	2*sizeof(reiserfs_objid_t)));
+
+    /* Forming stat data item and body */
+    item = (reiserfs_item40_header_t *)(block->data + block->size - sizeof(reiserfs_stat40_t));
+    aal_memset(&item->key, 0, sizeof(reiserfs_key_t));
+    
+    set_key_type(&item->key, KEY_SD_MINOR);
+    set_key_locality(&item->key, 41);
+    set_key_objectid(&item->key, 42);
+    
+    set_ih40_plugin_id(item, 0x0);
+    set_ih40_length(item, sizeof(reiserfs_stat40_t));
+    set_ih40_offset(item, sizeof(reiserfs_node40_header_t));
+    
+    stat_body = (reiserfs_stat40_t *)(node + get_ih40_offset(item));
+    stat_body->mode = S_IFDIR | 0111;
+    stat_body->extmask = 0;
+    stat_body->nlink = 2;
+    stat_body->size = 0;
+
+    /* Forming direntry item and body */
+    item = (reiserfs_item40_header_t *)(block->data + block->size - sizeof(reiserfs_stat40_t) - 
+	sizeof(reiserfs_direntry40_t) - 2*sizeof(reiserfs_direntry40_unit_t) - 2 - 3 - 
+	2*sizeof(reiserfs_objid_t));
+    
+    aal_memset(&item->key, 0, sizeof(reiserfs_key_t));
+    
+    set_key_type(&item->key, KEY_FILE_NAME_MINOR);
+    set_key_locality(&item->key, 42);
+    
+    set_ih40_plugin_id(item, 0x1);
+    
+    set_ih40_length(item, sizeof(reiserfs_direntry40_t) + 
+	2*sizeof(reiserfs_direntry40_unit_t) + 2*sizeof(reiserfs_objid_t) + aal_strlen(".") + 1 +
+	aal_strlen("..") + 1);
+    
+    set_ih40_offset(item, sizeof(reiserfs_node40_header_t) + sizeof(reiserfs_stat40_t));
+    
+    direntry_body = (reiserfs_direntry40_t *)node + get_ih40_offset(item);
+    direntry_body->num_entries = 2;
+    
+    direntry_body->entry[0].hash.objectid[0] = 42;
+    aal_memset(direntry_body->entry[0].hash.offset, 0, 16);
+
+    /* Offset of the first name in directory */
+    direntry_body->entry[0].offset = sizeof(reiserfs_direntry40_t) + 
+	2*sizeof(reiserfs_direntry40_unit_t);
+    
+    direntry_body->entry[1].hash.objectid[0] = 42;
+    {
+	uint64_t packed = pack_string(".." + OID_CHARS, 0);
+	aal_memcpy(direntry_body->entry[1].hash.offset, &packed, sizeof(packed));
+    }	
+
+    /* 
+	Offset of the second name in directory.
+	
+	It is calculated as following: size of direntry header + 
+	size of dir units array + key for first name + size of first 
+	name (".") + zero for terminating first name.
+    */
+    direntry_body->entry[1].offset = sizeof(reiserfs_direntry40_t) + 
+	2*sizeof(reiserfs_direntry40_unit_t) + sizeof(reiserfs_objid_t) + 2;
+
+    aal_memcpy((char *)(direntry_body + direntry_body->entry[0].offset), ".\0", 2);
+    dot_key = (reiserfs_objid_t *)(direntry_body + direntry_body->entry[0].offset + 2);
+    dot_key->locality[0] = 41;
+    dot_key->objectid[0] = 42;
+    
+    aal_memcpy((char *)(direntry_body + direntry_body->entry[1].offset), "..\0", 3);
+    dot_key = (reiserfs_objid_t *)(direntry_body + direntry_body->entry[1].offset + 3);
+    dot_key->locality[0] = 41 - 3;
+    dot_key->objectid[0] = 41;
+
+    aal_device_write_block(fs->device, block);
+    aal_device_free_block(block);
+
+    return root_blk;
+}
+
