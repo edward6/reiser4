9dca34c49a50d790290390192aa86140f38c67c7
namesys.com!vitaly <namesys.com!vitaly>
2002-06-27 16:15:09 +0000

    2002/06/25 19:08:52+04:00 namesys.com!vitaly
    (Logical change 1.1153)

--- reiser4lib/libreiserfs/item.c
+++ reiser4lib/libreiserfs/item.c
@@ -10,57 +10,47 @@
 
 #include <reiserfs/reiserfs.h>
 
-reiserfs_item_t *reiserfs_item_open(reiserfs_node_t *node, uint16_t pos) {
-    reiserfs_item_t *info;
+error_t reiserfs_item_open(reiserfs_item_t *item, reiserfs_coord_t *coord) {
     reiserfs_plugin_id_t plugin_id;
-    void * item;
 
-    if (!(info = aal_calloc(sizeof(*info), 0)))
-	return NULL;
-    
-    reiserfs_check_method(node->plugin->node, item_length, goto free_info);
-    info->length = node->plugin->node.item_length(node->entity, pos); 
-    
-    reiserfs_check_method(node->plugin->node, item_min_key, goto free_info); 
-    if (!(info->key = node->plugin->node.item_min_key(node->entity, pos))) {
+    aal_assert("vpf-045", item != NULL, return -1);
+    aal_assert("vpf-046", coord != NULL, return -1);
+
+    item->coord = coord;
+
+    if (!(plugin_id = reiserfs_node_get_item_plugin_id (coord->node, coord->item_pos))) {
 	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
-	    "Can't get key of the item (%u) in node (%llu).",
-	    pos, aal_device_get_block_nr(node->device, node->block));
-	goto free_info;
+	    "Can't get plugin id of the item (%u) in the node (%llu).", coord->item_pos, 
+	    aal_device_get_block_nr(coord->node->device, coord->node->block));
+	return -1;
     }
-
-    reiserfs_check_method(node->plugin->node, item_plugin_id, goto free_info); 
-    plugin_id = node->plugin->node.item_plugin_id(node->entity, pos);
     
-    if (!(info->plugin = reiserfs_plugins_find_by_coords(REISERFS_ITEM_PLUGIN, plugin_id))) {
+    if (!(item->plugin = reiserfs_plugins_find_by_coords(REISERFS_ITEM_PLUGIN, plugin_id))) {
 	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
 	    "Can't find item plugin by its id %x.", plugin_id);
-	    goto free_info;
+	return -1;
     }
     
-    reiserfs_check_method(node->plugin->node, item, goto free_info);
-    reiserfs_check_method(info->plugin->item.common, open, goto free_info);
-    
-    if (!(info->entity = info->plugin->item.common.open(node->plugin->node.item))) {
-	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
-	    "Can't open item (%u) in node (%llu).",
-	    pos, aal_device_get_block_nr(node->device, node->block));
-	goto free_info;
+    if (item->plugin->item.common.open != NULL) {
+	if (!(item->entity = item->plugin->item.common.open(&item->coord))) {
+	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+		"Can't open item (%u) in node (%llu).",	coord->item_pos, 
+		aal_device_get_block_nr(coord->node->device, coord->node->block));
+	    return -1;
+	}
     }
-
-    return info;
-
-free_info:
-    aal_free(info);
-    return NULL;
+    
+    return 0;
 }
 
 
-blk_t reiserfs_item_down_link(reiserfs_item_t *item, uint16_t unit_pos) {
+blk_t reiserfs_item_down_link(reiserfs_item_t *item) {
     aal_assert("vpf-041", item != NULL, return 0);
 
-    reiserfs_check_method(item->plugin->item.specific.internal, down_link, return 0);
-    return item->plugin->item.specific.internal.down_link(item->entity, unit_pos);
+    if (item->plugin->item.specific.internal.down_link == NULL)
+	return 0;
+    
+    return item->plugin->item.specific.internal.down_link(item);
 }
 
 int reiserfs_item_is_internal (reiserfs_item_t * item) {
@@ -70,4 +60,38 @@ int reiserfs_item_is_internal (reiserfs_item_t * item) {
     return item->plugin->item.common.is_internal();
 }
 
+int reiserfs_item_estimate (reiserfs_coord_t *coord, reiserfs_item_info_t *item_info, 
+    reiserfs_plugin_id_t id) 
+{
+    reiserfs_plugin_id_t plugin_id;
 
+    if (item_info->plugin == NULL) {
+	if (coord == NULL || coord->unit_pos == -1) 
+	    plugin_id = id;
+	else {
+	    if (!(plugin_id = reiserfs_node_get_item_plugin_id (coord->node, 
+		coord->item_pos))) 
+	    {
+		aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+		    "Can't get plugin id of the item (%u) in the node (%llu).", 
+		    coord->item_pos, 
+		    aal_device_get_block_nr(coord->node->device, coord->node->block));
+		return -1;
+	    }	    
+	}
+	
+	if (!(item_info->plugin = reiserfs_plugins_find_by_coords
+		(REISERFS_ITEM_PLUGIN, plugin_id))) 
+	{	
+	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+		"Can't find internal item plugin by its identifier %x.", plugin_id);
+	    return -1;
+	}
+    }
+
+    reiserfs_check_method (item_info->plugin->item.common, estimate, return -1);    
+
+    item_info->plugin->item.common.estimate(coord, item_info);  
+    
+    return 0;
+}
--- reiser4lib/libreiserfs/tree.c
+++ reiser4lib/libreiserfs/tree.c
@@ -88,6 +88,105 @@ error:
     return -1;
 }
 
+error_t reiserfs_tree_create_2(reiserfs_fs_t *fs, reiserfs_default_plugin_t *default_plugins) {
+    blk_t block_n;
+    reiserfs_plugin_t *item_plugin;
+    reiserfs_plugin_id_t item_plugin_id;
+    reiserfs_key_t key;
+    reiserfs_node_t *node;
+    reiserfs_coord_t coord;
+    reiserfs_item_info_t item;
+    reiserfs_internal_item_info_t internal_info;
+
+    aal_assert("umka-129", fs != NULL, return -1);
+    aal_assert("umka-130", fs->super != NULL, return -1);
+
+    if (!(fs->tree = aal_calloc(sizeof(*fs->tree), 0)))
+	return -1;
+    
+    /* Create a root node */
+    if (!(block_n = reiserfs_alloc_find(fs))) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't allocate block for root node.");
+	goto error_free_tree;
+    }
+    
+    reiserfs_alloc_use(fs, block_n);
+    reiserfs_super_set_root(fs, block_n);
+  
+    if (!(node = reiserfs_node_create(fs->device, block_n, default_plugins->node, 
+	REISERFS_LEAF_LEVEL + 1))) 
+    {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't create a root node.");
+	goto error_free_tree;
+    }
+    
+    if (!(block_n = reiserfs_alloc_find(fs))) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't allocate block for root node.");
+	goto error_free_node;
+    }
+
+    reiserfs_alloc_use(fs, block_n);
+
+    internal_info.block = &block_n;
+    
+    /* Insert an internal item */
+    set_key_type(&key, KEY_SD_MINOR);
+    set_key_locality(&key, REISERFS_RESERVED_IDS + 1);
+    set_key_objectid(&key, REISERFS_RESERVED_IDS + 2);
+
+    item.info = &internal_info;
+
+    coord.node = node;
+    coord.item_pos = 0;
+    coord.unit_pos = -1;
+
+    if (reiserfs_item_estimate (&coord, &item, default_plugins->item.internal)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't prepare info about internal item to be inserted.");
+	goto error_free_node;
+    }
+    
+    /* Item create method will be called from the node create method */
+    if (reiserfs_node_insert_item (&coord, &key, &item)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't insert an internal item into the node %llu.", 
+	    aal_device_get_block_nr(node->device, node->block));
+	goto error_free_node;
+    }
+
+    /* free node, block, etc. about squeeze node */
+    reiserfs_node_close (node);
+    
+    if (!(node = reiserfs_node_create (fs->device, block_n, default_plugins->node, 
+	REISERFS_LEAF_LEVEL))) 
+    {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't create a leaf node.");
+	goto error_free_tree;
+    } 
+/*
+    if (!reiserfs_direntry_create ()) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't create the root directory.");
+	goto error_free_node;
+    }
+*/
+
+    return 0;
+
+error_free_node:
+    reiserfs_node_close(node);
+error_free_tree:
+    aal_free(fs->tree);
+    fs->tree = NULL;
+error:
+    return -1;
+}
+
+
 error_t reiserfs_tree_sync(reiserfs_fs_t *fs) {
     aal_assert("umka-131", fs != NULL, return -1);
     aal_assert("umka-132", fs->tree != NULL, return -1);
@@ -115,7 +214,7 @@ int reiserfs_tree_lookup(reiserfs_fs_t *fs, blk_t from,
     reiserfs_node_t *node;
     reiserfs_coord_t *coord;
     reiserfs_plugin_t *plugin;
-    reiserfs_item_t *item_info;
+    reiserfs_item_t item;
 	
     aal_assert("umka-458", fs != NULL, return 0);
     aal_assert("umka-459", key != NULL, return 0);
@@ -137,19 +236,20 @@ int reiserfs_tree_lookup(reiserfs_fs_t *fs, blk_t from,
 	if (path && !reiserfs_path_append(path, coord))
 	    return 0;
 	
-	if (!(item_info = reiserfs_item_open(node, coord->item_pos))) {
+	if (reiserfs_item_open(&item, coord)) {
 	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
-		"Can't get item info from node for item %d.", coord->item_pos);
+		"Can't open item %d, from node %llu.", 
+		coord->item_pos, from);
 	    return 0;
 	}
 
-	if (!reiserfs_item_is_internal(item_info))
+	if (!reiserfs_item_is_internal(&item))
 	    return 1;
 	
-	if (!(from = reiserfs_item_down_link (item_info, coord->unit_pos))) {
+	if (!(from = reiserfs_item_down_link (&item))) {
 	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
 		"Can't get node in block %llu.", from);
-	    return 1;
+	    return 0;
 	}
     }
     return 0;
