a292ed196f3e03bac039ea5f1240afd24df4c378
namesys.com!jmacd <namesys.com!jmacd>
2002-06-30 13:37:52 +0000

    zam beat me to it.
    2002/06/30 17:37:07+04:00 namesys.com!jmacd
    add some tracing help, identified the issue in extent_copy_and_allocate.  go Brazil\!
    
    (Logical change 1.1199)

--- regression.sh
+++ regression.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+PROGRAM=./a.out
+
 printf "REISER4_TRACE_FLAGS:     %x\n" $REISER4_TRACE_FLAGS
 printf "REISER4_BLOCK_SIZE:      %i\n" ${REISER4_BLOCK_SIZE:-256}
 echo "REISER4_MOUNT:         " $REISER4_MOUNT
@@ -16,7 +18,7 @@ function run()
 
 function do_mkfs()
 {
-	echo "mkfs $REISER4_MOUNT" | ./a.out sh
+	echo "mkfs $REISER4_MOUNT" | ${PROGRAM} sh
 }
 
 export REISER4_PRINT_STATS=1
@@ -33,53 +35,53 @@ do_mkfs
 for r in `seq 1 $ROUNDS` 
 do
 echo Round $r
-run ./a.out nikita ibk 30${ORDER} || exit 1
+run ${PROGRAM} nikita ibk 30${ORDER} || exit 1
 mv gmon.out gmon.out.ibk.30${ORDER}.$r 2>/dev/null
 
 
-#	run ./a.out jmacd build 3 1000 1000 || exit 4
+#	run ${PROGRAM} jmacd build 3 1000 1000 || exit 4
 #	mv gmon.out gmon.out.build.3.1000.$r 2>/dev/null
 
-run ./a.out nikita dir 1 100${ORDER} 0 || exit 2
+run ${PROGRAM} nikita dir 1 100${ORDER} 0 || exit 2
 mv gmon.out gmon.out.dir.1.100${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita dir 1 100${ORDER} 1 || exit 2
+run ${PROGRAM} nikita dir 1 100${ORDER} 1 || exit 2
 mv gmon.out gmon.out.dir.1.100${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita dir 4 7${ORDER} 0 || exit 3
+run ${PROGRAM} nikita dir 4 7${ORDER} 0 || exit 3
 mv gmon.out gmon.out.dir.7.7${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita dir 4 7${ORDER} 1 || exit 3
+run ${PROGRAM} nikita dir 4 7${ORDER} 1 || exit 3
 mv gmon.out gmon.out.dir.7.7${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita mongo 3 20${ORDER} 0 || exit 5
+run ${PROGRAM} nikita mongo 3 20${ORDER} 0 || exit 5
 mv gmon.out gmon.out.mongo.3.20${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita mongo 3 20${ORDER} 1 || exit 5
+run ${PROGRAM} nikita mongo 3 20${ORDER} 1 || exit 5
 mv gmon.out gmon.out.mongo.3.20${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita rm 6 10${ORDER} 0 || exit 6
+run ${PROGRAM} nikita rm 6 10${ORDER} 0 || exit 6
 mv gmon.out gmon.out.rm.6.10${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita rm 6 10${ORDER} 1 || exit 6
+run ${PROGRAM} nikita rm 6 10${ORDER} 1 || exit 6
 mv gmon.out gmon.out.rm.6.10${ORDER}.1.$r 2>/dev/null
 
-run ./a.out nikita unlink 15${ORDER} || exit 7
+run ${PROGRAM} nikita unlink 15${ORDER} || exit 7
 mv gmon.out gmon.out.unlink.15${ORDER}.$r 2>/dev/null
 
-#run ./a.out nikita queue 30 10${ORDER} 10000  || exit 6
+#run ${PROGRAM} nikita queue 30 10${ORDER} 10000  || exit 6
 #mv gmon.out gmon.out.rm.30.10${ORDER}.10000.$r 2>/dev/null
 
-run ./a.out nikita mongo 30 1${ORDER} 0 || exit 6
+run ${PROGRAM} nikita mongo 30 1${ORDER} 0 || exit 6
 mv gmon.out gmon.out.rm.30.1${ORDER}.0.$r 2>/dev/null
 
-run ./a.out nikita mongo 30 1${ORDER} 1 || exit 6
+run ${PROGRAM} nikita mongo 30 1${ORDER} 1 || exit 6
 mv gmon.out gmon.out.rm.30.1${ORDER}.1.$r 2>/dev/null
 
 run ulevel/cp-r plugin || exit 7
 mv gmon.out gmon.out.cp-r.plugin.$r 2>/dev/null
 
-#( find /tmp | ./a.out vs copydir ) || exit 8
+#( find /tmp | ${PROGRAM} vs copydir ) || exit 8
 #mv gmon.out gmon.out.vs.copydir.tmp.$r
 echo Round $r done.
 done
