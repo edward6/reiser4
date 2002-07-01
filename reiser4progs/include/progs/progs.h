3fbc3b275180492798f597319f8c3d171a14aed1
namesys.com!vitaly <namesys.com!vitaly>
2002-07-01 16:45:30 +0000

    Rename: reiser4progs/progs/include/reiser4progs.h -> reiser4progs/include/progs/progs.h
    (Logical change 1.1231)

---
+++ reiser4progs/include/progs/progs.h
@@ -0,0 +1,10 @@
+/*
+    reiser4progs.h -- the central reiser4progs include file.
+    Copyright (C) 1996 - 2002 Hans Reiser
+    Author Vitaly Fertman
+*/
+
+#ifndef REISER4PROGS_H
+#define REISER4PROGS_H
+
+#endif
