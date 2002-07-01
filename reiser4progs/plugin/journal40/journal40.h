a5dd78c0613491ca606f87eb369b9e87f0cafb7d
namesys.com!vitaly <namesys.com!vitaly>
2002-07-01 15:47:42 +0000

    Auto merged
    2002/07/01 19:47:40+04:00 namesys.com!vitaly
    Merge rename: reiser4lib/plugin/journal40/journal40.h -> reiser4progs/plugin/journal40/journal40.h
    
    2002/07/01 19:43:22+04:00 namesys.com!vitaly
    mvdir
    
    (Logical change 1.1230)

---
+++ reiser4progs/plugin/journal40/journal40.h
@@ -0,0 +1,29 @@
+/*
+    journal40.h -- reiser4 default journal plugin.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef JOURNAL40_H
+#define JOURNAL40_h
+
+#include <aal/aal.h>
+
+#define REISERFS_JOURNAL40_HEADER   (4096 * 19)
+#define REISERFS_JOURNAL40_FOOTER   (4096 * 20)
+
+struct reiserfs_journal40 {
+    aal_device_t *device;
+    aal_block_t *header;
+};
+
+typedef struct reiserfs_journal40 reiserfs_journal40_t;
+
+struct reiserfs_journal40_header {
+    /* Journal40 specific fileds must be here. */
+};
+
+typedef struct reiserfs_journal40_header reiserfs_journal40_header_t;
+
+#endif
+
