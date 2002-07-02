3b3d29501e035fbb73ce39cc94cc62adce74dcb2
namesys.com!umka <namesys.com!umka>
2002-07-02 06:56:08 +0000

    Oid allocator added.
    2002/07/01 22:44:06+04:00 namesys.com!umka
    
    (Logical change 1.1243)

---
+++ reiser4progs/plugin/oid40/oid40.h
@@ -0,0 +1,23 @@
+/*
+    oid40.h -- reiser4 default oid allocator structures.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef OID40_H
+#define OID40_H
+
+#include <aal/aal.h>
+
+#define REISERFS_OID40_INITIAL 40
+
+struct reiserfs_oid40 {
+    aal_block_t *block;
+    uint16_t offset;
+    uint64_t current;
+};
+
+typedef struct reiserfs_oid40 reiserfs_oid40_t;
+
+#endif
+
