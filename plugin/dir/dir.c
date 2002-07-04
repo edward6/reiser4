110a457fab3a59a3712ebb7c2ee52ea87fc9e279
namesys.com!god <namesys.com!god>
2002-07-04 15:28:05 +0000

    Auto merged
    2002/07/04 19:03:23+04:00 namesys.com!green
    check that -> create method exists before calling it.
    
    (Logical change 1.1312)

--- plugin/dir/dir.c
+++ plugin/dir/dir.c
@@ -279,6 +279,10 @@ static int common_create_child( struct inode *parent /* parent object */,
 	/* reget plugin after installation */
 	fplug = inode_file_plugin( object );
 
+	if ( !fplug->create ) {
+		return -EPERM;
+	}
+
 	/*
 	 * if any of hash, tail, sd or permission plugins for newly created
 	 * object are not set yet set them here inheriting them from parent
