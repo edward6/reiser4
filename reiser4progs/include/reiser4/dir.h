8e9562d5ea1f88a0496db4652021cb7058dd975f
namesys.com!umka <namesys.com!umka>
2002-07-04 19:27:43 +0000

    mvdir
    (Logical change 1.1318)

---
+++ reiser4progs/include/reiser4/dir.h
@@ -0,0 +1,13 @@
+/*
+    dir.h -- reiserfs directory functions.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Vitaly Fertman.
+*/
+
+#ifndef DIR_H
+#define DIR_H
+
+extern int reiserfs_dir_create_child(void *data);
+
+#endif
+
