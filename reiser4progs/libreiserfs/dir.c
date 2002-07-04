ad754c1eaafa8818f52915eb09958e8bedd0ce7f
namesys.com!umka <namesys.com!umka>
2002-07-04 19:27:43 +0000

    mvdir
    }(Logical change 1.1318)

--- reiser4progs/libreiserfs/dir.c
+++
@@ -1,9 +0,0 @@
-/*
-    dir.c -- reiserfs directory code. 
-    Copyright (C) 1996-2002 Hans Reiser.
-    Author Vitaly Fertman.
-*/
-
-int reiserfs_dir_create_child(void *data) {
-    return 0;
-}
--- reiser4progs/libreiserfs/plugins_header.c
+++
@@ -1,9 +0,0 @@
-/*
-    plugins_header.c -- this just defines __plugin_start symbol at the start of .plugins
-    ELF-section. It is needed for monolithic building.
-    Copyright (C) 1996-2002 Hans Reiser.
-    Author Yury Umanets.
-*/
-
-unsigned long int __plugin_start __attribute__((__section__(".plugins"))) = 0;
-
