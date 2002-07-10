f9b77822b6c86ef389b841b7bf313d3c4645644b
namesys.com!umka <namesys.com!umka>
2002-07-10 08:08:09 +0000

    (Logical change 1.1423)

---
+++ reiser4progs/plugin/key40/key40.c
@@ -0,0 +1,7 @@
+/*
+    key40.c -- reiser4 default key plugin.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+
---
+++ reiser4progs/plugin/key40/key40.h
@@ -0,0 +1,11 @@
+/*
+    key40.h -- reiser4 default key structures.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef KEY40_H
+#define KEY40_H
+
+#endif
+
