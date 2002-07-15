81e98d52a52600b1328425563efed2159c5ca08f
namesys.com!umka <namesys.com!umka>
2002-07-15 10:39:55 +0000

    Added blocksize, label, uuid, etc options into mkfs
    2002/07/15 13:30:25+04:00 namesys.com!umka
    
    (Logical change 1.1475)

---
+++ reiser4progs/include/reiser4progs/misc.h
@@ -0,0 +1,25 @@
+/*
+    misc.h -- reiser4progs common include.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef REISER4_MISC_H
+#define REISER4_MISC_H
+
+#include <aal/aal.h>
+
+extern long int reiser4progs_misc_strtol(const char *str, int *error);
+
+extern int reiser4progs_misc_choose_check(const char *chooses, int choose);
+extern int reiser4progs_misc_choose_propose(const char *chooses, 
+    const char *error, const char *format, ...) __check_format__(printf, 3, 4);
+
+extern int reiser4progs_misc_dev_check(const char *dev);
+
+extern int reiser4progs_misc_size_check(const char *str);
+extern unsigned long long reiser4progs_misc_size_parse(const char *str, 
+    int *error);
+
+#endif
+
