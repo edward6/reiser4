b19ac3db5e122f372022edf0709234104b01b898
namesys.com!vs <namesys.com!vs>
2002-07-16 17:56:40 +0000

    Auto merged
    2002/07/16 13:46:18+04:00 namesys.com!vs
    Auto merged
    
    2002/07/13 14:33:01+04:00 namesys.com!vs
    conflict resolved
    
    2002/07/13 14:30:37+04:00 namesys.com!vs
    info_jnode: printing blocknr added
    
    (Logical change 1.1501)

--- jnode.c
+++ jnode.c
@@ -494,7 +494,7 @@ void info_jnode( const char *prefix /* prefix to print */,
 		return;
 	}
 
-	info( "%s: %p: state: %lu: [%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s], level: %i, pg: %p, ",
+	info( "%s: %p: state: %lu: [%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s], level: %i, block: %llu, pg: %p, ",
 	      prefix, node, node -> state, 
 
 	      jnode_state_name( node, ZNODE_LOADED ),
@@ -513,7 +513,7 @@ void info_jnode( const char *prefix /* prefix to print */,
 	      jnode_state_name( node, ZNODE_FLUSH_BUSY ),
 	      jnode_state_name( node, ZNODE_FLUSH_QUEUED ),
 	      
-	      jnode_get_level( node ), jnode_page( node ) );
+	      jnode_get_level( node ), *jnode_get_block( node ), jnode_page( node ) );
 }
 
 #endif
