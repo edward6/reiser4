28bb10d7d31c6016c5a5f27e52f5687f723436ba
namesys.com!vs <namesys.com!vs>
2002-07-25 18:31:13 +0000

    reiser4_invalidatepage: only for completely invalidated page
    (Logical change 1.1649)

--- vfs_ops.c
+++ vfs_ops.c
@@ -1823,9 +1823,10 @@ define_never_ever_op( direct_IO_vfs )
 int reiser4_invalidatepage( struct page *page, unsigned long offset )
 {
 	REISER4_ENTRY (page->mapping->host->i_sb);
-	assert( "nikita-2160", offset == 0 ); /* FIXME */
-	txn_delete_page( page );
-	page_detach_jnode( page );
+	if( offset == 0 ) {
+		txn_delete_page( page );
+		page_detach_jnode( page );
+	}
 	REISER4_EXIT (0);
 }
 
