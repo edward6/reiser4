6d957961a4b0aa3f08e9b4795bf1040826c40660
namesys.com!zam <namesys.com!zam>
2002-07-26 15:11:12 +0000

    comment corrected
    2002/07/26 18:44:00+04:00 namesys.com!zam
    
    
    (Logical change 1.1674)

--- plugin/oid/oid.c
+++ plugin/oid/oid.c
@@ -4,6 +4,67 @@
 
 #include "../../reiser4.h"
 
+/* a wrapper for allocate_oid method of oid_allocator plugin */
+int allocate_oid( oid_t *oid )
+{
+	struct super_block *s = reiser4_get_current_sb();
+
+	reiser4_super_info_data * private = get_super_private(s);
+	oid_allocator_plugin *oplug;
+
+	assert( "vs-479", private != NULL );
+	oplug = private-> oid_plug;
+	assert( "vs-480", oplug && oplug -> allocate_oid );
+	return oplug -> allocate_oid( get_oid_allocator( s ), oid );
+}
+
+/* a wrapper for release_oid method of oid_allocator plugin */
+int release_oid( oid_t oid )
+{
+	struct super_block *s = reiser4_get_current_sb();
+
+	reiser4_super_info_data * private = get_super_private(s);
+	oid_allocator_plugin *oplug;
+
+	assert( "nikita-1902", private != NULL);
+	oplug = private -> oid_plug;
+	assert( "nikita-1903", ( oplug != NULL ) && 
+		( oplug -> release_oid != NULL ) );
+	return  oplug -> release_oid
+		( get_oid_allocator(s), oid );
+}
+
+/* FIXME-ZAM: it is enough ugly but each operation of OID allocating/releasing
+ * are split into two parts, allocating/releasing itself and counting it (or
+ * logging). At OID allocation time an atom could not be available yet, but it
+ * should be available after we modify at least one block. So, new OID is
+ * allocated _before_ actual transaction begins, and this operations is
+ * counted _after_ it.  An allocation of empty atom can be a solution which
+ * has inefficiency in case of further atom fusion on first captured
+ * block. The OID releasing operation is spilt just for symmetry */
+
+/* count an object allocation in atom's nr_objects_created when current atom
+ * is available */
+void count_allocated_oid ( void )
+{
+	txn_atom * atom;
+
+	atom = get_current_atom_locked();
+	atom->nr_objects_created ++;
+	spin_unlock_atom(atom);
+}
+
+/* count an object deletion in atom's nr_objects_deleted */
+void count_released_oid( void )
+{
+	txn_atom * atom;
+
+	atom = get_current_atom_locked();
+	atom->nr_objects_created ++;
+	spin_unlock_atom(atom);
+}
+
+
 /* initialization of objectid management plugins */
 reiser4_plugin oid_plugins[ LAST_OID_ALLOCATOR_ID ] = {
 	[ OID_40_ALLOCATOR_ID ] = {
