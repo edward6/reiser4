f1d80c9d7510057385a49c6faceb2042909c7c46
namesys.com!demidov <namesys.com!demidov>
2002-08-07 14:28:44 +0000

    new table for token.
    (Logical change 1.1815)

--- parser/pars.cls.h
+++ parser/pars.cls.h
@@ -51,130 +51,154 @@
 
 
 
-#define EL -2
+#define ERR  -128
 
-#define OK   -1
-#define ERR   0
-#define Idn  1
-#define Int  2
-#define fld  3
-#define E_D  4
-#define Edg  2
-#define STR  2
-#define str  2
-#define DOT  2
-#define DD   2
-#define blk  2
-#define SLH  2
-#define BSL  22 
+#define OK   0
 
-#define blk 10
-#define DD   9
+#define Blk  1   /* blank */
+#define Wrd  2   /* any symbol exept spec symbl */
+#define Int  3   /* numeric */
+#define Ptr  4   /* pointer */
+#define Pru  5   /* _pruner */
+
+#define Stb  6   /* ` string begin */
+#define Ste  7   /* ' string end */
+#define Lpr  8   /* ( */ 
+#define Rpr  9   /* ) */
+#define Com 10   /* , */
+#define Mns 11   /* - */
+#define Dot 12   /* . */
+#define Slh 13   /* / */
+#define Lsq 14   /* [ */
+#define Rsq 15   /* ] */
+#define Bsl 16   /* \ */
+#define Lfl 18   /* { */
+#define Rfl 19   /* } */
+#define Pip 20   /* | */
+#define Sp1 22   /* : */
+#define Sp2 23   /* ; */
+#define Sp3 24   /* < */
+
+#define Sp4 25   /* = */
+#define Sp5 26   /* > */
+#define Sp6 27   /* ? */
+
+#define LastTerm Sp6
+
+#define Res 28   /*  */
+
+
+#define Str 32
+
+
 
 static
 char   ncl     [256] =
 {
-
 	ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,
 	ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,
 	ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,
 	ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,  ERR,
 	/* 32*/
-  /*        !     "    #     $     %     &     ' */
-	blk,    9,  STR,   15,    8,   15,   15,  str,
-  /* (      )     *    +     ,     -     .     / */
-     15,   15,   10,  PLS,   15,  PLS,  DOT,    8,
-  /* 0      1     2    3     4     5     6     7 */
+      /*        !     "    #     $     %     &     ' */
+	Blk,  Res,  Res,  Res,  Res,  Res,  Res,  Ste,
+      /* (      )     *    +     ,     -     .     / */
+        Lpr,  Rpr,  Res,  Pls,  Com,  Mns,  Sp3,  Slh,
+      /* 0      1     2    3     4     5     6     7 */
 	Int,  Int,  Int,  Int,  Int,  Int,  Int,  Int,
-  /* 8      9     :    ;     <     =     >     ? */
-	Int,  Int,   15,   15,   11,   12,   13,   15,
+      /* 8      9     :    ;     <     =     >     ? */
+	Int,  Int,  Sp2,  Sp1,  Les,  Sp4,  Sp5,  Sp6,
         
 	/* 64*/
-  /* @      A     B    C     D     E     F     G */
-	Ind,  Ind,  Ind,  Ind,  Ind,  Exp,  Ind,  Ind,
-  /* H      I     J    K     L     M     N     O */
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-  /* P      Q     R    S     T     U     V     W */
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-  /* X      Y     Z    [     \     ]     ^     _ */
-	Ind,  Ind,  Ind,    3,  BSL,   15,   14,  Ind,
+      /* @      A     B    C     D     E     F     G */
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Exp,  Wrd,  Wrd,
+      /* H      I     J    K     L     M     N     O */
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+      /* P      Q     R    S     T     U     V     W */
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+      /* X      Y     Z    [     \     ]     ^     _ */
+	Wrd,  Wrd,  Wrd,  Lsq,  Bsl Rsq,  Res,  Pru,
 	/* 96*/
-  /* `      a     b    c     d     e     f     g */
-    STR,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-  /* h      i     j    k     l     m     n     o */
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-  /* p      q     r    s     t     u     v     w */
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-  /* x      y     z    {     |     }     ~     */
-	Ind,  Ind,  Ind,   15,   15,   15,   15,    0,
+      /* `      a     b    c     d     e     f     g */
+        Stb,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+      /* h      i     j    k     l     m     n     o */
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+      /* p      q     r    s     t     u     v     w */
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+      /* x      y     z    {     |     }     ~     */
+	Wrd,  Wrd,  Wrd,  Lfl,  Pip,  Rfl,  St0,    0,
         
 	/*128*/
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
 	/*160*/
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
 	/*192*/
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
 	/*224*/
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,
-	Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind,  Ind
-},
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,
+	Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd,  Wrd
+};
 
 
-        lexcls[32][16]=
+static
+char        lexcls[32][32]=
 {
+/*               a    1         _    `    '      (    )    ,    -    <    /    [    ]      \    {    }    |    ;    :    .    =      >    ?            */
+/*          Blk  Wrd  Int  Ptr  Pru  Stb  Ste    Lpr  Rpr  Com  Mns  Les  Slh  Lsq  Rsq    Bsl  Lfl  Rfl  Pip  Sp1  Sp2  Sp3  Sp4    Sp5  Sp6  Res ...  */
 
+/*Blk*/{0,  Blk, Wrd, Int, Ptr, Pru, Str, ERR,   Lpr, Rpr, Com, Mns, OK , Slh, Lsq, Rsq,   Bsl, Lfl, Rfl, Pip, Sp1, Sp2, Sp3, Sp4,   Sp5, Sp6, OK , OK , OK , OK , OK , OK },
+/*Wrd*/{0,  OK , Wrd, Wrd, Wrd, Wrd, OK , OK ,   OK , OK , OK , Wrd, Wrd, Wrd, OK , OK ,   Wrd, OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
-/*            a     1     E       +-     .   blank   "      \     &     *     <        =      >     ^  ...*/
-/*           Ind   Int   Exp     PLS   DOT   blk   STR     BSL    &     *     <        =      >     ^  ...*/
+/*Int*/{0,  OK , Wrd, Int, Wrd, Wrd, OK , OK ,   OK , OK , OK , Wrd, Wrd, OK , OK , OK ,   Wrd, OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Ptr*/{0,  OK , Wrd, Wrd, Wrd, OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   Wrd, OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Pru*/{0,  OK , Pru, Pru, Pru, Pru, OK , OK ,   OK , OK , OK , Pru, Pru, Pru, OK , OK ,   Pru, OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
-/* ERR   */  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-/* Idn   */  Idn,  Idn,  Ind,    OK ,  Ind,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/* Int   */  OK ,  Int,  E_d,    OK ,  fld,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/* Exp   */  Ind,  Ind,  Ind,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
+/*Stb*/{0,  OK , Str, Str, Str, Str, Str, OK ,   Str, Str, Str, Str, Str, Str, Str, Str,   Str, Str, Str, Str, Str, Str, Str, Str,   Str, Str, Str, Str, Str, Str, Str, Str},
+/*Ste*/{0,  ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR},
+/*Lpr*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
-/* PLS   */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/* DOT   */  OK ,  fld,  OK ,    OK ,  DD ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/* blk   */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/* STR   */  STR,  STR,  STR,    STR,  STR,  STR,  STR,    Sbs,  STR,  STR,  STR,     STR,  STR,  STR,  STR,
+/*Rpr*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Com*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Mns*/{0,  ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR,   ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR},
+/*Les*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , ASG, App, OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
-/* BSL   */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
+/*Slh*/{0,  OK , Wrd, Wrd, Wrd, Wrd, OK , OK ,   OK , OK , OK , Wrd, OK , Wrd, OK , OK ,   Wrd, OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
+/*Lsq*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Rsq*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Bsl*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
+/*Lfl*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Rfl*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Pip*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Sp1*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Sp2*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Sp3*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Sp4*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Sp5*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
+/*Sp6*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Res*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 
-
-
-/* str   */  OK ,  OK ,  OK ,    OK ,  OK ,  OK , STR,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-
-    
-/* fld   */  OK ,  fld,  E_d,    OK ,  ERR,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-/*  DD   */  Idn,  Idn,  ERR,    ERR,  ERR,  ERR,  ERR,    17,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-
-/* SLH   */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-
-/* !     */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,      blk,  OK ,  OK ,  OK ,
-/* *     */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  blk,  OK ,      OK ,  OK ,  OK ,  OK ,
-/* <     */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  ERR,   18,      blk,   24,  ERR,  OK ,
-/* =     */  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  ERR,  ERR,      blk,  ERR,  ERR,  OK ,
-
-
-/* Sbs   */  OK ,  Sbo,  ERR,    ERR,  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-/* So1   */  ERR,  Sbo,  ERR,    ERR,  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-/* So2   */  ERR,  STR,  ERR,    ERR,  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-
-/* E_d   */  ERR,  Edg,  ERR,    Edg,  ERR,  ERR,  ERR,    ERR,  ERR,  ERR,  ERR,     ERR,  ERR,  ERR,  ERR,
-/* Edg   */  OK ,  Edg,  OK ,    OK ,  OK ,  OK ,  OK ,    OK ,  OK ,  OK ,  OK ,     OK ,  OK ,  OK ,  OK ,
-
-
+/*Str*/{0,  OK , Str, Str, Str, Str, Str, OK ,   Str, Str, Str, Str, Str, Str, Str, Str,   Str, Str, Str, Str, Str, Str, Str, Str,   Str, Str, Str, Str, Str, Str, Str, Str},
+/*ASG*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   Lnk, OK , OK , OK , OK , OK , OK , OK },
+/*App*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
+/*Lnk*/{0,  OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK ,   OK , OK , OK , OK , OK , OK , OK , OK },
 };
