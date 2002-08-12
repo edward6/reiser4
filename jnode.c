428a2ac1874c50c423357793dea7b3d6d5eb91f8
namesys.com!green <namesys.com!green>
2002-08-12 05:13:18 +0000

    Auto merged
    2002/08/12 09:12:41+04:00 namesys.com!green
    Silent the assertion, because it is a little bit invalid now.
    
    (Logical change 1.1877)

--- jnode.c
+++ jnode.c
@@ -840,7 +840,7 @@ int jdelete( jnode *node /* jnode to finish with */ )
 
 	trace_stamp( TRACE_ZNODES );
 	assert( "nikita-467", node != NULL );
-	assert( "nikita-2123", JF_ISSET( node, JNODE_HEARD_BANSHEE ) );
+	assert( "nikita-2123", JF_ISSET( node, JNODE_HEARD_BANSHEE ) || !jnode_page(node));
 
 	trace_on( TRACE_PCACHE, "delete node: %p\n", node );
 
