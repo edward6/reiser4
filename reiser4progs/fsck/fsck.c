77312eebcf0c692650aab335667a0d4281a4c788
namesys.com!reiser <namesys.com!reiser>
2002-08-22 07:13:16 +0000

    mvdir
    (Logical change 1.1992)

---
+++ reiser4progs/fsck/fsck.c
@@ -0,0 +1,228 @@
+/*
+    fsck.c -- reiser4 filesystem checking and recovering program.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h> 
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
+
+#include <reiser4/reiser4.h>
+#include <reiser4progs/misc.h>
+
+static void fsck_print_usage(void) {
+    fprintf(stderr, "Usage: fsck.reiser4 [ options ] FILE\n");
+    
+    fprintf(stderr, "Options:\n"
+	"  -v | --version                 prints current version.\n"
+	"  -u | -h | --usage | --help     prints program usage.\n"
+	"  -q | --quiet                   forces filesystem check without\n"
+	"                                 warning message.\n"
+	"  -p | --profile                 profile to be used for recovering.\n"
+	"  -k | --known-profiles          prints known profiles.\n"
+	"  -w | --without-replaying       do not replay the journal.\n"
+	"  -c | --check                   check the filesystem (default).\n"
+	"  -r | --rebuild                 rebuild internal tree.\n");
+}
+
+int main(int argc, char *argv[]) {
+    int c, error, force = 0, replay = 1;
+    int check = 0, rebuild = 0;
+    char *host_dev, *profile_label = "default40";
+    
+    reiserfs_fs_t *fs;
+    aal_device_t *device;
+    reiserfs_profile_t *profile;
+    
+    static struct option long_options[] = {
+	{"version", no_argument, NULL, 'v'},
+	{"usage", no_argument, NULL, 'u'},
+	{"help", no_argument, NULL, 'h'},
+	{"quiet", no_argument, NULL, 'q'},
+	{"profile", no_argument, NULL, 'p'},
+	{"known-profiles", no_argument, NULL, 'k'},
+	{"without-replaying", no_argument, NULL, 'w'},
+	{"check", no_argument, NULL, 'c'},
+	{"rebuild", no_argument, NULL, 'r'},
+	{0, 0, 0, 0}
+    };
+    
+    if (argc < 2) {
+	fsck_print_usage();
+	return 0xfe;
+    }
+
+    while ((c = getopt_long_only(argc, argv, "uhvp:kqcr", long_options, 
+	(int *)0)) != EOF) 
+    {
+	switch (c) {
+	    case 'u': 
+	    case 'h': {
+		fsck_print_usage();
+		return 0;
+	    }
+	    case 'v': {
+		printf("%s %s\n", argv[0], VERSION);
+		return 0;
+	    }
+	    case 'q': {
+		force = 1;
+		break;
+	    }
+	    case 'p': {
+		profile_label = optarg;
+		break;
+	    }
+	    case 'k': {
+		reiser4progs_list_profile();
+		return 0;
+	    }
+	    case 'w': {
+		replay = 0;
+	        break;
+	    }
+	    case 'c': {
+		check = 1;
+	        break;
+	    }
+	    case 'r': {
+		rebuild = 1;
+	        break;
+	    }
+	    case '?': {
+	        fsck_print_usage();
+	        return 0xfe;
+	    }
+	}
+    }
+
+    if (optind >= argc) {
+	fsck_print_usage();
+	return 0xfe;
+    }
+    
+    if (!(profile = reiser4progs_find_profile(profile_label))) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't find profile by its label \"%s\".", profile_label);
+	return 0xff;
+    }
+    
+    if (!check && !rebuild)
+	check = 1;
+    
+    host_dev = argv[optind++];
+    
+    /* Checking given device for validness */
+    if (!reiser4progs_misc_dev_check(host_dev)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Device %s doesn't exists or invalid.", host_dev);
+	return 0xfe;
+    }
+
+    if (!(device = aal_file_open(host_dev, REISERFS_DEFAULT_BLOCKSIZE, O_RDWR))) {
+	char *error = strerror(errno);
+	
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't open device %s. %s.", host_dev, error);
+	goto error;
+    }
+    
+    if (libreiser4_init()) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't initialize libreiser4.");
+	goto error_free_device;
+    }
+    
+    if (check && rebuild) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Invalid mode. Please select one of --check or --rebuild.");
+	goto error_free_libreiser4;
+    }
+    
+    if (rebuild) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Sorry, filesystem rebuilding not supported yet!");
+	goto error_free_libreiser4;
+    }
+    
+    if (!force) {
+	if (!(c = reiser4progs_misc_choose_propose("ynYN", "Please select (y/n) ", 
+		"Do you realy want to run filesystem check on %s? (y/n) ", host_dev)))
+	    goto error_free_libreiser4;
+	
+	if (c == 'n' || c == 'N')
+	    goto error_free_libreiser4;
+    }
+
+    /*
+	Most probably filesystem will not be openable, due to incorrect 
+	control structures. So, we should add one more argument to 
+	reiserfs_fs_open function, which will force it do not any checks 
+	on control structures.
+    */
+    if (!(fs = reiserfs_fs_open(device, device, replay))) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't open filesystem on %s.", host_dev);
+	goto error_free_libreiser4;
+    }
+    
+    if (check)
+	fprintf(stderr, "Checking reiser4 with \"%s\" profile...", profile->label);
+    else
+	fprintf(stderr, "Rebuilding reiser4 with \"%s\" profile...", profile->label);
+    
+    fflush(stderr);
+
+    
+    if (check) {
+	if (reiserfs_fs_check(fs)) {
+	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+		"Can't check filesystem on %s.", host_dev);
+	    goto error_free_fs;
+	}
+    } else {
+	/* Rebuilding will be here */
+    }
+
+    fprintf(stderr, "done\n");
+
+    fprintf(stderr, "Synchronizing...");
+    
+    if (reiserfs_fs_sync(fs)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't synchronize created filesystem.");
+	goto error_free_fs;
+    }
+
+    if (aal_device_sync(device)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't synchronize device %s.", aal_device_name(device));
+	goto error_free_fs;
+    }
+
+    fprintf(stderr, "done\n");
+    
+    reiserfs_fs_close(fs);
+    libreiser4_done();
+    aal_file_close(device);
+    
+    return 0;
+
+error_free_fs:
+    reiserfs_fs_close(fs);
+error_free_libreiser4:
+    libreiser4_done();
+error_free_device:
+    aal_file_close(device);
+error:
+    return 0xff;
+}
+
---
+++ reiser4progs/mkfs/mkfs.c
@@ -0,0 +1,244 @@
+/*
+    mkfs.c -- the program to create reiser4 filesystem.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h> 
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <errno.h>
+#include <string.h>
+
+#include <reiser4/reiser4.h>
+#include <reiser4progs/misc.h>
+
+static void mkfs_print_usage(void) {
+    fprintf(stderr, "Usage: mkfs.reiser4 [ options ] FILE [ size[K|M|G] ]\n");
+    
+    fprintf(stderr, "Options:\n"
+	"  -v | --version                 prints current version.\n"
+	"  -u | -h | --usage | --help     prints program usage.\n"
+	"  -q | --quiet                   forces creating filesystem without\n"
+	"  -p | --profile                 profile to be used.\n"
+	"                                 warning message.\n"
+	"  -k | --known-profiles          prints known profiles.\n"
+	"  -b N | --block-size=N          block size, 4096 by default,\n"
+	"                                 other are not supported for awhile.\n"
+	"  -l LABEL | --label=LABEL       volume label lets to mount\n"
+	"                                 filesystem by its label.\n"
+	"  -d UUID | --uuid=UUID          universally unique identifier.\n");
+}
+
+int main(int argc, char *argv[]) {
+    int c, error, force = 0;
+    char uuid[17], label[17];
+    count_t fs_len = 0, dev_len = 0;
+    char *host_dev, *profile_label = "default40";
+    uint16_t blocksize = REISERFS_DEFAULT_BLOCKSIZE;
+    
+    reiserfs_fs_t *fs;
+    aal_device_t *device;
+    reiserfs_profile_t *profile;
+
+    static struct option long_options[] = {
+	{"version", no_argument, NULL, 'v'},
+	{"usage", no_argument, NULL, 'u'},
+	{"help", no_argument, NULL, 'h'},
+	{"profile", required_argument, NULL, 'p'},
+	{"known-profiles", no_argument, NULL, 'k'},
+	{"quiet", no_argument, NULL, 'q'},
+	{"block-size", required_argument, NULL, 'b'},
+	{"label", required_argument, NULL, 'l'},
+	{"uuid", required_argument, NULL, 'i'},
+	{0, 0, 0, 0}
+    };
+    
+    if (argc < 2) {
+	mkfs_print_usage();
+	return 0xfe;
+    }
+    
+    memset(uuid, 0, sizeof(uuid));
+    memset(label, 0, sizeof(label));
+    
+    while ((c = getopt_long_only(argc, argv, "uhvp:qkb:i:l:", long_options, 
+	(int *)0)) != EOF) 
+    {
+	switch (c) {
+	    case 'u': 
+	    case 'h': {
+		mkfs_print_usage();
+		return 0;
+	    }
+	    case 'v': {
+		printf("%s %s\n", argv[0], VERSION);
+		return 0;
+	    }
+	    case 'p': {
+		profile_label = optarg;
+		break;
+	    }
+	    case 'q': {
+		force = 1;
+		break;
+	    }
+	    case 'k': {
+		reiser4progs_list_profile();
+		return 0;
+	    }
+	    case 'b': {
+	        if (!(blocksize = (uint16_t)reiser4progs_misc_strtol(optarg, &error)) && error) {
+		    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+		        "Invalid blocksize (%s).", optarg);
+		    return 0xfe;
+		}
+		if (!aal_pow_of_two(blocksize)) {
+		    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_CANCEL, 
+			"Invalid block size %u. It must power of two.", (uint16_t)blocksize);
+		    return 0xfe;	
+		}
+		break;
+	    }
+	    case 'i': {
+		if (strlen(optarg) < 16) {
+		    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+			"Invalid uuid (%s).", optarg);
+		    return 0xfe;
+		}
+		strncpy(uuid, optarg, sizeof(uuid) - 1);
+		break;
+	    }
+	    case 'l': {
+		strncpy(label, optarg, sizeof(label) - 1);
+	        break;
+	    }
+	    case '?': {
+	        mkfs_print_usage();
+	        return 0xfe;
+	    }
+	}
+    }
+
+    if (optind >= argc) {
+	mkfs_print_usage();
+	return 0xfe;
+    }
+    
+    if (!(profile = reiser4progs_find_profile(profile_label))) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Can't find profile by its label \"%s\".", profile_label);
+	return 0xff;
+    }
+    
+    host_dev = argv[optind++];
+    
+    if (optind < argc) {
+	char *len_str = argv[optind];
+		
+	aal_exception_fetch_all();
+	if (!reiser4progs_misc_dev_check(host_dev)) {
+	    char *tmp = host_dev;
+	    host_dev = len_str;
+	    len_str = tmp;
+	}
+	aal_exception_leave_all();
+		
+	if (!(fs_len = (reiser4progs_misc_size_parse(len_str, 
+	    &error) / blocksize)) && error) 
+	{
+	    aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+		"Invalid filesystem size (%s).", len_str);
+	    return 0xfe;
+	}
+    }
+    
+    /* Checking given device for validness */
+    if (!reiser4progs_misc_dev_check(host_dev)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Device %s doesn't exists or invalid.", host_dev);
+	return 0xfe;
+    }
+    
+    if (!(device = aal_file_open(host_dev, blocksize, O_RDWR))) {
+	char *error = strerror(errno);
+	
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't open device %s. %s.", host_dev, error);
+	goto error;
+    }
+    
+    if (libreiser4_init()) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't initialize libreiser4.");
+	goto error_free_device;
+    }
+
+    dev_len = aal_device_len(device);
+    
+    if (!fs_len)
+        fs_len = dev_len;
+	
+    if (fs_len > dev_len) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK, 
+	    "Filesystem size is too big for device %llu blocks long.", dev_len);
+	goto error_free_libreiser4;
+    }
+
+    if (!force) {
+	if (!(c = reiser4progs_misc_choose_propose("ynYN", "Please select (y/n) ", 
+		"All data on %s will be lost (y/n) ", host_dev)))
+	    goto error_free_libreiser4;
+	
+	if (c == 'n' || c == 'N')
+	    goto error_free_libreiser4;
+    }
+
+    fprintf(stderr, "Creating reiser4 with \"%s\" profile...", profile->label);
+    fflush(stderr);
+    
+    if (!(fs = reiserfs_fs_create(profile, device, blocksize, 
+	(const char *)uuid, (const char *)label, fs_len, device, NULL))) 
+    {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't create filesystem on %s.", aal_device_name(device));
+	goto error_free_libreiser4;
+    }
+    fprintf(stderr, "done\n");
+
+    fprintf(stderr, "Synchronizing...");
+    
+    if (reiserfs_fs_sync(fs)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't synchronize created filesystem.");
+	goto error_free_fs;
+    }
+
+    if (aal_device_sync(device)) {
+	aal_exception_throw(EXCEPTION_ERROR, EXCEPTION_OK,
+	    "Can't synchronize device %s.", aal_device_name(device));
+	goto error_free_fs;
+    }
+
+    fprintf(stderr, "done\n");
+    
+    reiserfs_fs_close(fs);
+    libreiser4_done();
+    aal_file_close(device);
+    
+    return 0;
+
+error_free_fs:
+    reiserfs_fs_close(fs);
+error_free_libreiser4:
+    libreiser4_done();
+error_free_device:
+    aal_file_close(device);
+error:
+    return 0xff;
+}
