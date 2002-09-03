fa5146bfa3f22e51143ba5bdb793f2ca98ab8692
namesys.com!vitaly <namesys.com!vitaly>
2002-09-03 18:10:53 +0000

    2002/08/30 23:01:46+04:00 namesys.com!vitaly
    (Logical change 1.2055)

---
+++ reiser4progs/include/progs/profile.h
@@ -0,0 +1,26 @@
+/*
+    profile.h -- headers of methods for working with profiles in reiser4 programs.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Vitaly Fertman.
+*/
+
+#ifndef PROGS_PROFILE_H
+#define PROGS_PROFILE_H
+
+#ifndef HAVE_CONFIG_H
+#   include <config.h>
+#endif
+
+#include <reiser4/reiser4.h>
+
+extern reiserfs_profile_t *progs_profile_find(const char *profile);
+extern void reiserfs_profile_list(void);
+extern reiserfs_profile_t *progs_profile_get_default();
+
+extern void progs_profile_print_list(void);
+extern void progs_plugins_print(); 
+extern int progs_profile_override_plugin_id_by_name(reiserfs_profile_t *profile, 
+    const char *plugin_type_name, const char *plugin_label);
+extern void progs_profile_print(reiserfs_profile_t *profile); 
+
+#endif
