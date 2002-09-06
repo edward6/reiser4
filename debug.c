6795e575ac5a209ab0a4b5fec25b2b4b6f2aa75a
namesys.com!vs <namesys.com!vs>
2002-09-06 17:22:22 +0000

    reiser4_print_stats: added statistic about number of reads for unformatted nodes
    (Logical change 1.2079)

--- debug.c
+++ debug.c
@@ -254,6 +254,9 @@ void reiser4_print_stats()
 	      s -> file.pointers,
 	      s -> file.find_items,
 	      s -> file.full_find_items );
+	info( "extent:\n"
+	      "\t read unformatted nodes:\t %lu\n",
+	      s -> extent.unfm_block_reads );
 
 	info( "flush:\n"
 	      "\t squeeze:\t %lu\n"
