b282613932cd0b860f9cf6e70026aa275a0b029e
namesys.com!umka <namesys.com!umka>
2002-09-06 18:57:34 +0000

    A lot of chnagesm cleanups and comments.
    2002/09/06 22:10:15+04:00 namesys.com!umka
    mvdir
    
    (Logical change 1.2082)

--- reiser4progs/include/progs/misc.h
+++ reiser4progs/include/progs/misc.h
@@ -1,31 +1,63 @@
 /*
-    misc.h -- miscellaneous useful tools for reiser4 progs.
-    Copyright (C) 1996 - 2002 Hans Reiser
-    Author Vitaly Fertman.
+    misc.h -- reiser4progs common include.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
 */
 
-#ifndef PROGS_MISC_H
-#define PROGS_MISC_H
+#ifndef REISER4_MISC_H
+#define REISER4_MISC_H
 
-#ifndef HAVE_CONFIG_H
-#   include <config.h>
+#include <aal/aal.h>
+#include <reiser4/filesystem.h>
+
+#include <linux/major.h>
+
+#ifndef MAJOR
+#  define MAJOR(rdev) ((rdev) >> 8)
+#  define MINOR(rdev) ((rdev) & 0xff)
 #endif
 
-#include <errno.h>
-#include <mntent.h>
-#include <stdlib.h>
-#include <string.h>
-#include <utime.h>
-#include <sys/stat.h>
-#include <sys/vfs.h>
+#ifndef SCSI_DISK_MAJOR
+#  define SCSI_DISK_MAJOR(maj) ((maj) == SCSI_DISK0_MAJOR || \
+	((maj) >= SCSI_DISK1_MAJOR && (maj) <= SCSI_DISK7_MAJOR))
+#endif
 
-#include <progs/progs.h>
+#ifndef SCSI_BLK_MAJOR
+#  define SCSI_BLK_MAJOR(maj) (SCSI_DISK_MAJOR(maj) || \
+	(maj) == SCSI_CDROM_MAJOR)
+#endif
 
-extern long long int progs_misc_strtol(const char *str);
+#ifndef IDE_DISK_MAJOR
+#  ifdef IDE9_MAJOR
+#  define IDE_DISK_MAJOR(maj) ((maj) == IDE0_MAJOR || \
+	(maj) == IDE1_MAJOR || (maj) == IDE2_MAJOR || \
+	(maj) == IDE3_MAJOR || (maj) == IDE4_MAJOR || \
+	(maj) == IDE5_MAJOR || (maj) == IDE6_MAJOR || \
+	(maj) == IDE7_MAJOR || (maj) == IDE8_MAJOR || \
+	(maj) == IDE9_MAJOR)
+#  else
+#  define IDE_DISK_MAJOR(maj) ((maj) == IDE0_MAJOR || \
+	(maj) == IDE1_MAJOR || (maj) == IDE2_MAJOR || \
+	(maj) == IDE3_MAJOR || (maj) == IDE4_MAJOR || \
+	(maj) == IDE5_MAJOR)
+#  endif
+#endif
+	
+extern long int progs_misc_strtol(const char *str, int *error);
 
-extern long long int progs_misc_size_parse(const char *str);
+extern int progs_misc_dev_mounted(const char *name, 
+    const char *ops);
 
-extern int progs_is_mounted(const char *filename);
-extern int progs_is_mounted_ro(const char *filename);
+extern int progs_misc_choose_check(const char *chooses, int choose);
+extern int progs_misc_choose_propose(const char *chooses, 
+    const char *error, const char *format, ...) __check_format__(printf, 3, 4);
+
+extern int progs_misc_size_check(const char *str);
+extern unsigned long long progs_misc_size_parse(const char *str, 
+    int *error);
+
+extern reiserfs_profile_t *progs_misc_profile_find(const char *profile);
+extern void progs_misc_profile_list(void);
 
 #endif
+
