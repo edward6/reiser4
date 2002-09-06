c08c5bf31d91bf5121c7553e8470339539250b9a
namesys.com!umka <namesys.com!umka>
2002-09-06 18:57:34 +0000

    Delete: reiser4progs/include/progs/version.h
    }(Logical change 1.2082)

--- reiser4progs/include/progs/version.h
+++
@@ -1,7 +0,0 @@
-/*
-    version.h -- version information of reiser4progs.
-    Copyright (C) 1996 - 2002 Hans Reiser
-    Author Vitaly Fertman
-*/
-
-#define BANNER(prog_name) "\n%s, 2002 (%s %s)\n\n", prog_name, PACKAGE, VERSION
