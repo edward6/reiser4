d92d4d50f80bcce19898b53d1323b9aa33e881e3
namesys.com!umka <namesys.com!umka>
2002-09-10 15:55:35 +0000

    Support for stat data extentions was added. sdext_unix plugin was added in order to support unix stat data extention.
    2002/09/10 17:41:52+04:00 namesys.com!umka
    
    (Logical change 1.2112)

---
+++ reiser4progs/plugin/sdext_unix/sdext_unix.h
@@ -0,0 +1,46 @@
+/*
+    sdext_unix.h -- stat data exception plugin, that implements unix stat data 
+    fields.
+    
+    Copyright 1996-2002 (C) Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef SDEXT_UNIX_H
+#define SDEXT_UNIX_H
+
+struct reiserfs_sdext_unix {
+    uint32_t uid;
+    uint32_t gid;
+    uint32_t atime;
+    uint32_t mtime;
+    uint32_t ctime;
+    uint32_t rdev;
+    uint64_t bytes;
+};
+
+typedef struct reiserfs_sdext_unix reiserfs_sdext_unix_t;
+
+#define sdext_unix_get_uid(ext)		aal_get_le32(ext, uid)
+#define sdext_unix_set_uid(ext, val)	aal_set_le32(ext, uid, val)
+
+#define sdext_unix_get_gid(ext)		aal_get_le32(ext, gid)
+#define sdext_unix_set_gid(ext, val)	aal_set_le32(ext, gid, val)
+
+#define sdext_unix_get_atime(ext)	aal_get_le32(ext, atime)
+#define sdext_unix_set_atime(ext, val)	aal_set_le32(ext, atime, val)
+
+#define sdext_unix_get_mtime(ext)	aal_get_le32(ext, mtime)
+#define sdext_unix_set_mtime(ext, val)	aal_set_le32(ext, mtime, val)
+
+#define sdext_unix_get_ctime(ext)	aal_get_le32(ext, ctime)
+#define sdext_unix_set_ctime(ext, val)	aal_set_le32(ext, ctime, val)
+
+#define sdext_unix_get_rdev(ext)	aal_get_le32(ext, rdev)
+#define sdext_unix_set_rdev(ext, val)	aal_set_le32(ext, rdev, val)
+
+#define sdext_unix_get_bytes(ext)	aal_get_le64(ext, bytes)
+#define sdext_unix_set_bytes(ext, val)	aal_set_le64(ext, bytes, val)
+
+#endif
+
