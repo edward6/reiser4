1a60a9e48387e6e8a368374bf9cb3c72d724520e
namesys.com!umka <namesys.com!umka>
2002-09-11 19:56:57 +0000

    Small fixes.
    (Logical change 1.2128)

--- reiser4progs/plugin/dir40/dir40.c
+++ reiser4progs/plugin/dir40/dir40.c
@@ -437,13 +437,13 @@ static errno_t dir40_add(reiserfs_dir40_t *dir,
     item.type = REISERFS_CDE_ITEM;
     item.hint = &direntry_hint;
    
+    /* FIXME-UMKA: These magical digits are here for test purposes */
     libreiser4_plugin_call(goto error_free_entry, dir->key.plugin->key_ops, 
 	build_generic_short, &entry->objid, KEY40_STATDATA_MINOR, 
 	1000, 1001);
 	
     libreiser4_plugin_call(goto error_free_entry, dir->key.plugin->key_ops, 
 	build_entry_short, &entry->entryid, dir->hash_plugin, entry->name);
-    
     
     item.key.plugin = dir->key.plugin;
     
