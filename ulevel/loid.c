c207a29bcd6c9f9b68eea664f28654e4f3701949
namesys.com!god <namesys.com!god>
2002-09-17 16:41:15 +0000

    (Logical change 1.2168)

---
+++ ulevel/loid.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+
+#define MAX_LEN   (20)
+#define CYCLE     (20000)
+
+#define RAT( a, b ) ( ( ( double ) ( a ) ) / ( ( double ) ( b ) ) )
+int main( int argc, char **argv )
+{
+  const char alphabet[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  unsigned long i;
+  char name[ MAX_LEN + 1 ];
+  int min;
+  int base;
+  time_t start;
+  time_t instant;
+  unsigned long prev;
+  int ebusy;
+  
+  base = strlen( alphabet );
+  memset( name, base + 10, MAX_LEN + 1 );
+  ebusy = 0;
+  prev = 0;
+  instant = start = time( NULL );
+
+  for( i = 0 ; ; ++ i )
+	{
+	  int j;
+	  int c;
+	  int fd;
+	  char fname[ MAX_LEN + 1 ];
+
+	  for( j = MAX_LEN - 1, c = 1 ; ( j >= 0 ) && c ; -- j )
+		{
+		  c = 0;
+		  if( name[ j ] == base + 10 )
+			{
+			  name[ j ] = 1;
+			  min = j;
+			}
+		  else if( name[ j ] == base - 1 )
+			{
+			  c = 1;
+			  name[ j ] = 0;
+			}
+		  else
+			{
+			  name[ j ] ++;
+			}
+		}
+	  if( c == 1 )
+		{
+		  exit( 1 );
+		}
+	  for( j = min ; j < MAX_LEN ; ++ j )
+		{
+		  fname[ j - min ] = alphabet[ ( int ) name[ j ] ];
+		}
+	  fname[ MAX_LEN - min ] = 0;
+	  fd = open( fname, O_CREAT, 0777 );
+	  if( fd == -1 )
+		{
+		  if( errno != EBUSY )
+			{
+			  perror( "open" );
+			  printf( "%li files created\n", i );
+			  exit( 2 );
+			}
+		  else
+			{
+			  ebusy ++;
+			}
+		}
+	  close( fd );
+	  if( ( i % CYCLE ) == 0 )
+		{
+		  time_t now;
+
+		  now = time( NULL );
+		  printf( "%li files: %li (%f/%f), %i\n", i, time( NULL ) - instant,
+				  ( now - start ) ? RAT( i, now - start ) : 0.0, 
+				  ( now - instant ) ? RAT( i - prev, now - instant ) : 0.0,
+				  ebusy );
+		  instant = time( NULL );
+		  prev = i;
+		}
+	}
+}
---
+++ ulevel/readdir.c
@@ -0,0 +1,43 @@
+#define _LARGEFILE64_SOURCE
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <errno.h>
+
+int main( int argc, char **argv )
+{
+  DIR             *directory;
+  struct dirent64 *dentry;
+  int              do_stat;
+
+  directory = opendir( argv[ 1 ] );
+  do_stat   = atoi( argv[ 2 ] );
+  if( directory != NULL ) 
+	{
+	  while( ( dentry = readdir64( directory ) ) != NULL )
+		{
+		  puts( dentry -> d_name );
+		  if( do_stat ) {
+			struct stat buf;
+
+			if( stat( dentry -> d_name, &buf ) == -1 )
+			  {
+				perror( "stat" );
+				break;
+			  }
+		  }
+		}
+	  if( errno != 0 )
+		  perror( "readdir" );
+	  closedir( directory );
+    }
+  else 
+	{
+	  perror( "cannot opendir" );
+	}
+}
