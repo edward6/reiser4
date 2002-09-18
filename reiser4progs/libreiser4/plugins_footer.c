a4b9a9d7b0d62d0eaf44e858ef5c663affb8b4f9
namesys.com!umka <namesys.com!umka>
2002-09-18 12:45:35 +0000

    Delete: reiser4progs/libreiser4/plugins_footer.c
    }(Logical change 1.2174)

--- reiser4progs/libreiser4/plugins_footer.c
+++
@@ -1,9 +0,0 @@
-/*
-    plugins_footer.c -- this just defines __plugin_end symbol at the 
-    end of .plugins ELF-section. It is needed for monolithic building.
-    Copyright (C) 1996-2002 Hans Reiser.
-    Author Yury Umanets.
-*/
-
-unsigned long int __plugin_end __attribute__((__section__(".plugins"))) = 0;
-
