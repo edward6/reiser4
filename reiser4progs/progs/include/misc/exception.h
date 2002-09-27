67206b0a41aa304e740780084512430fbf4b41b3
namesys.com!umka <namesys.com!umka>
2002-09-27 18:46:48 +0000

    Rename: reiser4progs/include/progs/exception.h -> reiser4progs/progs/include/misc/exception.h
    (Logical change 1.2240)

---
+++ reiser4progs/progs/include/misc/exception.h
@@ -0,0 +1,17 @@
+/*
+    exception.h -- common for all progs exception functions.
+    Copyright 1996-2002 (C) Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef PROGS_EXCEPTION
+#define PROGS_EXCEPTION
+
+#include <aal/exception.h>
+
+extern aal_exception_option_t progs_exception_handler(aal_exception_t *exception);
+extern void progs_exception_set_stream(aal_exception_type_t type, void *stream);
+extern void *progs_exception_get_stream(aal_exception_type_t type);
+
+#endif
+
