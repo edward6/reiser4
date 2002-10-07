df5a96ce3c4cf9ecb8680505a5f45a34b31a53fc
namesys.com!umka <namesys.com!umka>
2002-10-07 13:44:52 +0000

    A lot of cleanups. All plugin entities has pointer to their plugin. All item related fucntions were moved into item.c. Fixes for compact mode. Removed dependencies from node level in all lookup and balancing fucntions.
    2002/10/07 12:13:15+04:00 namesys.com!umka
    
    (Logical change 1.2295)

---
+++ reiser4progs/include/reiser4/item.h
@@ -0,0 +1,42 @@
+/*
+    item.h -- common item functions.
+    Copyright 1996-2002 (C) Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef ITEM_H
+#define ITEM_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <reiser4/filesystem.h>
+
+extern errno_t reiser4_item_init(reiser4_item_t *item, 
+    reiser4_node_t *node, reiser4_pos_t *pos);
+
+extern errno_t reiser4_item_open(reiser4_item_t *item, 
+    reiser4_node_t *node, reiser4_pos_t *pos);
+
+extern uint32_t reiser4_item_count(reiser4_item_t *item);
+
+#ifndef ENABLE_COMPACT
+
+extern errno_t reiser4_item_estimate(reiser4_item_t *item,
+    reiser4_item_hint_t *hint);
+
+extern errno_t reiser4_item_pointo(reiser4_item_t *item,
+    blk_t blk); 
+
+#endif
+
+extern blk_t reiser4_item_target(reiser4_item_t *item);
+extern int reiser4_item_internal(reiser4_item_t *item);
+
+extern uint32_t reiser4_item_len(reiser4_item_t *item);
+extern reiser4_body_t *reiser4_item_body(reiser4_item_t *item);
+extern reiser4_plugin_t *reiser4_item_plugin(reiser4_item_t *item);
+
+#endif
+
