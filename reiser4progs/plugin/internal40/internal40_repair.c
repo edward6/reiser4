d3fe1637951ff07585c6bc5e1420016231dff07b
namesys.com!vitaly <namesys.com!vitaly>
2002-10-13 16:54:34 +0000

    2002/10/12 23:28:34+04:00 namesys.com!vitaly
    (Logical change 1.2394)

---
+++ reiser4progs/plugin/internal40/internal40_repair.c
@@ -0,0 +1,12 @@
+/*
+    internal40_repair.c -- reiser4 default internal item plugin.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Vitaly Fertman.
+*/
+
+#include "internal40.h"
+
+errno_t internal40_check(reiser4_body_t *body, uint16_t options) {
+    return 0;
+}
+
