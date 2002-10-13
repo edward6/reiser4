b3eac724b41ab670576439964df9af6e7c41175d
namesys.com!vitaly <namesys.com!vitaly>
2002-10-13 16:54:34 +0000

    2002/10/12 23:29:07+04:00 namesys.com!vitaly
    (Logical change 1.2394)

---
+++ reiser4progs/plugin/direntry40/direntry40_repair.c
@@ -0,0 +1,11 @@
+/*
+    direntry40_repair.c -- reiser4 default direntry plugin.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Vitaly Fertman.
+*/
+
+#include "direntry40.h"
+
+errno_t direntry40_check(reiser4_body_t *body, uint16_t options) {
+    return 0;
+}
