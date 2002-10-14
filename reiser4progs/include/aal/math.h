4f6f46889741e43a562bb09b16882b5c9d8ec6ea
namesys.com!umka <namesys.com!umka>
2002-10-14 17:46:03 +0000

    Added adler32 in libaal.
    (Logical change 1.2412)

--- reiser4progs/include/aal/math.h
+++ reiser4progs/include/aal/math.h
@@ -10,6 +10,7 @@
 extern int aal_pow_of_two(unsigned long n);
 extern int aal_log2(unsigned long n);
 extern long long int aal_fact(int64_t n);
+extern unsigned int aal_adler32(char *buff, unsigned int n);
 
 #endif
 
