572bf83be5e0dfea968c0214808818bbe3069273
namesys.com!green <namesys.com!green>
2002-10-18 14:18:13 +0000

    some oops fix from zam
    (Logical change 1.2470)

--- vfs_ops.c
+++ vfs_ops.c
@@ -2349,7 +2349,7 @@ int reiser4_releasepage( struct page *page, int gfp UNUSED_ARG )
 	 */
 	if( atomic_read( &node -> d_count ) || jnode_is_loaded( node ) ||
 	    blocknr_is_fake( jnode_get_block( node ) ) || 
-	    jnode_is_dirty( node ) || JF_ISSET( node, JNODE_OVRWR ) ) {
+	    jnode_is_dirty( node ) || JF_ISSET( node, JNODE_OVRWR ) ||			    JF_ISSET( node, JNODE_WRITEBACK) ) {
 		spin_unlock_jnode( node );
 		return 0;
 	}
