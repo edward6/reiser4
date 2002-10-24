378bd222f9db98c9e7cb3e3c0d71cf0060896e84
namesys.com!vitaly <namesys.com!vitaly>
2002-10-24 17:39:57 +0000

    merge
    2002/10/23 23:03:50+04:00 namesys.com!umka
    A lot of changes in file estimates methods.
    
    2002/10/23 20:41:05+04:00 namesys.com!umka
    Added estimate methods for read, write and truncate methods.
    
    (Logical change 1.2534)

--- plugin/file/file.h
+++ plugin/file/file.h
@@ -51,6 +51,16 @@ typedef enum {
 } write_mode;
 
 write_mode how_to_write(coord_t *, lock_handle *, const reiser4_key *);
+/* plugin->file.estimate.*
+   methods */
+reiser4_block_nr unix_file_estimate_write(struct inode *inode, size_t count, 
+    loff_t *off);
+
+reiser4_block_nr unix_file_estimate_read(struct inode *inode, size_t count);
+reiser4_block_nr unix_file_estimate_truncate(struct inode *inode, size_t count);
+reiser4_block_nr unix_file_estimate_mmap(struct inode *inode, size_t count);
+reiser4_block_nr unix_file_estimate_release(struct inode *inode);
+  
 
 /* plugin->file.*
    required by VFS */
