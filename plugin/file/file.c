cc4d671169898db51ff65397e15ac7780033887d
namesys.com!umka <namesys.com!umka>
2002-10-24 23:15:28 +0000

    Fixes.
    (Logical change 1.2552)

--- plugin/file/file.c
+++ plugin/file/file.c
@@ -1782,9 +1782,10 @@ reiser4_block_nr unix_file_estimate_write(struct inode *inode, size_t count,
     file_size = inode->i_size;
     new_file_size = *off + count > file_size ? *off + count : file_size;
     
-    return tail_plugin->estimate(inode, new_file_size, 0/*is_fake*/) + 
+    return tail_plugin->estimate(inode, new_file_size - file_size, 0/*is_fake*/) + 
 	    inode_file_plugin(inode)->estimate.update(inode) + 1;
 }
+
 /* plugin->u.file.write */
 ssize_t
 unix_file_write(struct file * file,	/* file to write to */
