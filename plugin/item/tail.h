b5cf9c876d54890821018687e0f64d909d5513be
namesys.com!vs <namesys.com!vs>
2002-10-28 14:23:11 +0000

    obsolete unused code removed read/write/convertion: cleanup, fixes, simplifications
    2002/10/25 18:46:30+04:00 namesys.com!vs
    file_read: unnecessary usage of seals - removed
    
    2002/10/25 01:06:08+04:00 botik.ru!grev
    Cleanuping code: Set maximum line length for non-comment lines to 120.
    
    (Logical change 1.2574)

--- plugin/item/tail.h
+++ plugin/item/tail.h
@@ -14,22 +14,18 @@
 #include <linux/fs.h>		/* for struct inode */
 
 reiser4_key *tail_max_key_inside(const coord_t *, reiser4_key *);
-int tail_can_contain_key(const coord_t * coord,
-			 const reiser4_key * key, const reiser4_item_data *);
+int tail_can_contain_key(const coord_t * coord, const reiser4_key * key, const reiser4_item_data *);
 int tail_mergeable(const coord_t * p1, const coord_t * p2);
 unsigned tail_nr_units(const coord_t *);
 lookup_result tail_lookup(const reiser4_key *, lookup_bias, coord_t *);
 int tail_paste(coord_t *, reiser4_item_data *, carry_plugin_info *);
 int tail_can_shift(unsigned free_space, coord_t * source,
-		   znode * target, shift_direction,
-		   unsigned *size, unsigned want);
+		   znode * target, shift_direction, unsigned *size, unsigned want);
 void tail_copy_units(coord_t * target, coord_t * source,
-		     unsigned from, unsigned count,
-		     shift_direction, unsigned free_space);
+		     unsigned from, unsigned count, shift_direction, unsigned free_space);
 int tail_cut_units(coord_t * item, unsigned *from,
 		   unsigned *to,
-		   const reiser4_key * from_key,
-		   const reiser4_key * to_key, reiser4_key * smallest_removed);
+		   const reiser4_key * from_key, const reiser4_key * to_key, reiser4_key * smallest_removed);
 reiser4_key *tail_unit_key(const coord_t * coord, reiser4_key * key);
 reiser4_key *tail_max_key(const coord_t * coord, reiser4_key * key);
 int tail_key_in_item(coord_t * coord, const reiser4_key * key);
@@ -37,8 +33,8 @@ int tail_key_in_item(coord_t * coord, const reiser4_key * key);
 /*
  * plugin->u.item.s.*
  */
-int tail_write(struct inode *, struct sealed_coord *, flow_t *);
-int tail_read(struct inode *, struct sealed_coord *, flow_t *);
+int tail_write(struct inode *, coord_t *, lock_handle *, flow_t *);
+int tail_read(struct inode *, coord_t *, flow_t *);
 
 /* __REISER4_TAIL_H__ */
 #endif
