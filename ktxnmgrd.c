143bbcaf352f7f11c4eacb4f74fc608be503d65c
namesys.com!god <namesys.com!god>
2002-10-28 15:44:42 +0000

    cleanup scan_mgr()
    2002/10/28 17:06:50+03:00 namesys.com!god
    use not-NULL nr_submitted to call flush_one_atom()
    
    (Logical change 1.2576)

--- ktxnmgrd.c
+++ ktxnmgrd.c
@@ -281,20 +281,16 @@ scan_mgr(txn_mgr * mgr)
 	assert("nikita-2456", tree->super != NULL);
 
 	{
+		long nr_submitted;
+
 		REISER4_ENTRY(tree->super);
 
 		ret = commit_one_atom(mgr);
 
-		if (ret)
-			goto out;
-
-		if (need_flush(mgr)) {
-			ret = flush_one_atom(mgr, NULL, JNODE_FLUSH_WRITE_BLOCKS);
-
-			if (ret)
-				goto out;
+		if (!ret && need_flush(mgr)) {
+			ret = flush_one_atom(mgr, &nr_submitted, 
+					     JNODE_FLUSH_WRITE_BLOCKS);
 		}
-out:
 		REISER4_EXIT(ret);
 	}
 }
