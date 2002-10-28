9ec72ffa95c9137ec76e9e9d5da3788b0a7c1160
namesys.com!vitaly <namesys.com!vitaly>
2002-10-28 14:23:11 +0000

    Auto merged
    2002/10/28 15:26:00+03:00 namesys.com!vs
    cleanup
    one more moving zload/zrelse around
    update_sd_if_necessary
    
    2002/10/25 16:54:16+04:00 namesys.com!vs
    cleanup
    
    2002/10/25 01:06:03+04:00 botik.ru!grev
    Cleanuping code: Set maximum line length for non-comment lines to 120.
    
    (Logical change 1.2574)

--- plugin/file/file.h
+++ plugin/file/file.h
@@ -14,34 +14,26 @@
 #include "../../seal.h"
 
 #include <linux/types.h>
-#include <linux/fs.h> /* for struct file  */
-#include <linux/mm.h> /* for struct page */
-#include <linux/buffer_head.h> /* for struct buffer_head */
+#include <linux/fs.h>		/* for struct file  */
+#include <linux/mm.h>		/* for struct page */
+#include <linux/buffer_head.h>	/* for struct buffer_head */
 
 void get_exclusive_access(struct inode *inode);
 void drop_exclusive_access(struct inode *inode);
 void get_nonexclusive_access(struct inode *inode);
 void drop_nonexclusive_access(struct inode *inode);
+void nea2ea(struct inode *);
+void ea2nea(struct inode *);
+
 int tail2extent(struct inode *inode);
 int extent2tail(struct file *file);
-int unix_file_writepage_nolock_locked_page(struct page *page);
+int unix_file_writepage_nolock(struct page *page);
 int find_next_item(struct sealed_coord *, const reiser4_key *, coord_t *,
 		   lock_handle *, znode_lock_mode, __u32 cbk_flags);
 void set_hint(struct sealed_coord *, const reiser4_key *, const coord_t *);
 void unset_hint(struct sealed_coord *hint);
 int hint_is_set(const struct sealed_coord *hint);
-int hint_validate(struct sealed_coord *,
-		  const reiser4_key *, coord_t *, lock_handle *);
-int coord_set_properly(const reiser4_key * key, coord_t * coord);
-reiser4_key *get_next_item_key(const coord_t *, reiser4_key *);
-void check_coord(const coord_t *, const reiser4_key *);
-int no_left_neighbor(const znode * node);
-int znode_contains_key_lock_unique(znode * node /* znode to look in */ ,
-				   const reiser4_key *
-				   key /* key to look for */ );
-int less_than_ldk(znode * node, const reiser4_key * key);
-int equal_to_rdk(znode * node, const reiser4_key * key);
-int less_than_rdk(znode * node, const reiser4_key * key);
+int hint_validate(struct sealed_coord *, const reiser4_key *, coord_t *, lock_handle *);
 
 typedef enum {
 	FIRST_ITEM = 1,
@@ -68,16 +60,14 @@ int unix_file_truncate(struct inode *, loff_t size);
 int unix_file_readpage(struct file *, struct page *);
 int unix_file_writepage(struct page *);
 ssize_t unix_file_read(struct file *, char *buf, size_t size, loff_t * off);
-ssize_t unix_file_write(struct file *, const char *buf, size_t size,
-			loff_t * off);
+int update_sd_if_necessary(struct inode *inode, const flow_t *f);
+ssize_t unix_file_write(struct file *, const char *buf, size_t size, loff_t * off);
 int unix_file_release(struct file *);
 int unix_file_mmap(struct file *, struct vm_area_struct *vma);
-int unix_file_get_block(struct inode *, sector_t block,
-			struct buffer_head *bh_result, int create);
+int unix_file_get_block(struct inode *, sector_t block, struct buffer_head *bh_result, int create);
 
 int unix_file_key_by_inode(struct inode *, loff_t off, reiser4_key *);
-int unix_file_create(struct inode *object, struct inode *parent,
-		     reiser4_object_create_data * data);
+int unix_file_create(struct inode *object, struct inode *parent, reiser4_object_create_data * data);
 int unix_file_owns_item(const struct inode *, const coord_t *);
 int unix_file_setattr(struct inode *inode, struct iattr *attr);
 
