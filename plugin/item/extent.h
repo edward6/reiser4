6fb1f4d7d0153f15ed4452b3caef132901b62425
namesys.com!god <namesys.com!god>
2002-10-28 14:23:11 +0000

    Auto merged
    2002/10/28 15:13:17+03:00 namesys.com!god
    fix off-by-one errors in disk space counting
    
    2002/10/26 20:25:32+04:00 namesys.com!vs
    obsolete unused code removed
    read/write/convertion: cleanup, fixes, simplifications
    
    2002/10/25 18:46:30+04:00 namesys.com!vs
    file_read: unnecessary usage of seals - removed
    
    2002/10/25 01:06:06+04:00 botik.ru!grev
    Cleanuping code: Set maximum line length for non-comment lines to 120.
    
    (Logical change 1.2574)

--- plugin/item/extent.h
+++ plugin/item/extent.h
@@ -50,9 +50,7 @@ static inline void
 extent_set_start(reiser4_extent * ext, reiser4_block_nr start)
 {
 	cassert(sizeof (ext->start) == 8);
-	assert("nikita-2510",
-	       ergo(start > 1,
-		    start < reiser4_block_count(reiser4_get_current_sb())));
+	assert("nikita-2510", ergo(start > 1, start < reiser4_block_count(reiser4_get_current_sb())));
 	cpu_to_dblock(start, &ext->start);
 }
 
@@ -60,11 +58,10 @@ static inline void
 extent_set_width(reiser4_extent * ext, reiser4_block_nr width)
 {
 	cassert(sizeof (ext->width) == 8);
+	cpu_to_dblock(width, &ext->width);
 	assert("nikita-2511",
 	       ergo(extent_get_start(ext) > 1,
-		    extent_get_start(ext) + width <
-		    reiser4_block_count(reiser4_get_current_sb())));
-	cpu_to_dblock(width, &ext->width);
+		    extent_get_start(ext) + width <= reiser4_block_count(reiser4_get_current_sb())));
 }
 
 #define extent_item(coord) ((reiser4_extent *)item_body_by_coord (coord))
@@ -75,42 +72,30 @@ extent_set_width(reiser4_extent * ext, reiser4_block_nr width)
  * plugin->item.common.*
  */
 reiser4_key *extent_max_key_inside(const coord_t *, reiser4_key *);
-int extent_can_contain_key(const coord_t * coord,
-			   const reiser4_key * key, const reiser4_item_data *);
+int extent_can_contain_key(const coord_t * coord, const reiser4_key * key, const reiser4_item_data *);
 int extent_mergeable(const coord_t * p1, const coord_t * p2);
 unsigned extent_nr_units(const coord_t *);
 lookup_result extent_lookup(const reiser4_key *, lookup_bias, coord_t *);
 int extent_init(coord_t *, reiser4_item_data *);
 int extent_paste(coord_t *, reiser4_item_data *, carry_plugin_info *);
 int extent_can_shift(unsigned free_space,
-		     coord_t * source,
-		     znode * target, shift_direction,
-		     unsigned *size, unsigned want);
+		     coord_t * source, znode * target, shift_direction, unsigned *size, unsigned want);
 void extent_copy_units(coord_t * target,
 		       coord_t * source,
-		       unsigned from, unsigned count,
-		       shift_direction where_is_free_space,
-		       unsigned free_space);
-int extent_kill_item_hook(const coord_t *, unsigned from,
-			  unsigned count, void *kill_params);
+		       unsigned from, unsigned count, shift_direction where_is_free_space, unsigned free_space);
+int extent_kill_item_hook(const coord_t *, unsigned from, unsigned count, void *kill_params);
 int extent_create_hook(const coord_t * coord, void *arg);
 int extent_cut_units(coord_t *, unsigned *from,
 		     unsigned *to,
-		     const reiser4_key * from_key,
-		     const reiser4_key * to_key,
-		     reiser4_key * smallest_removed);
+		     const reiser4_key * from_key, const reiser4_key * to_key, reiser4_key * smallest_removed);
 int extent_kill_units(coord_t *, unsigned *from,
 		      unsigned *to,
-		      const reiser4_key * from_key,
-		      const reiser4_key * to_key,
-		      reiser4_key * smallest_removed);
+		      const reiser4_key * from_key, const reiser4_key * to_key, reiser4_key * smallest_removed);
 reiser4_key *extent_unit_key(const coord_t * coord, reiser4_key * key);
-int extent_item_data_by_flow(const coord_t *, const flow_t *,
-			     reiser4_item_data *);
+int extent_item_data_by_flow(const coord_t *, const flow_t *, reiser4_item_data *);
 void extent_print(const char *, coord_t *);
 int extent_utmost_child(const coord_t * coord, sideof side, jnode ** child);
-int extent_utmost_child_real_block(const coord_t * coord,
-				   sideof side, reiser4_block_nr * block);
+int extent_utmost_child_real_block(const coord_t * coord, sideof side, reiser4_block_nr * block);
 reiser4_key *extent_max_key(const coord_t * coord, reiser4_key * key);
 int extent_key_in_item(coord_t * coord, const reiser4_key * key);
 int extent_key_in_unit(const coord_t * coord, const reiser4_key * key);
@@ -120,23 +105,20 @@ int extent_check(const coord_t * coord, const char **error);
 /*
  * plugin->u.item.s.file.*
  */
-int extent_write(struct inode *, struct sealed_coord *, flow_t *);
-int extent_read(struct inode *, struct sealed_coord *, flow_t *);
+int extent_write(struct inode *, coord_t *, lock_handle *, flow_t *);
+int extent_read(struct inode *, coord_t *, flow_t *);
 int extent_readpage(coord_t *, lock_handle *, struct page *page);
 int extent_writepage(coord_t *, lock_handle *, struct page *);
 #if 0
 int extent_page_cache_readahead(struct file *file, coord_t * coord,
 				lock_handle * lh UNUSED_ARG,
-				unsigned long start_page,
-				unsigned long intrafile_readahead_amount);
+				unsigned long start_page, unsigned long intrafile_readahead_amount);
 #endif
 
 /* these are used in flush.c
  * FIXME-VS: should they be somewhere in item_plugin? */
-int allocate_extent_item_in_place(coord_t *, lock_handle *,
-				  flush_position * pos);
-int allocate_and_copy_extent(znode * left, coord_t * right,
-			     flush_position * pos, reiser4_key * stop_key);
+int allocate_extent_item_in_place(coord_t *, lock_handle *, flush_position * pos);
+int allocate_and_copy_extent(znode * left, coord_t * right, flush_position * pos, reiser4_key * stop_key);
 
 int extent_is_unallocated(const coord_t * item);	/* True if this extent is unallocated (i.e., not a hole, not allocated). */
 __u64 extent_unit_index(const coord_t * item);	/* Block offset of this unit. */
