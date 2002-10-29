86216ade645fc9b0702a78b0f9ce177ff07f71e8
namesys.com!umka <namesys.com!umka>
2002-10-29 09:46:17 +0000

    Fixes in estimating file write for tail40 policy
    (Logical change 1.2588)

--- plugin/tail_policy.c
+++ plugin/tail_policy.c
@@ -46,8 +46,8 @@ reiser4_block_nr never_tail_estimate ( const struct inode *inode, loff_t size,
 	if (is_fake) {
 	    reiser4_block_nr amount;
 	    
-	    /* In the case of unallocated allocated extent (truncate does) we are 
-	     * counting only overhead for one balancing */
+	    /* In the case of unallocated extent (truncate does) we are counting 
+	     * only overhead for one balancing */
 	    estimate_internal_amount(1, tree_by_inode(inode)->height, &amount);
 	    return inode_file_plugin(inode)->estimate.update(inode) + amount;
 	} else {
