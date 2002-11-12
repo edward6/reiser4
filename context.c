ca2acb1b39e3d98b9c7eadf0a84b192b375adcb6
namesys.com!god <namesys.com!god>
2002-11-12 17:35:51 +0000

    (Logical change 1.2704)

---
+++ context.c
@@ -0,0 +1,215 @@
+/* Copyright 2002 by Hans Reiser, licensing governed by reiser4/README */
+
+/* Manipulation of reiser4_context */
+
+#include "debug.h"
+#include "super.h"
+#include "context.h"
+
+#include <linux/writeback.h> /* balance_dirty_pages() */
+
+#if REISER4_DEBUG
+/* This list and the two fields that follow maintain the currently active
+   contexts, used for debugging purposes.  */
+
+spinlock_t active_contexts_lock;
+context_list_head active_contexts;
+#endif
+
+/* initialise context and bind it to the current thread
+  
+   This function should be called at the beginning of reiser4 part of
+   syscall.
+*/
+int
+init_context(reiser4_context * context	/* pointer to the reiser4 context
+					 * being initalised */ ,
+	     struct super_block *super	/* super block we are going to
+					 * work with */ )
+{
+	reiser4_tree *tree;
+	reiser4_super_info_data *sdata;
+
+	assert("nikita-2662", !no_context);
+
+	if (context == NULL || super == NULL) {
+		BUG();
+	}
+
+	if (super->s_op != NULL && super->s_op != &reiser4_super_operations)
+		BUG();
+
+	xmemset(context, 0, sizeof *context);
+
+
+	if (is_in_reiser4_context()) {
+		reiser4_context *parent;
+
+		grab_space_enable();
+		parent = (reiser4_context *) current->fs_context;
+		if (parent->super == super) {
+			context->parent = parent;
+#if (REISER4_DEBUG)
+			++context->parent->nr_children;
+#endif
+			return 0;
+		}
+	}
+	sdata = get_super_private(super);
+	assert("nikita-2727", sdata != NULL);
+	tree = &sdata->tree;
+
+	context->super = super;
+#if (REISER4_DEBUG)
+	context->tid = set_current();
+#endif
+	assert("green-7", super->s_op == NULL || super->s_op == &reiser4_super_operations);
+
+	context->magic = context_magic;
+	context->outer = current->fs_context;
+	current->fs_context = (struct fs_activation *) context;
+
+	init_lock_stack(&context->stack);
+
+	txn_begin(context);
+
+	context->parent = context;
+	tap_list_init(&context->taps);
+#if REISER4_DEBUG
+	context_list_clean(context);	/* to satisfy assertion */
+	spin_lock(&active_contexts_lock);
+	context_list_check(&active_contexts);
+	context_list_push_front(&active_contexts, context);
+	spin_unlock(&active_contexts_lock);
+	context->task = current;
+#endif
+
+	grab_space_enable();
+	if (sdata->fake && !(current->flags & PF_MEMALLOC))
+		/*
+		 * FIXME-ZAM: temporary
+		 */
+		balance_dirty_pages(sdata->fake->i_mapping);
+
+	return 0;
+}
+
+reiser4_context *
+get_context_by_lock_stack(lock_stack * owner)
+{
+	return (reiser4_context *) ((char *) owner - (int) (&((reiser4_context *) 0)->stack));
+}
+
+/* release resources associated with context.
+  
+   This function should be called at the end of "session" with reiser4,
+   typically just before leaving reiser4 driver back to VFS.
+  
+   This is good place to put some degugging consistency checks, like that
+   thread released all locks and closed transcrash etc.
+  
+*/
+/* Audited by: umka (2002.06.16) */
+void
+done_context(reiser4_context * context /* context being released */ )
+{
+	reiser4_context *parent;
+	assert("nikita-860", context != NULL);
+	parent = context->parent;
+	assert("nikita-2174", parent != NULL);
+	assert("nikita-2093", parent == parent->parent);
+	assert("nikita-859", parent->magic == context_magic);
+	assert("vs-646", (reiser4_context *) current->fs_context == parent);
+	assert("zam-686", !no_context);
+	/* add more checks here */
+
+	if (parent == context) {
+		assert("jmacd-673", parent->trans == NULL);
+		assert("jmacd-1002", lock_stack_isclean(&parent->stack));
+		assert("nikita-1936", no_counters_are_held());
+		assert("nikita-2626", tap_list_empty(taps_list()));
+
+		if (context->grabbed_blocks != 0) {
+			trace_on(TRACE_RESERVE, "%llu grabbed blocks "
+				 "were not freed, free them now.\n", 
+				 context->grabbed_blocks);
+			all_grabbed2free();
+		}
+
+		
+#if REISER4_DEBUG
+		/* remove from active contexts */
+		spin_lock(&active_contexts_lock);
+		context_list_remove(parent);
+		spin_unlock(&active_contexts_lock);
+
+		assert("zam-684", context->nr_children == 0);
+#endif
+		current->fs_context = context->outer;
+	} else {
+#if REISER4_DEBUG
+		parent->nr_children--;
+		assert("zam-685", parent->nr_children >= 0);
+#endif
+	}
+}
+
+/* Audited by: umka (2002.06.16) */
+int
+init_context_mgr(void)
+{
+#if REISER4_DEBUG
+	spin_lock_init(&active_contexts_lock);
+	context_list_init(&active_contexts);
+#endif
+	return 0;
+}
+
+#if REISER4_DEBUG_OUTPUT
+void
+print_context(const char *prefix, reiser4_context * context)
+{
+	if (context == NULL) {
+		info("%s: null context\n", prefix);
+		return;
+	}
+	info("%s: trace_flags: %x, tid: %i\n", prefix, context->trace_flags, context->tid);
+#if REISER4_DEBUG
+	print_lock_counters("\tlocks", &context->locks);
+	info("pid: %i, comm: %s\n", context->task->pid, context->task->comm);
+#endif
+	print_lock_stack("\tlock stack", &context->stack);
+	info_atom("\tatom", context->trans_in_ctx.atom);
+}
+
+#if REISER4_DEBUG
+void
+print_contexts(void)
+{
+	reiser4_context *context;
+
+	spin_lock(&active_contexts_lock);
+
+	for (context = context_list_front(&active_contexts);
+	     !context_list_end(&active_contexts, context); context = context_list_next(context)) {
+
+		print_context("context", context);
+	}
+
+	spin_unlock(&active_contexts_lock);
+}
+#endif
+#endif
+
+
+
+/* Make Linus happy.
+   Local variables:
+   c-indentation-style: "K&R"
+   mode-name: "LC"
+   c-basic-offset: 8
+   tab-width: 8
+   fill-column: 120
+   scroll-step: 1
+   End:
+*/
---
+++ context.h
@@ -0,0 +1,232 @@
+/* Copyright 2001, 2002 by Hans Reiser, licensing governed by reiser4/README */
+
+/* Reiser4 context */
+
+#if !defined( __REISER4_CONTEXT_H__ )
+#define __REISER4_CONTEXT_H__
+
+#include "forward.h"
+#include "debug.h"
+#include "spin_macros.h"
+#include "dformat.h"
+#include "tslist.h"
+#include "plugin/node/node.h"
+#include "plugin/plugin.h"
+#include "jnode.h"
+#include "znode.h"
+#include "tap.h"
+
+#include <linux/types.h>	/* for __u??  */
+#include <linux/fs.h>		/* for struct super_block  */
+#include <linux/spinlock.h>
+#include <linux/sched.h>	/* for struct task_struct */
+
+/* list of active lock stacks */
+ON_DEBUG(TS_LIST_DECLARE(context);)
+
+typedef enum {
+	FLUSH_MODE	    =   (1 << 0),
+	GRAB_ENABLED        =	(1 << 1)
+} context_flags_t;
+
+/* global context used during system call. Variable of this type is
+   allocated on the stack at the beginning of the reiser4 part of the
+   system call and pointer to it is stored in the
+   current->fs_context. This allows us to avoid passing pointer to
+   current transaction and current lockstack (both in one-to-one mapping
+   with threads) all over the call chain.
+
+   It's kind of like those global variables the prof used to tell you
+   not to use in CS1, except thread specific.;-) Nikita, this was a
+   good idea.
+*/
+struct reiser4_context {
+	/* magic constant. For debugging */
+	__u32 magic;
+
+	/* current lock stack. See lock.[ch]. This is where list of all
+	   locks taken by current thread is kept. This is also used in
+	   deadlock detection. */
+	lock_stack stack;
+
+	/* current transcrash. */
+	txn_handle *trans;
+	txn_handle trans_in_ctx;
+
+	/* super block we are working with.  To get the current tree
+	   use &get_super_private (reiser4_get_current_sb ())->tree. */
+	struct super_block *super;
+
+	/* parent fs activation */
+	struct fs_activation *outer;
+
+	/* per-thread grabbed (for further allocation) blocks counter */
+	reiser4_block_nr grabbed_blocks;
+
+	reiser4_block_nr flush_reserved;
+	/* per-thread tracing flags. Use reiser4_trace_flags enum to set
+	   bits in it. */
+	__u32 trace_flags;
+
+	/* parent context */
+	reiser4_context *parent;
+	tap_list_head taps;
+	long                  flags;
+	int		      grab_enabled;
+	    
+#if REISER4_DEBUG
+	/* thread ID */
+	__u32 tid;
+
+	/* A link of all active contexts. */
+	context_list_link contexts_link;
+	lock_counters_info locks;
+	int nr_children;	/* number of child contexts */
+	struct task_struct *task;	/* so we can easily find owner of the stack */
+#endif
+#if REISER4_DEBUG_NODE
+	int disable_node_check;
+#endif
+};
+
+#if REISER4_DEBUG
+TS_LIST_DEFINE(context, reiser4_context, contexts_link);
+#endif
+
+extern reiser4_context *get_context_by_lock_stack(lock_stack *);
+
+/* Debugging helps. */
+extern int init_context_mgr(void);
+#if REISER4_DEBUG_OUTPUT
+extern void print_context(const char *prefix, reiser4_context * ctx);
+#else
+#define print_context(p,c) noop
+#endif
+
+#if REISER4_DEBUG_OUTPUT && REISER4_DEBUG
+extern void print_contexts(void);
+#else
+#define print_contexts() noop
+#endif
+
+/* Hans, is this too expensive? */
+#define current_tree (&(get_super_private(reiser4_get_current_sb())->tree))
+#define current_blocksize reiser4_get_current_sb()->s_blocksize
+#define current_blocksize_bits reiser4_get_current_sb()->s_blocksize_bits
+
+extern int init_context(reiser4_context * context, struct super_block *super);
+extern void done_context(reiser4_context * context);
+
+/* magic constant we store in reiser4_context allocated at the stack. Used to
+   catch accesses to staled or uninitialized contexts. */
+#define context_magic ((__u32) 0x4b1b5d0b)
+
+static inline int
+is_in_reiser4_context(void)
+{
+	return current->fs_context != NULL && ((__u32) current->fs_context->owner) == context_magic;
+}
+
+/* return context associated with given thread */
+static inline reiser4_context *
+get_context(const struct task_struct *tsk)
+{
+	if (tsk == NULL) {
+		BUG();
+	}
+
+	return (reiser4_context *) tsk->fs_context;
+}
+
+/* return context associated with current thread */
+static inline reiser4_context *
+get_current_context(void)
+{
+	reiser4_context *context;
+
+	context = get_context(current);
+	if (context != NULL)
+		return context->parent;
+	else
+		return NULL;
+}
+
+static inline int is_flush_mode(void)
+{
+	return get_current_context()->flags &   FLUSH_MODE;
+}
+
+static inline void flush_mode(void)
+{
+	get_current_context()->flags |=  FLUSH_MODE;
+}
+
+static inline void not_flush_mode(void)
+{
+	get_current_context()->flags &= ~FLUSH_MODE;
+}
+
+static inline void grab_space_enable(void) 
+{
+	get_current_context()->flags |= GRAB_ENABLED;
+}
+
+static inline void grab_space_disable(void) 
+{
+	get_current_context()->flags &= ~GRAB_ENABLED;
+}
+
+static inline int is_grab_enabled(void)
+{
+	return get_current_context()->flags & GRAB_ENABLED;
+}
+
+#define __REISER4_ENTRY( super, errret )			\
+	reiser4_context __context;				\
+	do {							\
+                int __ret;					\
+                __ret = init_context( &__context, ( super ) );	\
+                if (__ret != 0) {				\
+			return errret;				\
+		}						\
+        } while (0)
+
+#define REISER4_ENTRY_PTR( super )  __REISER4_ENTRY( super, ERR_PTR(__ret) )
+#define REISER4_ENTRY( super )      __REISER4_ENTRY( super, __ret )
+
+#define __REISER4_EXIT( context )		\
+({						\
+        int __ret1 = txn_end( context );	\
+	done_context( context );		\
+        if (__ret1 > 0) __ret1 = 0;             \
+        __ret1;					\
+})
+
+#define REISER4_EXIT( ret_exp ) 		                       \
+({						                       \
+	typeof ( ret_exp ) __result = ( ret_exp );                     \
+        int __ret = __REISER4_EXIT( &__context );                      \
+	return __result ? : __ret;		                       \
+})
+
+#define REISER4_EXIT_PTR( ret_exp ) 		                       \
+({						                       \
+	typeof ( ret_exp ) __result = ( ret_exp );                     \
+        int __ret = __REISER4_EXIT( &__context );                      \
+	return IS_ERR (__result) ? __result : ERR_PTR (__ret);         \
+})
+
+
+/* __REISER4_CONTEXT_H__ */
+#endif
+
+/* Make Linus happy.
+   Local variables:
+   c-indentation-style: "K&R"
+   mode-name: "LC"
+   c-basic-offset: 8
+   tab-width: 8
+   fill-column: 120
+   scroll-step: 1
+   End:
+*/
