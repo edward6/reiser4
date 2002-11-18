b9c9a0536202f9eaa3de077ecf1460f962c52374
namesys.com!vs <namesys.com!vs>
2002-11-18 15:39:41 +0000

    estimation API
    (Logical change 1.2743)

--- tree.h
+++ tree.h
@@ -502,6 +502,12 @@ jput(jnode * node)
 	}
 }
 
+/* estimate api. Implementation is in estimate.c */
+reiser4_block_nr estimate_internal_amount(reiser4_block_nr childen, tree_level);
+reiser4_block_nr estimate_one_insert_item(tree_level);
+reiser4_block_nr estimate_one_insert_into_item(tree_level);
+reiser4_block_nr estimate_one_item_removal(tree_level);
+
 /* __REISER4_TREE_H__ */
 #endif
 
