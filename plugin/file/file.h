f5250600f8a78562c1c626e9c4c29ac696a8ba79
namesys.com!vs <namesys.com!vs>
2002-11-27 12:32:16 +0000

    update_inode_and_sd: additional parameter
    2002/11/23 13:12:58+03:00 namesys.com!vs
    delete method of regular file plugin
    
    2002/11/22 12:27:31+03:00 namesys.com!vs
    estimating code changes
    
    (Logical change 1.2804)

--- plugin/file/file.h
+++ plugin/file/file.h
@@ -23,6 +23,7 @@ int extent2tail(struct file *file);
 int unix_file_writepage_nolock(struct page *page);
 int find_next_item(struct sealed_coord *, const reiser4_key *, coord_t *,
 		   lock_handle *, znode_lock_mode, __u32 cbk_flags);
+int goto_right_neighbor(coord_t *, lock_handle *);
 void set_hint(struct sealed_coord *, const reiser4_key *, coord_t *);
 void unset_hint(struct sealed_coord *hint);
 int hint_is_set(const struct sealed_coord *hint);
@@ -57,13 +58,14 @@ int unix_file_truncate(struct inode *, loff_t size);
 int unix_file_readpage(struct file *, struct page *);
 int unix_file_writepage(struct page *);
 ssize_t unix_file_read(struct file *, char *buf, size_t size, loff_t * off);
-int update_sd_if_necessary(struct inode *inode, const flow_t *f);
+int update_inode_and_sd_if_necessary(struct inode *inode, loff_t, int);
 ssize_t unix_file_write(struct file *, const char *buf, size_t size, loff_t * off);
 int unix_file_release(struct file *);
 int unix_file_mmap(struct file *, struct vm_area_struct *vma);
 int unix_file_get_block(struct inode *, sector_t block, struct buffer_head *bh_result, int create);
 
 int unix_file_key_by_inode(struct inode *, loff_t off, reiser4_key *);
+int unix_file_delete(struct inode *);
 int unix_file_create(struct inode *object, struct inode *parent, reiser4_object_create_data * data);
 int unix_file_owns_item(const struct inode *, const coord_t *);
 int unix_file_setattr(struct inode *inode, struct iattr *attr);
