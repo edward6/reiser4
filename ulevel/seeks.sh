440b930295693d1f10bad46de63e825af1c993be
namesys.com!god <namesys.com!god>
2002-12-02 12:31:34 +0000

    add getopt() support for seeks.sh script
    (Logical change 1.2831)

--- ulevel/seeks.sh
+++ ulevel/seeks.sh
@@ -1,7 +1,34 @@
 #! /bin/sh
 
-START=$1
-END=$2
+OPTVAL=`getopt -o s:e:t: -n 'seeks.sh' -- "$@"`
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$OPTVAL"
+
+while true ;do
+	case "$1" in
+		-s)
+			START=$2
+			shift 2
+		;;
+		-e) 
+			END=$2
+			shift 2
+		;;
+		-t) 
+			TITLE=$2
+			shift 2
+		;;
+		--) 
+			shift 
+			break 
+		;;
+		*) 
+			echo "Internal error!" 
+			exit 1 
+		;;
+	esac
+done
 
 if [ $START ] ;then
 	XRANGE="[$START:$END]"
@@ -9,10 +36,17 @@ else
 	XRANGE=""
 fi
 
+if [ $TITLE ]; then
+	XTITLE="title \"$TITLE\""
+else
+	XTITLE=""
+fi
+
 (
-	echo 'set terminal postscript;'
-#	echo 'clear;'
-	echo 'set data style lines;'
-	echo "plot $XRANGE '-'"
+	echo "set terminal postscript;"
+#	echo "clear;"
+	echo "set data style lines;"
+	echo "set label \"generated on `date +%Y-%m-%d` by `whoami` at `uname -a`\" at graph -0.1,-0.07"
+	echo "plot $XRANGE '-' $XTITLE"
 	bio-out.sh
 ) | gnuplot #| gv -landscape -
