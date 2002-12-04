ef9b687e2b96429a2aba8b0399361422d79f06dc
namesys.com!god <namesys.com!god>
2002-12-04 13:43:19 +0000

    fix bio-out.sh
    (Logical change 1.2843)

--- ulevel/bio-out.sh
+++ ulevel/bio-out.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 egrep '\.\.\.\.\.\.bio' | \
-awk 'BEGIN {cnt=1} { split($9, pair, "[(,)]"); for(i=0 ; i<pair[3] ; ++i) print cnt " " pair[2]+i " " $7; cnt +=1; }' 
+awk 'BEGIN {cnt=1} { split($9, pair, "[(,)]"); for(i=0 ; i<pair[3] ; ++i) { print cnt " " pair[2]+i " " $7; cnt +=1; } }' 
