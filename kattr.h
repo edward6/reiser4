096ecc5cdeba435577daf6d32f064c42ed608fdb
namesys.com!god <namesys.com!god>
2002-12-17 11:29:11 +0000

    sysfs cleanups
    (Logical change 1.2892)

--- kattr.h
+++ kattr.h
@@ -5,10 +5,11 @@
 #if !defined( __REISER4_KATTR_H__ )
 #define __REISER4_KATTR_H__
 
+extern int reiser4_sysfs_init_all(void);
+extern void reiser4_sysfs_done_all(void);
+
 extern int  reiser4_sysfs_init(struct super_block *super);
 extern void reiser4_sysfs_done(struct super_block *super);
-extern int  reiser4_sysfs_init_all(void);
-extern void reiser4_sysfs_done_all(void);
 
 /* __REISER4_KATTR_H__ */
 #endif
