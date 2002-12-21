4db8357d756c6900dbaa5132c68ec06e02bdf7f7
namesys.com!vs <namesys.com!vs>
2002-12-21 14:22:41 +0000

    readdir readahead: handling readahead options
    (Logical change 1.2908)

--- readahead.c
+++ readahead.c
@@ -2,32 +2,41 @@
 
 #include "tree.h"
 #include "tree_walk.h"
+#include "super.h"
 
 /*
- * FIXME-VS: no sure about this
+ * there are three "types" of readahead:
+ * 1. readahead for unformatted nodes
+ * 2. readahead for directories: it is implemented via zloa
+ * 3. 
  */
-#define MAX_READDIR_RA (256)
 
 void
 readdir_readahead(znode *node, ra_info_t *info)
 {
+	ra_params_t *ra_params;
 	znode *next;
 	int i;
 
-	if (znode_get_level(node) != LEAF_LEVEL)
+	assert("vs-1143", !blocknr_is_fake(node));
+
+	ra_params = get_current_super_ra_params();
+
+	if (ra_params->leaves_only && znode_get_level(node) != LEAF_LEVEL)
 		/*
 		 * FIXME-VS: for now only for leaf 
 		 */
 		return;
 
-	i = 0;
 	jstartio(ZJNODE(node));
+	i = 1;
+
 	next = zref(node);
 	while (1) {
 		lock_handle lh;
 		int grn_flags;
 
-		if (++i >= MAX_READDIR_RA)
+		if (i >= ra_params->max)
 			break;
 		if (UNDER_SPIN(dk, ZJNODE(next)->tree, (get_key_locality(znode_get_rd_key(next)) != info->u.readdir.oid ||
 							get_key_type(znode_get_rd_key(next)) >= KEY_BODY_MINOR)))
@@ -37,10 +46,20 @@ readdir_readahead(znode *node, ra_info_t *info)
 		grn_flags = GN_DO_READ; /* read all necessary internals */
 		if (reiser4_get_right_neighbor(&lh, next, ZNODE_READ_LOCK, grn_flags))
 			break;
+		if (blocknr_is_fake(znode_get_block(lh.node))) {
+			done_lh(&lh);
+			break;
+		}
+		if (ra_params->adjacent_only && *znode_get_block(lh.node) != *znode_get_block(next) + 1) {
+			done_lh(&lh);
+			break;
+		}
+
 		zput(next);
 		next = zref(lh.node);
 		done_lh(&lh);
 		jstartio(ZJNODE(next));
+		i ++;
 	}
 	zput(next);
 }
