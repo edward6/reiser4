d50fa00f433dd98169b8c36c1e67fd6ab9ca814a
namesys.com!zam <namesys.com!zam>
2002-12-26 13:18:29 +0000

    another fix for e-flush off.
    (Logical change 1.2934)

--- plugin/item/extent.c
+++ plugin/item/extent.c
@@ -2062,6 +2062,7 @@ unflush_finish_part(oid_t oid, unsigned long ind, __u64 count)
 #define unflush_finish(coord, done) noop
 #define unflush(coord) (0)
 #define unflush_finish_part(oid, ind, count) noop
+#define unflush_part(oid, ind, count) (0)
 #endif
 
 /* @right is extent item. @left is left neighbor of @right->node. Copy item
