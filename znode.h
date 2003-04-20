71c3eb75fc55be4fee899c49981351b618de924c
namesys.com!god <namesys.com!god>
2003-04-20 18:23:33 +0000

    bad block support: znode_relocate()
    (Logical change 1.3399)

--- znode.h
+++ znode.h
@@ -306,6 +306,8 @@ extern z_hash_table *znode_get_htable(const znode *node);
 
 extern __u64 znode_build_version(reiser4_tree * tree);
 
+extern int znode_relocate(znode * node, reiser4_block_nr * blk);
+
 /* Data-handles.  A data handle object manages pairing calls to zload() and zrelse().  We
    must load the data for a node in many places.  We could do this by simply calling
    zload() everywhere, the difficulty arises when we must release the loaded data by
