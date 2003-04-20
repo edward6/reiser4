5fe1928c5ecaac3797da90e4f7d3531f9310b576
namesys.com!edward <namesys.com!edward>
2003-04-20 18:18:46 +0000

    Remove old cluster cache support
    2003/02/12 21:27:28+03:00 namesys.com!edward
    Rename: plugin/crypto_compressed.h -> plugin/cryptcompress.h
    
    (Logical change 1.3137)

---
+++ plugin/cryptcompress.h
@@ -0,0 +1,14 @@
+/* Copyright 2002 by Hans Reiser, licensing governed by reiser4/README */
+
+#define MIN_CLUSTER_SIZE 4096
+
+/* Make Linus happy.
+   Local variables:
+   c-indentation-style: "K&R"
+   mode-name: "LC"
+   c-basic-offset: 8
+   tab-width: 8
+   fill-column: 120
+   scroll-step: 1
+   End:
+*/
