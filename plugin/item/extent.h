44fc8e34742b83765e8566d759441b6f3641ca2c
namesys.com!vs <namesys.com!vs>
2003-04-20 18:21:54 +0000

    Auto merged
    2003/03/21 12:12:25+03:00 namesys.com!vs
    extent code update
    
    2003/03/20 14:49:26+03:00 namesys.com!vs
    cleanup
    
    (Logical change 1.3311)

--- plugin/item/extent.h
+++ plugin/item/extent.h
@@ -55,8 +55,8 @@ extent_set_width(reiser4_extent * ext, reiser4_block_nr width)
 #define extent_by_coord(coord) (extent_item (coord) + (coord)->unit_pos)
 #define width_by_coord(coord) extent_get_width (extent_by_coord(coord))
 
-/* plugin->item.common.* */
-reiser4_key *extent_max_key_inside(const coord_t *, reiser4_key *);
+/* plugin->u.item.b.* */
+reiser4_key *extent_max_key_inside(const coord_t *, reiser4_key *, void *);
 int extent_can_contain_key(const coord_t * coord, const reiser4_key * key, const reiser4_item_data *);
 int extent_mergeable(const coord_t * p1, const coord_t * p2);
 unsigned extent_nr_units(const coord_t *);
@@ -81,23 +81,18 @@ int extent_item_data_by_flow(const coord_t *, const flow_t *, reiser4_item_data 
 void extent_print(const char *, coord_t *);
 int extent_utmost_child(const coord_t * coord, sideof side, jnode ** child);
 int extent_utmost_child_real_block(const coord_t * coord, sideof side, reiser4_block_nr * block);
-reiser4_key *extent_max_key(const coord_t * coord, reiser4_key * key);
-int extent_key_in_item(coord_t * coord, const reiser4_key * key);
 int extent_key_in_unit(const coord_t * coord, const reiser4_key * key);
 void extent_item_stat(const coord_t * coord, void *vp);
 int extent_check(const coord_t * coord, const char **error);
 
 /* plugin->u.item.s.file.* */
-int extent_write(struct inode *, coord_t *, lock_handle *, flow_t *);
+int extent_write(struct inode *, coord_t *, lock_handle *, flow_t *, struct sealed_coord *);
 int extent_read(struct file *, coord_t *, flow_t *);
 int extent_readpage(coord_t *, struct page *page);
 void extent_readpages(coord_t *, struct address_space *, struct list_head *pages);
 int extent_writepage(coord_t *, lock_handle *, struct page *);
-#if 0
-int extent_page_cache_readahead(struct file *file, coord_t * coord,
-				lock_handle * lh UNUSED_ARG,
-				unsigned long start_page, unsigned long intrafile_readahead_amount);
-#endif
+reiser4_key *extent_append_key(const coord_t * coord, reiser4_key * key, void *);
+int extent_key_in_item(coord_t * coord, const reiser4_key * key, void *);
 int extent_get_block_address(const coord_t *, sector_t block, struct buffer_head *);
 
 /* these are used in flush.c
@@ -109,7 +104,6 @@ int extent_is_unallocated(const coord_t * item);	/* True if this extent is unall
 __u64 extent_unit_index(const coord_t * item);	/* Block offset of this unit. */
 __u64 extent_unit_width(const coord_t * item);	/* Number of blocks in this unit. */
 reiser4_block_nr extent_unit_start(const coord_t * item);	/* Starting block location of this unit. */
-void extent_get_inode(const coord_t * item, struct inode **inode);	/* Get the inode: you must iput() it. */
 
 /* __REISER4_EXTENT_H__ */
 #endif
