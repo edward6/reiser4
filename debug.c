04191047f0dcbeeee675c034bd4302325666b325
namesys.com!god <namesys.com!god>
2003-04-20 18:19:59 +0000

    conflict resolving
    2003/02/27 12:49:34+03:00 namesys.com!god
    Auto merged
    
    2003/02/19 17:30:19+03:00 namesys.com!god
    add spin lock wrapper functions for cbk cache
    
    2003/02/19 15:43:22+03:00 namesys.com!god
    add prof.counter for jrelse()
    
    (Logical change 1.3197)

--- debug.c
+++ debug.c
@@ -333,10 +333,10 @@ show_prof_attr(struct super_block * s, reiser4_kattr * kattr,
 
 reiser4_stats_cnt reiser4_prof_defs[] = {
 	DEFINE_PROF_CNT(jload),
+	DEFINE_PROF_CNT(jrelse),
 	DEFINE_PROF_CNT(carry),
 	DEFINE_PROF_CNT(flush_alloc),
-	DEFINE_PROF_CNT(forward_squalloc),
-	DEFINE_PROF_CNT(load_page)
+	DEFINE_PROF_CNT(forward_squalloc)
 };
 
 void calibrate_prof(void)
