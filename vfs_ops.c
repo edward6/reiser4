4b54a1572898370f2faee75b612870a41f76e6da
namesys.com!god <namesys.com!god>
2003-04-20 18:24:46 +0000

    Auto merged
    2003/04/17 18:26:04+04:00 namesys.com!god
    remove obsolete assertion
    
    (Logical change 1.3470)

--- vfs_ops.c
+++ vfs_ops.c
@@ -2486,8 +2486,6 @@ reiser4_invalidatepage(struct page *page, unsigned long offset)
 		if (node != NULL) {
 			jref(node);
 			JF_SET(node, JNODE_HEARD_BANSHEE);
-			assert("nikita-3149", 
-			       !JF_ISSET(node, JNODE_FLUSH_QUEUED));
 			/* page cannot be detached from jnode concurrently,
 			 * because it is locked */
 			uncapture_page(page);
