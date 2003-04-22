3a248a6141391f3ff1a7039c78b2dea34d4851d6
namesys.com!vs <namesys.com!vs>
2003-04-22 15:39:15 +0000

    (Logical change 1.3502)

---
+++ prof.c
@@ -0,0 +1,224 @@
+/* Copyright 2001, 2002 by Hans Reiser, licensing governed by reiser4/README */
+
+/* profiling facilities. */
+
+#include "kattr.h"
+#include "reiser4.h"
+#include "context.h"
+#include "super.h"
+#include "prof.h"
+
+#include <linux/sysfs.h>
+#include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <linux/spinlock.h>
+#include <linux/kallsyms.h>
+
+#if REISER4_PROF
+
+
+
+#ifdef CONFIG_FRAME_POINTER
+static void 
+update_prof_trace(reiser4_prof_cnt *cnt)
+{
+	int i;
+	int minind;
+	__u64 minhit;
+	unsigned long hash;
+	backtrace_path bt;
+
+	fill_backtrace(bt);
+
+	for (i = 0, hash = 0 ; i < REISER4_BACKTRACE_DEPTH ; ++ i) {
+		hash <<= 4;
+		hash ^= (((unsigned long)bt[i]) >> 2);
+	}
+	minhit = ~0ull;
+	minind = 0;
+	for (i = 0 ; i < REISER4_PROF_TRACE_NUM ; ++ i) {
+		if (hash == cnt->bt[i].hash) {
+			++ cnt->bt[i].hits;
+			return;
+		}
+		if (cnt->bt[i].hits < minhit) {
+			minhit = cnt->bt[i].hits;
+			minind = i;
+		}
+	}
+	memcpy(&cnt->bt[minind].path, bt, sizeof bt);
+	cnt->bt[minind].hash = hash;
+	cnt->bt[minind].hits = 1;
+}
+#else
+#define update_prof_trace(cnt) noop
+#endif
+
+void update_prof_cnt(reiser4_prof_cnt *cnt, __u64 then, __u64 now, 
+		     unsigned long swtch_mark, __u64 start_jif)
+{
+	__u64 delta;
+
+	delta = now - then;
+	cnt->nr ++;
+	cnt->total += delta;
+	cnt->max = max(cnt->max, delta);
+	if (swtch_mark == nr_context_switches()) {
+		cnt->noswtch_nr ++;
+		cnt->noswtch_total += delta;
+		cnt->noswtch_max = max(cnt->noswtch_max, delta);
+	}
+	update_prof_trace(cnt);
+}
+
+
+static ssize_t 
+show_prof_attr(struct kobject *kobj, struct attribute *attr, char *buf)
+{
+	char *p;
+	reiser4_prof_entry *entry;
+	reiser4_prof_cnt   *val;
+	int i;
+
+	entry = container_of(attr, reiser4_prof_entry, attr);
+	val = &entry->cnt;
+	p = buf;
+	KATTR_PRINT(p, buf, "%llu %llu %llu %llu %llu %llu\n",
+		    val->nr, val->total, val->max,
+		    val->noswtch_nr, val->noswtch_total, val->noswtch_max);
+#ifdef CONFIG_FRAME_POINTER
+	for (i = 0 ; i < REISER4_BACKTRACE_DEPTH ; ++ i) {
+		int j;
+
+		KATTR_PRINT(p, buf, "\t%llu: ", val->bt[i].hits);
+		for (j = 0 ; j < REISER4_BACKTRACE_DEPTH ; ++ j) {
+			char         *module;
+			const char   *name;
+			char          namebuf[128];
+			unsigned long address;
+			unsigned long offset;
+			unsigned long size;
+
+			address = (unsigned long) val->bt[i].path[j];
+			name = kallsyms_lookup(address, &size, 
+					       &offset, &module, namebuf);
+			KATTR_PRINT(p, buf, "0x%lx ", address);
+			if (name != NULL)
+				KATTR_PRINT(p, buf, "%s+%lx/%lx [%s] ", name, 
+					    offset, size, module ? : "core");
+		}
+		KATTR_PRINT(p, buf, "\n");
+	}
+#endif
+	return (p - buf);
+}
+
+/* zero a prof entry corresponding to @attr */
+static ssize_t 
+store_prof_attr(struct kobject *kobj, struct attribute *attr, const char *buf, size_t size)
+{
+	reiser4_prof_entry *entry;
+
+	entry = container_of(attr, reiser4_prof_entry, attr);
+	memset(&entry->cnt, 0, sizeof(reiser4_prof_cnt));
+	return sizeof(reiser4_prof_cnt);
+}
+
+static struct sysfs_ops prof_attr_ops = {
+	.show = show_prof_attr,
+	.store = store_prof_attr
+};
+
+struct kobj_type ktype_reiser4_prof = {
+	.sysfs_ops	= &prof_attr_ops,
+	.default_attrs	= NULL
+};
+
+#define DEFINE_PROF_ENTRY_0(attr_name,field_name)	\
+	.field_name = {					\
+		.attr = {	       			\
+			.name = attr_name,		\
+			.mode = 0644 /* rw-r--r-- */	\
+		}					\
+	}
+
+
+#define DEFINE_PROF_ENTRY(name)				\
+ 	DEFINE_PROF_ENTRY_0(#name,name)
+
+reiser4_prof reiser4_prof_defs = {
+	DEFINE_PROF_ENTRY(jload),
+	DEFINE_PROF_ENTRY(jrelse),
+	DEFINE_PROF_ENTRY(carry),
+	DEFINE_PROF_ENTRY(flush_alloc),
+	DEFINE_PROF_ENTRY(forward_squalloc),
+	DEFINE_PROF_ENTRY(atom_wait_event),
+	DEFINE_PROF_ENTRY(set_child_delimiting_keys),
+	DEFINE_PROF_ENTRY(zget),
+	/* write profiling */
+	DEFINE_PROF_ENTRY(extent_write),
+	DEFINE_PROF_ENTRY(copy),
+	DEFINE_PROF_ENTRY(extent_bdp),
+	DEFINE_PROF_ENTRY(set_seal),
+	DEFINE_PROF_ENTRY(update_sd),
+	DEFINE_PROF_ENTRY(bdp),
+	DEFINE_PROF_ENTRY(validate),
+	DEFINE_PROF_ENTRY(seal_validate),
+	DEFINE_PROF_ENTRY(update_sd_load),
+	DEFINE_PROF_ENTRY(update_sd_save),
+	DEFINE_PROF_ENTRY(update_sd_seal),
+	/* read profiling */
+	DEFINE_PROF_ENTRY(file_read),
+	DEFINE_PROF_ENTRY(prep),
+	DEFINE_PROF_ENTRY(load_hint),
+	DEFINE_PROF_ENTRY(find),
+	DEFINE_PROF_ENTRY(zload),
+	DEFINE_PROF_ENTRY(item_read),
+	DEFINE_PROF_ENTRY(set_hint),
+	DEFINE_PROF_ENTRY(save_hint)	
+};
+
+void calibrate_prof(void)
+{
+	__u64 start;
+	__u64 end;
+
+	rdtscll(start);
+	schedule_timeout(HZ/100);
+	rdtscll(end);
+	warning("nikita-2923", "1 sec. == %llu rdtsc.", (end - start) * 100);
+}
+
+
+int init_prof_kobject(struct super_block *super)
+{
+	reiser4_super_info_data *sbinfo;
+	int result;
+	struct kobject *prof_kobj;
+	
+	sbinfo = get_super_private(super);
+	prof_kobj = &sbinfo->prof_kobj;
+	prof_kobj->parent = kobject_get(&super->kobj);
+	snprintf(prof_kobj->name, KOBJ_NAME_LEN, "prof");
+	prof_kobj->ktype = &ktype_reiser4_prof;
+	result = kobject_register(prof_kobj);
+	if (result != 0)
+		return result;		
+	
+	/* populate */
+	{
+		int i;
+		reiser4_prof_entry *array;
+
+		array = (reiser4_prof_entry *)&reiser4_prof_defs;
+		for(i = 0 ; i < sizeof(reiser4_prof_defs)/sizeof(reiser4_prof_entry) && !result ; ++ i)
+			result = sysfs_create_file(prof_kobj,
+						   &array[i].attr);
+	}
+	return result;
+}
+
+
+/* REISER4_PROF */
+#endif
---
+++ prof.h
@@ -0,0 +1,109 @@
+/* Copyright 2001, 2002 by Hans Reiser, licensing governed by reiser4/README */
+
+#if !defined( __REISER4_PROF_H__ )
+#define __REISER4_PROF_H__
+
+#include "kattr.h"
+
+/* profiling. This is i386, rdtsc-based profiling. */
+
+#if (defined(__i386__) || defined(CONFIG_USERMODE)) && defined(CONFIG_REISER4_PROF)
+#define REISER4_PROF (1)
+#else
+#define REISER4_PROF (0)
+#endif
+
+#if REISER4_PROF
+
+#include <asm-i386/msr.h>
+
+#define REISER4_PROF_TRACE_NUM (8)
+
+typedef struct {
+	unsigned long hash;
+	backtrace_path path;
+	__u64 hits;
+} reiser4_trace;
+
+typedef struct {
+	__u64 nr;
+	__u64 total;
+	__u64 max;
+	__u64 noswtch_nr;
+	__u64 noswtch_total;
+	__u64 noswtch_max;
+	reiser4_trace bt[REISER4_PROF_TRACE_NUM];	
+} reiser4_prof_cnt;
+
+typedef struct {
+	struct attribute attr;
+	reiser4_prof_cnt cnt;
+} reiser4_prof_entry;
+
+typedef struct {
+	reiser4_prof_entry jload;
+	reiser4_prof_entry jrelse;
+	reiser4_prof_entry carry;
+	reiser4_prof_entry flush_alloc;
+	reiser4_prof_entry forward_squalloc;
+	reiser4_prof_entry atom_wait_event;
+	reiser4_prof_entry set_child_delimiting_keys;
+	reiser4_prof_entry zget;
+	/* write profiling */
+	reiser4_prof_entry extent_write;
+	reiser4_prof_entry copy;
+	reiser4_prof_entry extent_bdp;
+	reiser4_prof_entry set_seal;
+	reiser4_prof_entry update_sd;
+	reiser4_prof_entry bdp;
+	reiser4_prof_entry validate;
+	reiser4_prof_entry seal_validate;
+	reiser4_prof_entry update_sd_load;
+	reiser4_prof_entry update_sd_save;
+	reiser4_prof_entry update_sd_seal;
+	/* read profiling */
+	reiser4_prof_entry file_read;
+	reiser4_prof_entry prep;
+	reiser4_prof_entry load_hint;
+	reiser4_prof_entry find;
+	reiser4_prof_entry zload;
+	reiser4_prof_entry item_read;
+	reiser4_prof_entry set_hint;
+	reiser4_prof_entry save_hint;
+} reiser4_prof;
+
+extern reiser4_prof reiser4_prof_defs;
+
+extern unsigned long nr_context_switches(void);
+void update_prof_cnt(reiser4_prof_cnt *cnt, __u64 then, __u64 now, 
+		     unsigned long swtch_mark, __u64 start_jif);
+void calibrate_prof(void);
+
+#define PROF_BEGIN(aname)							\
+	unsigned long __swtch_mark__ ## aname = nr_context_switches();		\
+        __u64 __prof_jiffies ## aname = jiffies;				\
+	__u64 __prof_cnt__ ## aname = ({ __u64 __tmp_prof ;			\
+			      		rdtscll(__tmp_prof) ; __tmp_prof; })
+
+#define PROF_END(aname, acnt)							\
+({										\
+	__u64 __prof_end;							\
+										\
+	rdtscll(__prof_end);							\
+	update_prof_cnt(&reiser4_prof_defs.acnt.cnt, __prof_cnt__ ## aname, __prof_end,		\
+			__swtch_mark__ ## aname, __prof_jiffies ## aname );	\
+})
+
+#else
+
+typedef struct reiser4_prof_cnt {} reiser4_prof_cnt;
+typedef struct reiser4_prof {} reiser4_prof;
+
+#define PROF_BEGIN(aname) noop
+#define PROF_END(aname, acnt) noop
+#define calibrate_prof() noop
+
+#endif
+
+/* __REISER4_PROF_H__ */
+#endif
