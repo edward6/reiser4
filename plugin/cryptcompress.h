76613fa1b23943a1b7f2c2ea8ab262ca66f1d11f
namesys.com!edward <namesys.com!edward>
2003-04-24 15:31:11 +0000

    added crypto stat, cryptcompreess info
    (Logical change 1.3517)

--- plugin/cryptcompress.h
+++ plugin/cryptcompress.h
@@ -2,6 +2,19 @@
 
 #define MIN_CLUSTER_SIZE 4096
 
+inline struct cryptcompress_info *cryptcompress_inode_data(const struct inode * inode);
+
+/* secret key params supposed to be stored on disk */
+typedef struct crypto_stat {
+	__u16 keysize; /* key size, bits */
+	__u8 * keyid;  /* key public id */
+} crypto_stat_t; 
+
+typedef struct cryptcompress_info {
+	/* cpu-key words */
+	__u32 * expkey;
+} cryptcompress_info_t;
+
 /* Make Linus happy.
    Local variables:
    c-indentation-style: "K&R"
