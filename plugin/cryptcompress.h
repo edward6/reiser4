076352aedd7aa8bad6a74806ad3cb65ef759bbc1
namesys.com!edward <namesys.com!edward>
2003-04-25 18:35:15 +0000

    Added  typedefs for cluster and its status
    (Logical change 1.3531)

--- plugin/cryptcompress.h
+++ plugin/cryptcompress.h
@@ -1,6 +1,22 @@
 /* Copyright 2002 by Hans Reiser, licensing governed by reiser4/README */
+#include <linux/pagemap.h>
 
-#define MIN_CLUSTER_SIZE 4096
+#define MIN_CLUSTER_SIZE PAGE_CACHE_SIZE
+#define MAX_CLUSTER_SHIFT 4
+#define MIN_SIZE_FOR_COMPRESSION 64
+#define MIN_CRYPTO_BLOCKSIZE 8
+
+typedef enum {
+	DATA_CLUSTER = 0,
+	HOLE_CLUSTER = 1
+} reiser4_cluster_status;
+
+typedef struct reiser4_cluster{
+	coord_t coord;
+	__u8 * buf;
+	size_t len;
+	reiser4_cluster_status stat;
+} reiser4_cluster_t;
 
 inline struct cryptcompress_info *cryptcompress_inode_data(const struct inode * inode);
 
