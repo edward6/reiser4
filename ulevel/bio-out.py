e8b4ec7c0723e55a98c079d997e5d96cc0c1c47b
namesys.com!zam <namesys.com!zam>
2003-04-27 07:32:33 +0000

    n increment fix
    (Logical change 1.3533)

--- ulevel/bio-out.py
+++ ulevel/bio-out.py
@@ -1,23 +1,31 @@
 #! /usr/bin/python
 
+# Parse a reiser4 even log, find all mark=* events, print
+# all bios as "<seq.number> <block> <rw>". And, print
+# optional <trace_mark> suffix by a trace_mark if
+# mark=<trace_mark> event was found earlier for the same
+# thread.
+
 import sys
 import re
 
-marks = {}
-n = 0
+n = 1
+marks={}
+
 mark_cp = re.compile("mark=")
 bio_cp = re.compile("\.\.\.\.\.\.bio")
 
-def out_bio(first, len, rw, n, pid):
+def out_bio(first, len):
+    global n
     block = first
     mrk = ""
     if marks.has_key(pid):
         mrk = " " + marks[pid]
     for i in xrange(1, len + 1):
         print str(n) + " " + str(block) + " " + rw + mrk
+        n = n + 1
 
 for line in sys.stdin:
-    n = n + 1
     try:
         (pid, comm, dev, jiffies, rest) = re.split(" +", line, 4)
         if mark_cp.match(rest):
@@ -29,7 +37,7 @@ for line in sys.stdin:
             rw = m[2]
             blocks=m[4]
             (first, len) = re.match("\((\d+),(\d+)\)", blocks).groups()
-            out_bio(long(first), long(len), rw, n, pid)
+            out_bio(long(first), long(len))
     except ValueError:
         pass
 
