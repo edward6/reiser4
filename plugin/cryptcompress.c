6ffe5d384469614331546fcee30de5707cc2da94
namesys.com!edward <namesys.com!edward>
2003-05-15 09:08:32 +0000

    Fixed assert
    (Logical change 1.3611)

--- plugin/cryptcompress.c
+++ plugin/cryptcompress.c
@@ -218,7 +218,7 @@ static int
 cluster_key_by_inode(struct inode *inode, loff_t off, reiser4_key * key)
 {
 	assert("edward-64", inode != 0);
-	assert("edward-112", !(off & ~(~0UL << inode_cluster_shift(inode))));
+	assert("edward-112", !(off & ~(~0UL << inode_cluster_shift(inode) << PAGE_CACHE_SHIFT)));
 	/* don't come here with other offsets */
 	
 	build_sd_key(inode, key);
