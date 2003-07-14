f0fe7bb4f94b3ef9d50cd09a8e8da2b6c94cb8a4
namesys.com!god <namesys.com!god>
2003-07-14 08:30:08 +0000

    comment some fixme-s
    (Logical change 1.3909)

--- jnode.h
+++ jnode.h
@@ -152,8 +152,7 @@ struct jnode {
 TS_LIST_DEFINE(capture, jnode, capture_link);
 
 typedef enum {
-	/* jnode's data parsed */
-/* NIKITA-FIXME-HANS: this means what? */
+	/* jnode's page is loaded and data checked */
 	JNODE_PARSED = 0,
        /* node was deleted, not all locks on it were released. This
 	   node is empty and is going to be removed from the tree
