bbe46d697d3c5c4f1f54e13873fe1d4aa86a6b6a
namesys.com!god <namesys.com!god>
2003-09-08 13:07:04 +0000

    fix error in last commit
    (Logical change 1.4101)

--- tree_walk.c
+++ tree_walk.c
@@ -268,7 +268,8 @@ link_left_and_right(znode * left, znode * right)
 			ZF_SET(left, JNODE_RIGHT_CONNECTED);
 		} else if (ZF_ISSET(left->right, JNODE_HEARD_BANSHEE)) {
 			left->right->left = NULL;
-			left->right = NULL;
+			left->right = right;
+			ZF_SET(left, JNODE_RIGHT_CONNECTED);
 		} else
 			/*
 			 * there is a race condition in renew_sibling_link()
@@ -282,17 +283,17 @@ link_left_and_right(znode * left, znode * right)
 			 * overwriting correct left->right pointer installed
 			 * by T2.
 			 */
-			assert("nikita-3302", 
+			assert("nikita-3302",
 			       right == NULL || left->right == right);
 	}
-
 	if (right != NULL) {
 		if (right->left == NULL) {
 			right->left = left;
 			ZF_SET(right, JNODE_LEFT_CONNECTED);
 		} else if (ZF_ISSET(right->left, JNODE_HEARD_BANSHEE)) {
 			right->left->right = NULL;
-			right->left = NULL;
+			right->left = left;
+			ZF_SET(right, JNODE_LEFT_CONNECTED);
 		} else
 			assert("nikita-3303", 
 			       left == NULL || right->left == left);
