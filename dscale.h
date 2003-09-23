1efecbaafc326978013e7e42f2fbedc46e1b3819
namesys.com!god <namesys.com!god>
2003-09-23 08:45:27 +0000

    update copyright notice
    (Logical change 1.4157)

--- dscale.h
+++ dscale.h
@@ -1,4 +1,5 @@
-/* Copyright 2001, 2002 by Hans Reiser, licensing governed by reiser4/README */
+/* Copyright 2001, 2002, 2003 by Hans Reiser, licensing governed by
+ * reiser4/README */
 
 /* Scalable on-disk integers */
 
