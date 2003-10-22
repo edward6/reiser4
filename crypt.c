89204cec4632e445c2ba6a324803ad5b0b00af5e
namesys.com!edward <namesys.com!edward>
2003-10-22 15:36:37 +0000

    Added comments
    (Logical change 1.4276)

--- crypt.c
+++ crypt.c
@@ -9,51 +9,68 @@
 #define NONE_EXPKEY_WORDS 8
 #define NONE_BLOCKSIZE 8
 
-/* EDWARD-FIXME-HANS: define armoring and aligning
-
-
-default align() method of the crypto-plugin
-   1) creates the following aligning armored format of the input flow before encryption :
+/*   
+     
+Default align() method of the crypto-plugin (look for description of this method
+in plugin/plugin.h)  
+1) creates the following aligning armored format of the input flow before encryption :
 
    [ flow | aligninig_padding ]
             ^   
             |
 	  @pad
-  2) returns length of appended padding
+2) returns length of appended padding
+
+"armored" means that padding is filled by private data (for example,
+pseudo-random sequence of bytes is not private data).   
+
 */
-static int align_cluster_common(__u8 *pad, int clust_size, int blocksize)
+static int align_cluster_common(__u8 *pad /* pointer to the first byte of aligning format */,
+				int flow_size /* size of non-aligned flow */,
+				int blocksize /* crypto-block size */)
 {
 	int pad_size;
 	
 	assert("edward-01", pad != NULL);
-	assert("edward-02", clust_size != 0);
+	assert("edward-02", flow_size != 0);
 	assert("edward-03", blocksize != 0 || blocksize <= MAX_CRYPTO_BLOCKSIZE);
 	
-	pad_size = blocksize - (clust_size % blocksize);
+	pad_size = blocksize - (flow_size % blocksize);
 	get_random_bytes (pad, pad_size);
 	return pad_size;
 }
 
-/* use this only for symmetric algorithms
-EDWARD-FIXME-HANS: go through all your code and write function headers that explain what the function does.  For instance, what is "scale".*/
+/* common scale method (look for description of this method in plugin/plugin.h)
+   for all symmetric algorithms which doesn't scale anything
+*/
 static loff_t scale_common(struct inode * inode UNUSED_ARG,
-			   size_t blocksize UNUSED_ARG,
-			   loff_t src_off)
+			   size_t blocksize UNUSED_ARG /* crypto block size, which is returned
+							  by blocksize method of crypto plugin */,
+			   loff_t src_off /* offset to scale */)
 {
 	return src_off;
 }
 
-static size_t blocksize_none (__u16 keysize UNUSED_ARG /* keysize bits */)
+/* blocksize method (look for description of this method in plugin/plugin.h)
+   for none crypto plugin */
+static size_t blocksize_none (__u16 keysize UNUSED_ARG /* size of private key, bits */)
 {
 	return NONE_BLOCKSIZE;
 }
 
-static int set_key_none(__u32 *expkey, const __u8 *key UNUSED_ARG)
+/* set_key (look for description of this method in plugin/plugin.h)
+   for none crypto plugin */
+static int set_key_none(__u32 *expkey /* cpu key */, 
+			const __u8 *key UNUSED_ARG)
 {
 	memset(expkey, 0, NONE_EXPKEY_WORDS * sizeof(__u32));
 	return 0;	
 }
 
+/* plugin->encrypt,
+   plugin->decrypt for none crypto plugin
+   (look for description of this methods in plugin/plugin.h)
+*/
 static void crypt_none (__u32 *expkey UNUSED_ARG, __u8 *dst, const __u8 *src)
 {
 	assert("edward-04", dst != NULL);
