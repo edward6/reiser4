46fc53d5433d3f3a3c801e06cd9373b651ea9238
namesys.com!god <namesys.com!god>
2003-11-17 14:39:25 +0000

    Auto merged
    2003/11/03 13:21:03+03:00 namesys.com!god
    add debugging checks
    
    (Logical change 1.4326)

--- block_alloc.c
+++ block_alloc.c
@@ -647,6 +647,7 @@ update_blocknr_hint_default (const struct super_block *s, const reiser4_block_nr
 	} else {
 		warning("zam-676",
 			"block number %llu is too large to be used in a blocknr hint\n", (unsigned long long) *block);
+		DEBUGON(1);
 	}
 	reiser4_spin_unlock_sb(sbinfo);
 }
