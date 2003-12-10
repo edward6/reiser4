55acf8549f2acb89475436b1dc7cae2305fc73a8
namesys.com!zam <namesys.com!zam>
2003-12-10 14:29:16 +0000

    removed unused (and wrong) r/a parameters
    (Logical change 1.4381)

--- readahead.h
+++ readahead.h
@@ -6,12 +6,6 @@
 
 typedef enum {
 	RA_ADJACENT_ONLY = 1,       /* only requests nodes which are adjacent. Default is NO (not only adjacent) */
-	RA_ALL_LEVELS = 2,	    /* only request readahead for children of twin nodes. Default is NO (leaves only) */
-	RA_CONTINUE_ON_PRESENT = 4, /* when one of nodes to be readahead is in memory already, skip it, but continue
-				       readahead submission. Default is NO (stop submission) */
-                                    /* The implementation was removed by Hans' request. */
-	RA_READ_ON_GRN = 8          /* looking for right neighbor read all necessary nodes. Default is NO (use cache
-				       only) */
 } ra_global_flags;
 
 /* reiser4 super block has a field of this type. It controls readahead during tree traversals */
