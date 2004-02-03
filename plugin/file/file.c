7245252a66330592fcbbc6b71f7436177ce8e84d
namesys.com!god <namesys.com!god>
2004-02-03 10:24:29 +0000

    invalidate extended coord at the entry to read/write
    (Logical change 1.4526)

--- plugin/file/file.c
+++ plugin/file/file.c
@@ -773,6 +773,9 @@ load_file_hint(struct file *file, hint_t *hint, lock_handle *lh)
 		if (seal_is_set(&fsdata->reg.hint.seal)) {
 			*hint = fsdata->reg.hint;
 			hint->coord.lh = lh;
+			/* force re-validation of the coord on the first
+			 * iteration of the read/write loop. */
+			hint->coord.valid = 0;
 			return 0;
 		}
 		xmemset(&fsdata->reg.hint, 0, sizeof(hint_t));
@@ -1635,15 +1638,15 @@ static struct vm_operations_struct unix_file_vm_ops = {
 	.nopage = unix_file_filemap_nopage,
 };
 
-/* This function takes care about @file's pages. First of all it checks if 
-   filesystems readonly and if so gets out. Otherwise, it throws out all 
-   pages of file if it was mapped for read and going to be mapped for write 
-   and consists of tails. This is done in order to not manage few copies 
-   of the data (first in page cache and second one in tails them selves) 
-   for the case of mapping files consisting tails. 
-   
-   Here also tail2extent conversion is performed if it is allowed and file 
-   is going to be written or mapped for write. This functions may be called 
+/* This function takes care about @file's pages. First of all it checks if
+   filesystems readonly and if so gets out. Otherwise, it throws out all
+   pages of file if it was mapped for read and going to be mapped for write
+   and consists of tails. This is done in order to not manage few copies
+   of the data (first in page cache and second one in tails them selves)
+   for the case of mapping files consisting tails.
+
+   Here also tail2extent conversion is performed if it is allowed and file
+   is going to be written or mapped for write. This functions may be called
    from write_unix_file() or mmap_unix_file(). */
 static int
 check_pages_unix_file(struct file *file, int vm_flags, int caller_is_write,
