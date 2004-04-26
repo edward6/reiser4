2efce4a9fb13a3caf0d9bf8db1d20c538253cf9a
namesys.com!vs <namesys.com!vs>
2004-04-26 09:28:33 +0000

    reiser4_status_init reiser4_status_write: blk_run_queues is not needed?
    (Logical change 1.4802)

--- status_flags.c
+++ status_flags.c
@@ -63,7 +63,7 @@ reiser4_internal int reiser4_status_init(reiser4_block_nr block)
 	}
 	lock_page(page);
 	submit_bio(READ, bio);
-	blk_run_queues();
+	/*blk_run_queues();*/
 	wait_on_page_locked(page);
 	if ( !PageUptodate(page) ) {
 		warning("green-2007", "I/O error while tried to read status page\n");
@@ -171,7 +171,7 @@ reiser4_status_write(u64 status, u64 extended_status, char *message)
 	lock_page(get_super_private(sb)->status_page); // Safe as nobody should touch our page.
 	/* We can block now, but we have no other choice anyway */
 	submit_bio(WRITE, bio);
-	blk_run_queues(); // Now start the i/o.
+	/*blk_run_queues();*/ // Now start the i/o.
 	return 0; // We do not wait for io to finish.
 }
 
