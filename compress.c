1b181affbfd94e03dc60abb380be6e4ee3cd98f2
namesys.com!edward <namesys.com!edward>
2004-05-26 18:37:30 +0000

    added GZIP compression transform plugin
    (Logical change 1.4881)

--- compress.c
+++ compress.c
@@ -1,25 +1,47 @@
-/* compression plugins */
+/* Copyright 2001, 2002, 2003 by Hans Reiser, licensing governed by reiser4/README */
+/* reiser4 compression transform plugins */
 
+/* reiser4 compression transform plugins */
 
 #include "debug.h"
 #include "plugin/plugin.h"
 #include "plugin/cryptcompress.h"
 #include <linux/types.h>
 
-static void compress_none(__u8 *buf, __u8 *src_first, unsigned src_len,
-			  __u8 *dst_first, unsigned *dst_len)
+REGISTER_NONE_ALG(compress, COMPRESS)
+     
+     
+/* GZIP default compression */
+     
+int alloc_gzip_compress(struct inode * inode)
 {
-	assert("edward-17", buf != NULL);
-	assert("edward-18", src_first != NULL);
-	assert("edward-19", src_len != 0);
-	assert("edward-20", dst_first != NULL);
-	assert("edward-21", dst_len != NULL);
+	cryptcompress_info_t * info;
+        assert("edward-664", inode != NULL);
 	
-	*dst_len = src_len;
-	memcpy(dst_first, src_first, src_len);
+	info = cryptcompress_inode_data(inode);
+	assert("edward-665", info->tfm[COMPRESS_TFM] == NULL);
+	
+	info->tfm[COMPRESS_TFM] = crypto_alloc_tfm("deflate", 0);
+	if (info->tfm[COMPRESS_TFM] == NULL) {
+		warning("edward-666", "failed to load transform for deflate\n");
+		return -ENOMEM;
+	}
+  	return 0;
+}     
+
+void free_gzip_compress(struct inode * inode)
+{
+	cryptcompress_info_t * info;
+        assert("edward-667", inode != NULL);
+	
+	info = cryptcompress_inode_data(inode);
+	assert("edward-668", info->tfm[COMPRESS_TFM] != NULL);
+	
+	crypto_free_tfm(info->tfm[COMPRESS_TFM]);
+	info->tfm[COMPRESS_TFM] = NULL;
+  	return;
 }
-			
-/* compression plugins */
+
 compression_plugin compression_plugins[LAST_COMPRESSION_ID] = {
 	[NONE_COMPRESSION_ID] = {
 		.h = {
@@ -27,12 +49,40 @@ compression_plugin compression_plugins[LAST_COMPRESSION_ID] = {
 			.id = NONE_COMPRESSION_ID,
 			.pops = NULL,
 			.label = "none",
-			.desc = "Null compression",
+			.desc = "absence of any compression transform",
 			.linkage = TYPE_SAFE_LIST_LINK_ZERO
 		},
-		.mem_req = MIN_CLUSTER_SIZE,
+		.alloc = alloc_none_compress,
+		.free = free_none_compress,
 		.overrun = 0,
-	        .compress = compress_none,
-	        .decompress = compress_none
+	        .compress = NULL,
+	        .decompress = NULL
+	},
+	[GZIP_COMPRESSION_ID] = {
+		.h = {
+			.type_id = REISER4_COMPRESSION_PLUGIN_TYPE,
+			.id = GZIP_COMPRESSION_ID,
+			.pops = NULL,
+			.label = "gzip",
+			.desc = "gzip default level compression",
+			.linkage = TYPE_SAFE_LIST_LINK_ZERO
+		},
+		.alloc = alloc_gzip_compress,
+		.free = free_gzip_compress,
+		.overrun = 0,
+	        .compress = NULL,
+	        .decompress = NULL
 	}
 };
+
+/*
+  Local variables:
+  c-indentation-style: "K&R"
+  mode-name: "LC"
+  c-basic-offset: 8
+  tab-width: 8
+  fill-column: 120
+  scroll-step: 1
+  End:
+*/
+
