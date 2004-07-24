519ad87e0fd609d7d1c45891bfbdc138b4cb8862
namesys.com!reiser4 <namesys.com!reiser4>
2004-07-24 11:03:31 +0000

    extract_dent_name: debugging code
    (Logical change 1.4960)

--- plugin/item/sde.c
+++ plugin/item/sde.c
@@ -81,6 +81,8 @@ extract_dent_name(const coord_t * coord, directory_entry_format *dent, char *buf
 	reiser4_key key;
 
 	unit_key_by_coord(coord, &key);
+	if (get_key_type(&key) != KEY_FILE_NAME_MINOR)
+		print_address("oops", znode_get_block(coord->node));
 	if (!is_longname_key(&key)) {
 		if (is_dot_key(&key))
 			return (char *) ".";
