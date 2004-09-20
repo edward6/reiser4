bd588ee01420dd25bade7a41cc96990d33fa7d79
namesys.com!edward <namesys.com!edward>
2004-09-20 10:18:12 +0000

    moving definition of builtin compression plugins set
    (Logical change 1.5027)

--- plugin/compress/compress.h
+++ plugin/compress/compress.h
@@ -1,15 +1,26 @@
 #if !defined( __FS_REISER4_COMPRESS_H__ )
-#define REISER4_COMPRESS_H
+#define __FS_REISER4_COMPRESS_H__
 
 #include <linux/types.h>
 #include <linux/string.h>
-
-#define NONE_NRCOPY 4
 
 typedef enum {
 	TFM_READ,
 	TFM_WRITE
 } tfm_action;
+
+/* builtin compression plugins */
+
+typedef enum {
+	NONE_COMPRESSION_ID,
+	NULL_COMPRESSION_ID,
+	LZO1_COMPRESSION_ID,
+	GZIP1_COMPRESSION_ID,
+	LAST_COMPRESSION_ID,
+} reiser4_compression_id;
+
+typedef void * coa_t;
+typedef coa_t coa_set[LAST_COMPRESSION_ID];
 
 #endif /* __FS_REISER4_COMPRESS_H__ */
 
