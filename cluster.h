9f516f3d37cd47f27215ea707a83e7bf10ec4bdc
namesys.com!edward <namesys.com!edward>
2004-12-22 15:29:02 +0000

    Updated propotype
    (Logical change 1.5137)

--- cluster.h
+++ cluster.h
@@ -231,7 +231,7 @@ int flush_cluster_pages(reiser4_cluster_t *, jnode *, struct inode *);
 int deflate_cluster(reiser4_cluster_t *, struct inode *);
 void truncate_pg_clusters(struct inode * inode, pgoff_t start);
 void set_hint_cluster(struct inode * inode, hint_t * hint, unsigned long index, znode_lock_mode mode);
-int get_disk_cluster_locked(reiser4_cluster_t * clust, znode_lock_mode lock_mode);
+int get_disk_cluster_locked(reiser4_cluster_t * clust, struct inode * inode, znode_lock_mode lock_mode);
 int hint_prev_cluster(reiser4_cluster_t * clust, struct inode * inode);
 void set_nrpages_by_inode(reiser4_cluster_t * clust, struct inode * inode);
 int grab_cluster_pages(struct inode * inode, reiser4_cluster_t * clust);
