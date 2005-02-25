5ab2128ac0eeca446bdbb280dcda56b387d425d6
namesys.com!vs <namesys.com!vs>
2005-02-25 13:57:15 +0000

    debugging code in add_to_ctx_grabbed
    (Logical change 1.5173)

--- block_alloc.c
+++ block_alloc.c
@@ -159,6 +159,12 @@ sub_from_ctx_grabbed(reiser4_context *ctx, __u64 count)
 static void
 add_to_ctx_grabbed(reiser4_context *ctx, __u64 count)
 {
+#ifdef CONFIG_FRAME_POINTER
+	ctx->grabbed_at[0] = __builtin_return_address(0);
+	ctx->grabbed_at[1] = __builtin_return_address(1);
+	ctx->grabbed_at[2] = __builtin_return_address(2);
+	ctx->grabbed_at[3] = __builtin_return_address(3);
+#endif
 	ctx->grabbed_blocks += count;
 }
 
