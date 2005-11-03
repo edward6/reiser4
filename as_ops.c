ccf5f75669396178990d73450693af255a828878
Alexander Zarochentsev <zam@rogue.namesys.com>
2005-11-03 22:42:24 +0300

    A fix fo "no need to capture" path in reiser4_invalidatepage, there was a possibility to steal a node from atom's overwrite set when the atom is being committed. That leaded to incomplete post-commit-writes and and jnode's d_count and x_count leakage.

--- as_ops.c
+++ as_ops.c
@@ -229,7 +229,7 @@ int reiser4_invalidatepage(struct page *page, unsigned long offset)
 	node = jprivate(page);
 	spin_lock_jnode(node);
 	if (!JF_ISSET(node, JNODE_DIRTY) && !JF_ISSET(node, JNODE_FLUSH_QUEUED) &&
-	    !JF_ISSET(node, JNODE_WRITEBACK)) {
+	    !JF_ISSET(node, JNODE_WRITEBACK) && !JF_ISSET(node, JNODE_OVRWR)) {
 		/* there is not need to capture */
 		jref(node);
 		JF_SET(node, JNODE_HEARD_BANSHEE);
