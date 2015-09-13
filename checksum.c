213bf213a4ff08f2b85dfde60875d57b872913ba
Ivan Shapovalov <intelfx100@gmail.com>
2015-09-14 01:33:36 +0300

    reiser4: report an error on checksum engine load failure.
    It is apparently not possible to specify explicit module dependencies.
    For well-formed setups this is not a problem because
    crypto_alloc_shash() uses request_module() internally, but if one needs
    reiser4, say, in initramfs, then the checksum module must be added
    manually.
    
    Signed-off-by: Ivan Shapovalov <intelfx100@gmail.com>

--- checksum.c
+++ checksum.c
@@ -7,8 +7,10 @@ int reiser4_init_csum_tfm(struct crypto_shash **tfm)
 	struct crypto_shash *new_tfm;
 
 	new_tfm = crypto_alloc_shash("crc32c", 0, 0);
-	if (IS_ERR(new_tfm))
+	if (IS_ERR(new_tfm)) {
+		warning("intelfx-81", "Could not load crc32c driver");
 		return PTR_ERR(new_tfm);
+	}
 
 	*tfm = new_tfm;
 	return 0;
