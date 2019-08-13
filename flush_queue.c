8432e6ba801c377f5428534570c93fb447a147eb
Edward Shishkin <edward.shishkin@gmail.com>
2019-08-13 19:35:47 +0200

    Reiser4 port for Linux-5.1
    Adjust to changes in the block layer interface:
    . Use bio_for_each_segment_all macro to iterate over all bio's
      pages in end_io_handler() and undo_bio();
    . Don't use bi_vcnt, which doesn't represent total number of bio's
      pages any more.
    
    Signed-off-by: Edward Shishkin <edward.shishkin@gmail.com>

--- flush_queue.c
+++ flush_queue.c
@@ -399,8 +399,11 @@ int atom_fq_parts_are_clean(txn_atom * atom)
 static void end_io_handler(struct bio *bio)
 {
 	int i;
+	int nr = 0;
 	int nr_errors = 0;
 	flush_queue_t *fq;
+	struct bio_vec *bvec;
+	struct bvec_iter_all iter_all;
 
 	assert("zam-958", bio_op(bio) == WRITE);
 
@@ -408,8 +411,8 @@ static void end_io_handler(struct bio *bio)
 	 * for synchronization and error counting. */
 	fq = bio->bi_private;
 	/* Check all elements of io_vec for correct write completion. */
-	for (i = 0; i < bio->bi_vcnt; i += 1) {
-		struct page *pg = bio->bi_io_vec[i].bv_page;
+	bio_for_each_segment_all(bvec, bio, i, iter_all) {
+		struct page *pg = bvec->bv_page;
 
 		if (bio->bi_status) {
 			SetPageError(pg);
@@ -427,7 +430,7 @@ static void end_io_handler(struct bio *bio)
 
 			JF_CLR(node, JNODE_WRITEBACK);
 		}
-
+		nr ++;
 		end_page_writeback(pg);
 		put_page(pg);
 	}
@@ -438,7 +441,7 @@ static void end_io_handler(struct bio *bio)
 
 		/* If all write requests registered in this "fq" are done we up
 		 * the waiter. */
-		if (atomic_sub_and_test(bio->bi_vcnt, &fq->nr_submitted))
+		if (atomic_sub_and_test(nr, &fq->nr_submitted))
 			wake_up(&fq->wait);
 	}
 
@@ -453,7 +456,8 @@ void add_fq_to_bio(flush_queue_t *fq, struct bio *bio)
 	bio->bi_end_io = end_io_handler;
 
 	if (fq)
-		atomic_add(bio->bi_vcnt, &fq->nr_submitted);
+		atomic_add(bio->bi_iter.bi_size >> PAGE_SHIFT,
+			   &fq->nr_submitted);
 }
 
 /* Move all queued nodes out from @fq->prepped list. */
